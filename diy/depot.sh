839f141b2cafe3d04c781e0df3bc41b74b1e16c4
Luc Maranget <Luc.Maranget@inria.fr>
2015-09-09 13:26:55 +0000

    Script pre'limimaire pour depot/release (sans litmus a` ajouter...)
    git-svn-id: svn+ssh://scm.cs.ucl.ac.uk/cs/research/pplv/virginia/repos/herd/herd@1096 ca349960-8758-41ef-81b0-0e8c0ef917f4

---
+++ diy/depot.sh
@@ -0,0 +1,30 @@
+set -e
+DIR=`dirname $0`
+. $DIR/version.sh
+. $DIR/funs.sh
+TMP=/var/tmp
+FINAL=$TMP/diy-$V
+EXPORT=$FINAL
+/bin/rm -rf $FINAL && mkdir $FINAL
+(
+  extract gen gen && \
+  extract litmus litmus && \
+  extract herd herd && \
+  extract tools tools && \
+  extract lib lib && \
+  extract diy/Makefile.herd Makefile && \
+  extract diy/README.txt README.txt && \
+  extract diy/INSTALL.txt INSTALL.txt && \
+  extract diy/LICENSE.txt  LICENSE.txt && \
+  true
+)
+( cleandir $FINAL/litmus ) && \
+( cleandir $FINAL/gen ) && \
+( cleandir $FINAL/herd ) && \
+( cleandir $FINAL/tools )
+
+# add example and documentation
+sh $DIR/installdoc.sh $FINAL
+NAME=`basename $FINAL`
+( cd $TMP && tar cf - $NAME ) | gzip --best > $NAME.tar.gz
+/bin/rm -rf $FINAL
