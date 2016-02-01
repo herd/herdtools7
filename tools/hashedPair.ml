00f8dd0cec082f7adf3deab0ee7173c259fdd915
Luc Maranget <Luc.Maranget@inria.fr>
2016-02-01 10:40:28 +0000

    Bob Jenkins' hash for pairs.
    git-svn-id: svn+ssh://scm.cs.ucl.ac.uk/cs/research/pplv/virginia/repos/herd/herd@1156 ca349960-8758-41ef-81b0-0e8c0ef917f4

--- tools/hashedPair.ml
+++ tools/hashedPair.ml
@@ -17,11 +17,37 @@
 module S = struct
   type t = HashedString.t * HashedString.t
   let equal (a1,b1) (a2,b2) =  a1 == a2 && b1 == b2
+
+
+  let  mix a b c =
+    let a = a-b in let a = a-c in
+    let a = a lxor (c lsr 13) in
+    let b = b-c in let b = b-a in
+    let b = b lxor (a lsl 8) in
+    let c = c-a in let c = c-b in
+    let c = c lxor (b lsr 13) in
+    let a = a-b in let a = a-c in
+    let a = a lxor (c lsr 12) in 
+    let b = b-c in let b = b-a in
+    let b = b lxor (a lsl 16) in
+    let c = c-a in let c = c-b in
+    let c = c lxor (c lsr 5) in
+    let a = a-b in let a = a-c in
+    let a = a lxor (c lsl 3) in
+    let b = b-c in let b = b-a in
+    let b = b lxor (a lsl 10) in
+    let c = c-a in let c = c-b in
+    let c = c lxor (c lsr  15) in
+    c
+
+
+
   let combine seed v =
     (v + 0x9e3779b9 + (seed lsl 6) + (seed lsr 2)) lxor seed
   let hash (a,b) =
-    let seed =  HashedString.as_hash a in
-    abs (combine seed (HashedString.as_hash b))
+    let ah =  HashedString.as_hash a
+    and bh = HashedString.as_hash b in
+    abs (mix ah bh 0)
 end
 
 include(Hashcons.Make(S))
