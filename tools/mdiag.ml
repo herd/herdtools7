cb28529155dc7a659a5657b12e7edca16b379eef
Luc Maranget <Luc.Maranget@inria.fr>
2016-05-03 12:54:50 +0000

    ready for 7.09
    git-svn-id: svn+ssh://scm.cs.ucl.ac.uk/cs/research/pplv/virginia/repos/herd/herd@1205 ca349960-8758-41ef-81b0-0e8c0ef917f4

--- tools/mdiag.ml
+++ tools/mdiag.ml
@@ -18,44 +18,45 @@ open Printf
 module Top
     (Opt:
        sig
-         val verbose : bool
+         val verbose : int
        end) =
   struct
 
     module T = struct
       type t = 
         { tname : string ;
-          hash : string option; } 
+          fname : string ;
+          hash : string ; } 
+      let cmp_pair cmp1 cmp2 t1 t2 = match cmp1 t1 t2 with
+      | 0 -> cmp2 t1 t2
+      | r -> r
+
+      let compare t1 t2 =
+        cmp_pair
+          (fun t1 t2 -> String.compare t1.tname t2.tname)
+          (cmp_pair
+             (fun t1 t2 ->  String.compare t1.hash t2.hash)
+             (fun t1 t2 ->  String.compare t1.fname t2.fname))
+          t1 t2
     end
 
     module Make(A:ArchBase.S) = struct
 
       let zyva name parsed =
 	let tname = name.Name.name in
+        let fname =  name.Name.file in
 	let hash = MiscParser.get_hash parsed in
-	if Opt.verbose 
-	then eprintf "Name=%s Hash=%s\n"
-			   tname
-			    (match hash with 
-			    | None -> "none" 
-			    | Some h -> h);
-        { T.tname = tname ;
-          hash = hash; }
+        let hash = match hash with  | None -> assert false | Some h -> h in
+	if Opt.verbose > 1 then eprintf "Name=%s Hash=%s\n" tname hash ;
+        { T.tname = tname ; fname=fname; hash = hash; }
     end
 
     module Z = ToolParse.Top(T)(Make)
+    module TSet = MySet.Make(T)
 
-    let add k v m =
-      let old = StringMap.safe_find StringSet.empty k m in
-      StringMap.add k (StringSet.add v old) m
 
-    let do_test name (byName,byHash,fnames as k) =
-      try
-        let {T.tname; hash; } = Z.from_file name in
-        let hash = match hash with
-        | None -> assert false
-        | Some h -> h in
-        add tname hash byName,add hash tname byHash,add tname name fnames
+    let do_test name k =
+      try Z.from_file name::k
       with
       | Misc.Exit -> k
       | Misc.Fatal msg ->
@@ -69,22 +70,54 @@ module Top
     | Some _ -> true
     | None -> false
 
+    let add k v m =
+      let old = StringMap.safe_find TSet.empty k m in
+      StringMap.add k (TSet.add v old) m
+
     let zyva tests =
-      let byName,byHash,fnames =
-        Misc.fold_argv do_test tests (StringMap.empty,StringMap.empty,StringMap.empty) in
+      let tests = Misc.fold_argv do_test tests [] in
+      let tests = TSet.of_list tests in
+      let byName,byHash =
+        TSet.fold
+          (fun t (byName,byHash) -> add t.T.tname t byName,add t.T.hash t byHash)
+          tests (StringMap.empty,StringMap.empty) in            
       StringMap.iter
-        (fun tname hashes ->
-          if not (is_singleton hashes) then
-            printf "Error: name %s has different hashes\n"  tname)
+        (fun tname ts ->
+          let hashes = TSet.fold (fun t k -> t.T.hash::k) ts [] in
+          let hashes = StringSet.of_list hashes in
+          if not (is_singleton hashes) then begin
+            printf "Error: name %s has different hashes\n"  tname ;
+            StringSet.iter
+                (fun hash ->                  
+                  let fnames =
+                    try
+                      let ts = StringMap.find hash byHash in
+                      let fnames =
+                        TSet.fold
+                          (fun t k ->
+                            if t.T.tname = tname then t.T.fname::k else k)
+                          ts [] in
+                      StringSet.of_list fnames
+                    with Not_found -> StringSet.empty in
+                  if not (StringSet.is_empty fnames) then
+                    printf " %s\n"
+                      (StringSet.pp_str "," Misc.identity fnames))
+              hashes
+          end)
         byName ;
       StringMap.iter
-        (fun _hash names ->
+        (fun _hash ts ->
+          let names = TSet.fold (fun t k -> t.T.tname::k) ts [] in
+          let names = StringSet.of_list names in
           if not (is_singleton names) then
             printf "Warning: tests {%s} are the same test\n"
               (StringSet.pp_str "," Misc.identity names))
         byHash ;
+      
       StringMap.iter
-        (fun name fnames ->
+        (fun name ts ->
+          let fnames = TSet.fold (fun t k -> t.T.fname::k) ts [] in
+          let fnames = StringSet.of_list fnames in
           if not (is_singleton fnames) then begin
             printf "Warning: test %s is referenced more than once:\n"
               name ;
@@ -92,12 +125,12 @@ module Top
               (fun fname -> printf "  %s\n" fname)
               fnames
           end)
-        fnames ;
+        byName ;
       ()
   end
 
 
-let verbose = ref false
+let verbose = ref 0
 let arg = ref []
 let prog =
   if Array.length Sys.argv > 0 then Sys.argv.(0)
@@ -105,7 +138,7 @@ let prog =
 
 let () =
   Arg.parse
-    ["-v",Arg.Unit (fun () -> verbose := true), " be verbose";]
+    ["-v",Arg.Unit (fun () -> incr verbose), " be verbose";]
     (fun s -> arg := !arg @ [s])
     (sprintf "Usage: %s [options]* [test]*" prog)
 
