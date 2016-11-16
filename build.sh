0dd357e84fca7e3598db66487c59539d7cb93fa6
Vincent Jacques <vincent@vincent-jacques.net>
2016-11-16 13:48:06 +0100

    Compile with (almost) all warnings

--- build.sh
+++ build.sh
@@ -24,4 +24,14 @@ let rev = "$REV"
 let libdir = "$LIBDIR/"
 EOD
 
-ocamlbuild -no-links -j 5 $NATIVE
+ocamlbuild -no-links -j 5 -cflags -w,+a-4-9-27-29-33-41-45,-strict-sequence $NATIVE
+# Warnings ignored on purpose:
+# Warning 4: this pattern-matching is fragile
+# Warning 33: unused open
+# Warning 45: this open statement shadows the constructor
+
+# Warnings ignored temporarily to allow warning-free compilation (@todo Re-enable):
+# Warning 9: the following labels are not bound in this record pattern
+# Warning 27: unused variable
+# Warning 29: unescaped end-of-line in a string constant
+# Warning 41: xxx belongs to several types
