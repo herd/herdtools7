9f82a5614e4e1ebc458b9beab9bfe04fdf11a56c
Boqun Feng <boqun.feng@gmail.com>
2018-07-23 06:47:47 +0800

    gen_theme: Allow to try applying the functions multiple times
    Currently in gen_theme, we only support applying each call once in the
    call file, however, in order to support rule generation sequence such as
    relaxed -> release -> acq_release -> fully ordered, we need to be able
    to apply functions on unresolved call terms with updated environment
    multiple times.
    
    So this patch changes the logic of gen_theme: it will evaluate on the
    calls that could be resolved first, and update the environment and try
    the unresolved calls again and so forth. A new option is added to limit
    the loops for trying to resolve calls, the default value is 5.
    
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- jingle/gen_theme.ml
+++ jingle/gen_theme.ml
@@ -19,6 +19,7 @@ let verbose = ref false
 let map = ref None
 let call = ref None
 let outdir = ref None
+let loops = ref 5
 let prog =
   if Array.length Sys.argv > 0 
   then Sys.argv.(0)
@@ -35,15 +36,18 @@ let () =
      "-call",Arg.String (fun s -> call := Some s),
      "<name> - give the call file <name>";
      "-o",Arg.String (fun s -> outdir := Some s),
-     "<name> - directory for output files"]
+     "<name> - directory for output files";
+     "-n",Arg.Int (fun i -> loops := i),
+     "<loops> - times re-apply the functions in call file with the update env"]
     (fun _ -> ())
-    (sprintf "Usage: %s [option]* -map <file> -call <file>" prog)
+    (sprintf "Usage: %s [option]* -map <file> -call <file> -n <loops>" prog)
 
 
 let map = !map
 let call = !call
 let verbose = !verbose
 let outdir = !outdir
+let loops = !loops
 
 let parsed = match map with
 | None -> raise (Error "No map file provided.")
@@ -61,9 +65,16 @@ let parsed_call = match call with
         eprintf "File \"%s\": %s\n" s msg ;
         exit 1
 
-let rec apply f e map_env func_env = match List.filter (fun (x, _, _) -> String.equal x f) func_env with
+
+let filter_func f func_env = List.filter (fun (x, _, _) -> String.equal x f) func_env
+
+let filter_map e map_env = List.filter (fun (x, _) -> String.equal x e) map_env
+
+let filter_unmap e map_env = List.filter (fun (x, _) -> not (String.equal x e)) map_env
+
+let rec apply f e map_env func_env = match filter_func f func_env with
 | [] -> raise(Error ("no func for " ^ f ^ "."))
-| (_, d, c) :: tl -> match List.filter (fun (x, _) -> String.equal x e) map_env with
+| (_, d, c) :: tl -> match map_env with
                | [] -> raise(Error ("no left rule for " ^ e ^ "."))
                | (_, mr) :: _ ->
                     let re = Str.regexp d in
@@ -71,11 +82,18 @@ let rec apply f e map_env func_env = match List.filter (fun (x, _, _) -> String.
                         Str.replace_first (Str.regexp d) c mr
                     with Not_found -> apply f e map_env tl
 
-let expand call_list map_env func_env =
+let rec expand call_list map_env func_env times =
+        let callable = List.filter (fun (_, _, e) -> List.mem_assoc e map_env) call_list in
+        let uncallable = List.filter (fun (_, _, e) -> not (List.mem_assoc e map_env)) call_list in
         let try_expand (l, f, a) = 
-                let r = apply f a map_env func_env
+                let r = apply f a (filter_map a map_env) (filter_func f func_env)
                 in (l, r)
-        in List.map try_expand call_list
+        in
+        let res = List.map try_expand callable in
+        if times <= 1 then res
+        else match uncallable with
+                | [] -> res
+                | (_, _, _) :: _ -> (expand uncallable (res @ map_env) func_env (times - 1)) @ res
 
 let () =
   if verbose then begin
@@ -100,6 +118,6 @@ let () =
   let funcs = parsed.ParseMap.funcs in
 
   List.iter (fun (l, r) -> Printf.printf "\"%s\" -> \"%s\"\n" l r) maps;
-  List.iter (fun (l, r) -> Printf.printf "\"%s\" -> \"%s\"\n" l r) (expand calls maps funcs)
+  List.iter (fun (l, r) -> Printf.printf "\"%s\" -> \"%s\"\n" l r) (expand calls maps funcs loops)
 
 
