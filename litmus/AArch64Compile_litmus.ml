aff42a31d774daa0cbe257beb3b1e2e03e1f17b3
Luc Maranget <Luc.Maranget@inria.fr>
2019-04-16 18:06:41 +0200

    litmus: Add MRS, RBIT instructions. Add TST and CSET aliases.

--- litmus/AArch64Compile_litmus.ml
+++ litmus/AArch64Compile_litmus.ml
@@ -417,20 +417,22 @@ module Make(V:Constant.S)(C:Config) =
         memo = sprintf "adr %s,%s" f (A.Out.dump_label (tr_lab lbl));
         outputs=r; reg_env=add_v r; }
 
-    let movr v r1 r2 = match v with
+    let do_movr memo v r1 r2 = match v with
     | V32 ->
         let r1,f1 = arg1 "wzr" (fun s -> "^wo"^s) r1
         and r2,f2 = arg1 "wzr" (fun s -> "^wi"^s) r2 in
         { empty_ins with
-          memo=sprintf "mov %s,%s" f1 f2;
+          memo=sprintf "%s %s,%s" memo f1 f2;
           inputs = r2; outputs=r1; reg_env=add_w (r1@r2);}
     | V64 ->
         let r1,f1 = arg1 "xzr" (fun s -> "^o"^s) r1
         and r2,f2 = arg1 "xzr" (fun s -> "^i"^s) r2 in
         { empty_ins with
-          memo=sprintf "mov %s,%s" f1 f2;
+          memo=sprintf "%s %s,%s" memo f1 f2;
           inputs = r2; outputs=r1; reg_env=add_q (r1@r2);}
 
+    let movr = do_movr "mov"
+    and rbit = do_movr "rbit"
 
     let sxtw r1 r2 =
       { empty_ins with
@@ -460,6 +462,14 @@ module Make(V:Constant.S)(C:Config) =
         { empty_ins with
           memo = sprintf "cmp %s,%s" fm1 fm2;
           inputs = rs; reg_env=List.map (fun r -> r,quad) rs; }
+
+    let tst v r i =
+      let add,(r,f) =  match v with
+      | V32 -> add_w,arg1 "wzr" (fun s -> "^wo"^s) r
+      | V64 -> add_q,arg1 "xzr" (fun s -> "^o"^s) r in
+      { empty_ins with
+        memo = sprintf "tst %s,#%i" f i;
+        outputs=r; reg_env = add r;}
 
     let memo_of_op op = Misc.lowercase (pp_op op)
 
@@ -560,9 +570,11 @@ module Make(V:Constant.S)(C:Config) =
     | I_MOV (v,r,K i) ->  movk v r i::k
     | I_MOV (v,r1,RV (_,r2)) ->  movr v r1 r2::k
     | I_ADDR (r,lbl) -> adr tr_lab r lbl::k
+    | I_RBIT (v,rd,rs) -> rbit v rd rs::k
     | I_SXTW (r1,r2) -> sxtw r1 r2::k
     | I_OP3 (v,SUBS,ZR,r,K i) ->  cmpk v r i::k
     | I_OP3 (v,SUBS,ZR,r2,RV (v3,r3)) when v=v3->  cmp v r2 r3::k
+    | I_OP3 (v,ANDS,ZR,r,K i) -> tst v r i::k
     | I_OP3 (v,op,r1,r2,kr) ->  op3 v op r1 r2 kr::k
 (* Fence *)
     | I_FENCE f -> fence f::k
@@ -576,24 +588,41 @@ module Make(V:Constant.S)(C:Config) =
     | I_STOPBH (op,v,w,rs,rn) ->
         stop (stopbh_memo op v w) V32 rs rn::k
 (* Conditional selection *)
+    | I_CSEL (v,r,ZR,ZR,c,Inc) ->
+        let o,f = match v with
+        | V32 -> arg1 "wzr" (fun s -> "^wo"^s) r
+        | V64 -> arg1 "xzr" (fun s -> "^o"^s) r
+        and t = match v with V32 -> word | V64 -> quad in
+        let memo =
+          sprintf "cset %s,%s" f (pp_cond (inverse_cond c)) in
+        { empty_ins with memo; outputs=o; reg_env=add_type t o; }::k
     | I_CSEL (v,r1,r2,r3,c,op) ->
         let inputs,memo,t = match v with
         | V32 ->
             let r2,f2,r3,f3 = args2 "wzr" (fun s -> "^wi"^s) r2 r3 in
-            r2@r3,sprintf " ^wo0,%s,%s,%s" f2 f3 (pp_cond c),word
+            r2@r3,sprintf "%s,%s,%s" f2 f3 (pp_cond c),word
         | V64 ->
             let r2,f2,r3,f3 = args2 "xzr" (fun s -> "^i"^s) r2 r3 in
-            r2@r3,sprintf " ^o0,%s,%s,%s" f2 f3 (pp_cond c),quad in
-        let memo = Misc.lowercase (sel_memo op) ^ memo in
+            r2@r3,sprintf "%s,%s,%s" f2 f3 (pp_cond c),quad in
+        let o,f = match v with
+        | V32 -> arg1 "wzr" (fun s -> "^wo"^s) r1
+        | V64 -> arg1 "xzr" (fun s -> "^o"^s) r1 in
+        let memo = Misc.lowercase (sel_memo op) ^ " " ^ f ^ "," ^ memo in
         {
          empty_ins with
-         memo = memo; inputs=inputs; outputs=[r1;];
-         reg_env=add_type t (r1::inputs);
+         memo = memo; inputs=inputs; outputs=o;
+         reg_env=add_type t (o@inputs);
         }::k
     | I_IC (op,r) ->
         cache (sprintf "ic %s" (Misc.lowercase (IC.pp_op op))) r::k
     | I_DC (op,r) ->
         cache (sprintf "dc %s" (Misc.lowercase (DC.pp_op op))) r::k
+    | I_MRS (r,sr) ->
+        let r,f = arg1 "xzr" (fun s -> "^o"^s) r in
+        let memo =
+          sprintf "mrs %s,%s" f (Misc.lowercase (pp_sysreg sr)) in
+        {empty_ins with
+         memo; outputs=r; reg_env=add_type quad r;}::k
 
     let no_tr lbl = lbl
     let branch_neq r i lab k = cmpk V32 r i::bcc no_tr NE lab::k
