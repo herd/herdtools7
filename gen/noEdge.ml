cf2af246c9eb62f2adbb2aa64142728f8aefc32c
Luc Maranget <Luc.Maranget@inria.fr>
2019-07-03 15:35:31 +0200

    New "Noedge" file

---
+++ gen/noEdge.ml
@@ -0,0 +1,24 @@
+(****************************************************************************)
+(*                           the diy toolsuite                              *)
+(*                                                                          *)
+(* Jade Alglave, University College London, UK.                             *)
+(* Luc Maranget, INRIA Paris-Rocquencourt, France.                          *)
+(*                                                                          *)
+(* Copyright 2019-present Institut National de Recherche en Informatique et *)
+(* en Automatique and the authors. All rights reserved.                     *)
+(*                                                                          *)
+(* This software is governed by the CeCILL-B license under French law and   *)
+(* abiding by the rules of distribution of free software. You can use,      *)
+(* modify and/ or redistribute the software under the terms of the CeCILL-B *)
+(* license as circulated by CEA, CNRS and INRIA at the following URL        *)
+(* "http://www.cecill.info". We also give a copy in LICENSE.txt.            *)
+(****************************************************************************)
+
+type arch_edge
+
+let pp_arch_edge _ = assert false
+let dir_tgt _ = assert false
+let dir_src _ = assert false
+let loc_sd _ = assert false
+let get_ie _ = assert false
+let fold_edge _ r = r
