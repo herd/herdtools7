f5bf04ec15965e2f68ceba98e898554bed6b5ccc
Antoine Hacquard <antoine.hacquard@epita.fr>
2019-09-10 16:42:45 +0200

    [litmus] Litmus files of X86_64 architecture

---
+++ litmus/X86_64Arch_litmus.ml
@@ -0,0 +1,61 @@
+(****************************************************************************)
+(*                           the diy toolsuite                              *)
+(*                                                                          *)
+(* Jade Alglave, University College London, UK.                             *)
+(* Luc Maranget, INRIA Paris-Rocquencourt, France.                          *)
+(*                                                                          *)
+(* Copyright 2010-present Institut National de Recherche en Informatique et *)
+(* en Automatique and the authors. All rights reserved.                     *)
+(*                                                                          *)
+(* This software is governed by the CeCILL-B license under French law and   *)
+(* abiding by the rules of distribution of free software. You can use,      *)
+(* modify and/ or redistribute the software under the terms of the CeCILL-B *)
+(* license as circulated by CEA, CNRS and INRIA at the following URL        *)
+(* "http://www.cecill.info". We also give a copy in LICENSE.txt.            *)
+(****************************************************************************)
+
+open Printf
+
+let comment = "#"
+
+module Make(O:Arch_litmus.Config)(V:Constant.S) = struct
+  include X86_64Base
+  module V = V
+
+  let reg_to_string r = match r with
+    | RIP -> "%rip"
+(* We always want the 64 bits version of the register because the smaller version are created by GCC*)
+    | Ireg (r, t) -> "%" ^ X86_64Base.reg64_string r
+    | Internal i -> sprintf "i%i" i
+    | _ -> assert false
+
+  include
+    ArchExtra_litmus.Make(O)
+      (struct
+        module V = V
+
+        type arch_reg = reg
+        let arch = `X86_64
+        let forbidden_regs = []
+        let pp_reg = pp_reg
+        let reg_compare = reg_compare
+        let reg_to_string = reg_to_string
+        let internal_init r =
+          if reg_compare r loop_idx = 0 then Some ("max_loop","int")
+          else None
+
+        let reg_class = function
+          (* as some instructions have eax as implicit argument,
+             we must allocate our EAX to machine %eax
+          | Ireg RAX -> "=&a"
+          (* esi and edi implicit for MOVSD *)
+          | Ireg RSI -> "=&S"
+          | Ireg RDI -> "=&D" *)
+          | Ireg (_, H) -> "=&Q"
+          | _ -> "=&r"
+        let reg_class_stable r = reg_class r
+        let comment = comment
+        let error _ _ = false
+      end)
+  let nop =  I_NOP
+end
---
+++ litmus/X86_64Compile_litmus.ml
@@ -0,0 +1,235 @@
+(****************************************************************************)
+(*                           the diy toolsuite                              *)
+(*                                                                          *)
+(* Jade Alglave, University College London, UK.                             *)
+(* Luc Maranget, INRIA Paris-Rocquencourt, France.                          *)
+(*                                                                          *)
+(* Copyright 2010-present Institut National de Recherche en Informatique et *)
+(* en Automatique and the authors. All rights reserved.                     *)
+(*                                                                          *)
+(* This software is governed by the CeCILL-B license under French law and   *)
+(* abiding by the rules of distribution of free software. You can use,      *)
+(* modify and/ or redistribute the software under the terms of the CeCILL-B *)
+(* license as circulated by CEA, CNRS and INRIA at the following URL        *)
+(* "http://www.cecill.info". We also give a copy in LICENSE.txt.            *)
+(****************************************************************************)
+
+module Make(V:Constant.S)(O:Arch_litmus.Config) =
+  struct
+    module A = X86_64Arch_litmus.Make(O)(V)
+    open A
+    open A.Out
+    open Printf
+
+    let is_ret _ = assert false
+
+(* Not so nice..., the price of code sharing of
+   symbConst.ml with memevents *)
+
+    let abs_to_string abs =  ParsedConstant.vToName abs
+
+(***************************************************)
+(* Extract explicit [symbolic] addresses from code *)
+(***************************************************)
+    let internal_addr name = name = sig_cell
+
+    let extract_rm64 r = match r with
+    |  Rm64_reg _
+    |  Rm64_deref _ -> StringSet.empty
+    |  Rm64_abs v ->
+        let name = abs_to_string v in
+        if internal_addr name then
+          StringSet.empty
+        else
+          StringSet.singleton name
+
+    let extract_ea ea = match ea with
+    | Effaddr_rm64 r -> extract_rm64 r
+
+    let extract_op op = match op with
+    | Operand_effaddr ea -> extract_ea ea
+    | Operand_immediate _ -> StringSet.empty
+
+    let rec extract_addrs ins = match ins with
+      | I_EFF_OP (_, _, ea, op)
+        ->  StringSet.union (extract_ea ea) (extract_op op)
+      | I_NOP | I_JMP _ | I_MFENCE | I_JCC _
+        -> StringSet.empty
+
+    let stable_regs _ins = A.RegSet.empty
+
+(****************************)
+(* compilation to templates *)
+(****************************)
+
+
+
+    let compile_reg_move = function
+      | RIP -> sprintf "%%[rip]"
+      | Ireg (r, t) -> sprintf "%%%s[%s]" (reg_size_to_string t) (reg64_string r)
+      | Symbolic_reg s -> s
+      | Internal i -> sprintf "%i" i
+
+    let compile_rm64_move i o r =  match r with
+    |  Rm64_reg reg -> compile_reg_move reg,(i,[]),(o+1,[reg])
+    |  Rm64_deref reg -> "(" ^ compile_reg_move reg ^ ")",(i+1,[reg]),(o,[])
+    |  Rm64_abs abs ->
+        (let name = abs_to_string abs in
+        if internal_addr name then name
+        else sprintf "%%[%s]" name),
+        (i,[]),(o,[])
+
+    let compile_ea_move i o ea = match ea with
+    | Effaddr_rm64 r -> compile_rm64_move i o r
+
+
+    let compile_rm64_output i o r =  match r with
+    |  Rm64_reg reg -> compile_reg_move reg,(i,[]),(o+1,[reg])
+    |  Rm64_deref reg -> "(" ^ compile_reg_move reg ^ ")",(i+1,[reg]),(o,[])
+    |  Rm64_abs abs ->
+        let name = abs_to_string abs in
+        sprintf "%%[%s]" name,(i,[]),(o,[])
+
+    let compile_ea_output i o ea = match ea with
+    | Effaddr_rm64 r -> compile_rm64_output i o r
+
+
+    let compile_rm64_input i r = match r with
+    |  Rm64_reg reg -> "" ^ compile_reg_move reg,(i+1,[reg])
+    |  Rm64_deref reg -> "(" ^ compile_reg_move reg ^ ")",(i+1,[reg])
+    |  Rm64_abs abs ->
+        let name = abs_to_string abs in
+        sprintf "%%[%s]" name,(i,[])
+
+    let compile_ea_input i ea = match ea with
+    | Effaddr_rm64 r -> compile_rm64_input i r
+
+
+    let compile_op i op = match op with
+    | Operand_immediate x -> sprintf "$%i" x,(i,[])
+    | Operand_effaddr ea -> compile_ea_input i ea
+
+
+    let op_ea_output_op memo ea op =
+      let op,(i,ins1) = compile_op 0 op in
+      let ea,(_,ins2),(_,outs) = compile_ea_output i 0 ea in
+      { empty_ins with
+        memo = sprintf "%s %s,%s" memo op ea;
+        inputs = ins1@ins2;
+        outputs = outs; }
+
+    let op_ea_input_op memo ea op =
+      let op,(i,ins1) = compile_op 0 op in
+      let ea,(_,ins2) = compile_ea_input i ea in
+      { empty_ins with
+        memo = sprintf "%s %s,%s" memo op ea;
+        inputs = ins1@ins2;
+        outputs = [] ; }
+
+    let move memo ea op =
+       let op,(i,ins1) = compile_op 0 op in
+       let ea,(_,ins2),(_,outs2) = compile_ea_move i 0 ea in
+       { empty_ins with
+         memo = sprintf "%s %s,%s" memo op ea;
+         inputs = ins1@ins2;
+         outputs=outs2; }
+
+    let move_addr a i =
+      move "movl"
+        (Effaddr_rm64 (Rm64_abs (ParsedConstant.nameToV a)))
+        (Operand_immediate i)
+
+    let op_ea_ea memo ea1 ea2 =
+      let ea1,(i,ins1),(o,outs1) = compile_ea_output 0 0 ea1 in
+      let ea2,(_,ins2),(_,outs2) = compile_ea_output i o ea2 in
+(* For exchange, order of operands is irrelevant,
+   let us swap them anyway... good idea since operands
+   as asymmetric in practice (ea2 must be a reg) *)
+      { empty_ins with
+        memo = sprintf "%s %s,%s" memo ea2 ea1;
+        inputs = ins1@ins2;
+        outputs = outs1@outs2; }
+
+    let op_ea memo ea =
+      let ea,(_,ins),(_,outs) = compile_ea_output 0 0 ea in
+      { empty_ins with
+        memo = sprintf "%s %s" memo ea ;
+        inputs = ins ;
+        outputs = outs ; }
+
+    let op_none memo =
+      { empty_ins with
+        memo = memo;
+        inputs=[];
+        outputs=[]; }
+
+    let emit_lbl lbl =
+      { empty_ins with
+        memo=sprintf "%s:" (A.Out.dump_label lbl) ;
+        label = Some lbl ; branch=[Next] ; }
+
+    let next_label () = Label.next_label "__L"
+
+    let cmp r i =
+      op_ea_input_op "cmpl"
+        (Effaddr_rm64 (Rm64_reg r)) (Operand_immediate i)
+
+    let jcc tr_lab cond lbl = 
+      {empty_ins with
+       memo =
+       sprintf "j%s %s"
+         (Misc.lowercase (pp_condition cond))
+         (A.Out.dump_label (tr_lab lbl)) ;
+       label=None ; branch=[Next ; Branch lbl]; }
+
+    let jmp tr_lab lbl =
+      { empty_ins with
+       memo = sprintf "jmp %s" (A.Out.dump_label (tr_lab lbl)) ;
+       label=None ; branch=[Branch lbl]; }
+
+    let no_tr lbl = lbl
+
+    let emit_loop _k = assert false
+
+    let inst_string inst size = String.lowercase_ascii (pp_inst_eff_op inst size)
+
+    let rec do_compile_ins tr_lab ins = match ins with
+   | I_NOP ->
+        { empty_ins with memo = "nop"; }
+    | I_EFF_OP (inst, size, ea, op) ->
+       begin
+         match inst with
+         | I_OR | I_ADD | I_XOR
+           -> op_ea_output_op (inst_string inst size) ea op
+         (* as usual, move is quite special *)
+         | I_MOV -> move (inst_string inst size) ea op
+         (* Trap!! ea is input only... *)
+         | I_CMP
+           -> op_ea_input_op (inst_string inst size) ea op
+       end
+    | I_JMP lbl -> jmp tr_lab lbl
+    | I_JCC (cond, lbl) -> jcc tr_lab cond lbl
+    | I_MFENCE -> op_none "mfence"
+
+    let debug_regs chan rs =
+      fprintf chan "%s"
+        (String.concat "," (List.map reg_to_string rs))
+
+    let debug chan t =
+      fprintf chan "memo={%s}, inputs={%a}, output={%a}\n"
+        t.memo
+        debug_regs t.inputs
+        debug_regs t.outputs
+
+    let compile_ins tr_lab ins k =
+      let r = do_compile_ins tr_lab ins in
+      debug stderr r ; 
+      r::k
+
+    let branch_neq r i lab k = Warn.fatal "Not implemented"
+    let branch_eq r i lab k = Warn.fatal "Not implemented"
+
+    let signaling_write i k = move_addr sig_cell i::k
+
+    let emit_tb_wait _ = Warn.fatal "no time base for X86"
+end
