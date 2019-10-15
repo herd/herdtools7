126e934d6768f8f3c1c2f076974e114a6df8c108
Luc Maranget <Luc.Maranget@inria.fr>
2019-10-15 17:13:11 +0200

    www: Add aarch64-mixed book in catalogue

---
+++ catalogue/aarch64-mixed/shelf.py
@@ -0,0 +1,16 @@
+record = "AArch64"
+
+cats = [
+    "cats/aarch64-v02.cat",
+    "cats/aarch64-v01.cat",
+    ]
+
+cfgs = [
+    "cfgs/web-mixed.cfg",
+]
+
+illustrative_tests = [    
+    "tests/MP-Koeln.litmus",
+    "tests/LB+dmb.sy+data-wsi-wsi+MIXED+H.litmus",
+    "tests/MP+HAmo+BAcqAmo.litmus",
+]
