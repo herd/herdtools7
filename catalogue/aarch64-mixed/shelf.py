3a510162ae3a9c7b486032fd8666abe0610ab18d
Luc Maranget <Luc.Maranget@inria.fr>
2019-10-16 17:04:32 +0200

    www: More aarch64 mixed-size tests

--- catalogue/aarch64-mixed/shelf.py
+++ catalogue/aarch64-mixed/shelf.py
@@ -13,4 +13,9 @@ illustrative_tests = [
     "tests/MP-Koeln.litmus",
     "tests/LB+dmb.sy+data-wsi-wsi+MIXED+H.litmus",
     "tests/MP+HAmo+BAcqAmo.litmus",
+    "tests/CO-MIXED-20cc+H.litmus",
+    "tests/CoRW2+posh0h0+h0.litmus",
+    "tests/CoRW2+posb0b0+b0.litmus",
+    "tests/2+2W+posb0b0+posb1b1.litmus",
+    "tests/MP+dmb.syb0b1+datab1b1-rfib1h0.litmus",
 ]
