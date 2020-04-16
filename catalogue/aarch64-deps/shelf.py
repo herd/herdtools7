9ab38c0e4ab6757f40888248185219bef0293cf1
Luc Maranget <Luc.Maranget@inria.fr>
2020-04-16 13:55:45 +0200

    [herd-www] Add 'aarch64-deps' book, At the moment as a branch (dev-deps).

---
+++ catalogue/aarch64-deps/shelf.py
@@ -0,0 +1,58 @@
+record = "AArch64"
+
+cats = [
+    "cats/aarch64-deps.cat",
+    "cats/sc.cat",
+]
+
+cfgs = [
+    "cfgs/web-deps.cfg",
+]
+
+illustrative_tests = [
+    "tests/H10bX.litmus",
+    "tests/H10X.litmus",
+    "tests/H11bQ.litmus",
+    "tests/H11V.litmus",
+    "tests/H12V.litmus",
+    "tests/H1CX.litmus",
+    "tests/H1X.litmus",
+    "tests/H2CV.litmus",
+    "tests/H2V.litmus",
+    "tests/H3bCQ.litmus",
+    "tests/H3bQ.litmus",
+    "tests/H3CV.litmus",
+    "tests/H3V.litmus",
+    "tests/H4CX.litmus",
+    "tests/H4X.litmus",
+    "tests/H5CV.litmus",
+    "tests/H5V.litmus",
+    "tests/H6CX.litmus",
+    "tests/H6X.litmus",
+    "tests/H7bCQ.litmus",
+    "tests/H7bQ.litmus",
+    "tests/H7CV.litmus",
+    "tests/H7V.litmus",
+    "tests/H8bCQ.litmus",
+    "tests/H8bQ.litmus",
+    "tests/H8cQ.litmus",
+    "tests/H8CX.litmus",
+    "tests/H8X.litmus",
+    "tests/H9CV.litmus",
+    "tests/H9V.litmus",
+    "tests/LB6b2.litmus",
+    "tests/LB6b3.litmus",
+    "tests/LB6b.litmus",
+    "tests/mp-stxr-fail.litmus",
+    "tests/mp-stxr.litmus",
+    "tests/mp-stxr-success.litmus",
+    "tests/PPOCA4b.litmus",
+    "tests/PPOCA5b.litmus",
+    "tests/PPOCA6b.litmus",
+    "tests/PPOCA6c.litmus",
+    "tests/PPOCA.litmus",
+    "tests/STXR-ctrl1.litmus",
+    "tests/STXR-ctrl2.litmus",
+    "tests/STXR-ctrl3.litmus",
+    "tests/STXR-ctrl.litmus",
+]
