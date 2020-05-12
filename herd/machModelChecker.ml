herd/herdtools7

commit adb060096fc9227165b1733ed4db965ab4d6a499

[herd] Compute lxsx (lr/sc pairs) and amo (atomic rmw instructions) from rmw.

herd/libdir/stdlib.cat
@@ -28,6 +28,10 @@ let fencerel(B) = (po & (_ * B)) ;  po
 (* Control+cfence *)
 let ctrlcfence(CFENCE) = (ctrl & (_ * CFENCE)) ; po
 
+(* Make the difference between load-reserve/store conditional and amo insructions *)
+let lxsx = rmw & po
+let amo = rmw \ lxsx
+
 (* Some utilities (cpp11) *)
 
 let imply (A, B) = ~A | B

herd/machModelChecker.ml
@@ -205,7 +205,6 @@ module Make
                   (fun e1 e2 -> not (E.same_proc e1 e2)) (Lazy.force unv)
               end ;
               "rmw",lazy conc.S.atomic_load_store;
-              "amo",lazy (Lazy.force pr).S.amo;
               "po", lazy  po;
               "addr", lazy (Lazy.force pr).S.addr;
               "data", lazy (Lazy.force pr).S.data;

herd/modelUtils.ml
@@ -108,15 +108,11 @@ module Make(O:Model.Config) (S:SemExtra.S) = struct
         S.seq r1 r2
       with Misc.NoIsync -> S.E.EventRel.empty in
     let rf = U.make_rf conc in
-    let amo =
-      E.EventRel.filter
-        (fun (r,w) -> E.po_eq r w)
-        conc.S.atomic_load_store in
     { S.addr=addr_dep; data=data_dep; ctrl=ctrl_dep; depend=dd_pre;
       ctrlisync;
       data_commit;
       success;
-      rf; amo;}
+      rf;}
 
   let pp_procrels pp_isync pr =
     let pp =  ["data",pr.S.data; "addr",pr.S.addr;] in

herd/semExtra.ml
@@ -98,8 +98,7 @@ module type S = sig
         ctrl : event_rel;
         ctrlisync : event_rel;
         success : event_rel;
-        rf : event_rel;
-        amo : event_rel; }
+        rf : event_rel; }
 
 (*********)
 (* RFMap *)
@@ -256,8 +255,7 @@ module Make(C:Config) (A:Arch_herd.S) (Act:Action.S with module A = A)
         ctrl : event_rel;
         ctrlisync : event_rel;
         success : event_rel;
-        rf : event_rel;
-        amo : event_rel; }
+        rf : event_rel; }
 
 (* Read-From maps exploitation *)
     type write_to =

commit 7c4d6e05305a57707ed26567092b72ddfc3ebcd4

[herd] Correct regression introduced by adb060096fc9227165b1733ed4db965ab4d6a499

herd/libdir/stdlib.cat
@@ -29,8 +29,8 @@ let fencerel(B) = (po & (_ * B)) ;  po
 let ctrlcfence(CFENCE) = (ctrl & (_ * CFENCE)) ; po
 
 (* Make the difference between load-reserve/store conditional and amo insructions *)
-let lxsx = rmw & po
-let amo = rmw \ lxsx
+let lxsx = rmw \ amo
+
 
 (* Some utilities (cpp11) *)
 

herd/machModelChecker.ml
@@ -205,6 +205,12 @@ module Make
                   (fun e1 e2 -> not (E.same_proc e1 e2)) (Lazy.force unv)
               end ;
               "rmw",lazy conc.S.atomic_load_store;
+              "amo",
+              lazy begin
+                E.EventRel.filter
+                  (fun (r,w) -> E.po_eq r w)
+                  conc.S.atomic_load_store
+              end;
               "po", lazy  po;
               "addr", lazy (Lazy.force pr).S.addr;
               "data", lazy (Lazy.force pr).S.data;