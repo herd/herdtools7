herd/herdtools7

commit c73da1c5b7e8c900cb96e3894f363a7d13f646fd

[litmus] New "Align" info, so as to enforce more alignement than default.

litmus/ASMLang.ml
@@ -479,17 +479,29 @@ module RegMap = A.RegMap)
           (fmt_code p) (fmt_code_size p)
           (fmt_prelude p) (fmt_lbl_offset p lbl))
 
-      let compile_addr_call x = sprintf "&_a->%s[_i]" x
+      let indirect_star =
+        let open Memory in
+        match O.memory with
+        | Direct -> ""
+        | Indirect -> "*"
+
+      let compile_addr_call env x =
+        let pp = sprintf "&_a->%s[_i]" x in
+        try
+          let t = List.assoc x env in
+          sprintf "(%s %s*)%s" (CType.dump t) indirect_star pp
+        with Not_found  -> pp
+
       let compile_cpy_addr_call proc x =
         sprintf "&_a->%s[_i]" (Tmpl.addr_cpy_name x proc)
       let compile_out_reg_call proc reg =
         sprintf "&%s" (Tmpl.compile_out_reg proc reg)
 
-      let dump_call f_id _tr_idx chan indent _env _globEnv _volatileEnv proc t =
+      let dump_call f_id _tr_idx chan indent _env alignedEnv _volatileEnv proc t =
         let labels = List.map compile_label_call (Tmpl.get_labels t) in
         let addrs_proc = Tmpl.get_addrs t in
-        let addrs = List.map compile_addr_call addrs_proc
-        and addrs_cpy =
+        let addrs = List.map (compile_addr_call alignedEnv) addrs_proc in
+        let addrs_cpy =
           if O.memory = Memory.Indirect && O.cautious then
             List.map (compile_cpy_addr_call proc) addrs_proc
           else []

lib/CType.ml
@@ -134,3 +134,5 @@ let same_base t0 t1 = match t0,t1 with
     | _,_ -> false
     end
 | _,_ -> false
+
+let type_for_align i = Array ("uint8_t",i)

lib/miscParser.ml
@@ -186,6 +186,7 @@ let mach2generic parser lexer buff =
 (* Info keys *)
 let hash_key =  "Hash"
 and stable_key = "Stable"
+and align_key = "Align"
 
 (* get hash from info fields *)
 

lib/miscParser.mli
@@ -112,6 +112,7 @@ val mach2generic :
 (* Info keys *)
 val hash_key : string
 val stable_key : string
+val align_key : string
 
 (* Extract hash *)
 val get_hash : ('i, 'p, 'c, 'loc) result -> string option

litmus/CCompile_litmus.ml
@@ -186,7 +186,7 @@ module Make
         flocs = List.map fst locs;
         global_code = get_global_code code;
         src = t;
-        type_env = env ;
+        type_env = env,StringMap.empty;
       }
 
   end

litmus/compile.ml
@@ -605,7 +605,16 @@ type P.code = MiscParser.proc * A.pseudo list)
           } = t in
       let initenv = List.map (fun (loc,(_,v)) -> loc,v) init in
       let observed = Generic.all_observed final filter locs in
-      let ty_env = Generic.build_type_env init final filter locs in
+      let ty_env1 = Generic.build_type_env init final filter locs
+      and ty_env2 =
+        try
+          let ps = List.assoc MiscParser.align_key info in
+          List.fold_left
+            (fun m (x,i) ->
+              StringMap.add x (CType.type_for_align i) m)
+            StringMap.empty (InfoAlign.parse ps)
+        with Not_found -> StringMap.empty in
+      let ty_env = ty_env1,ty_env2 in
       let code = List.map (fun ((p,_),c) -> p,c) code in
       let code =
         if do_self then
@@ -622,14 +631,14 @@ type P.code = MiscParser.proc * A.pseudo list)
               | Some r -> r::k)
               [] rs in
           A.RegSet.of_list rs in
-      let code = mk_templates ty_env name stable_info initenv code observed in
-      let code_typed = type_outs ty_env code in
+      let code = mk_templates ty_env1 name stable_info initenv code observed in
+      let code_typed = type_outs ty_env1 code in
         { T.init = initenv ;
           info = info;
           code = code_typed;
           condition = final;
           filter = filter;
-          globals = comp_globals ty_env init code;
+          globals = comp_globals ty_env1 init code;
           flocs = List.map fst locs ;
           global_code = [];
           src = t;

litmus/dune
@@ -1,5 +1,5 @@
 (rule (copy ../Version.ml Version.ml))
-(ocamllex lexAffinity lexConf_litmus lexFmt lexHaveRcu prefetch showcode lexO)
+(ocamllex lexAffinity lexConf_litmus lexFmt lexHaveRcu prefetch showcode lexO infoAlign)
 (executables
    (names
       litmus klitmus

commit 8a65345193e72c4275c1707b61414aaa186dae1c

[litmus] Correct regression introduced by c73da1c5b7e8.    At the occasion group 'global' environement arguments to Lang modules as a record
Reported-by: Akira Yokosawa <akiyks@gmail.com>

litmus/ASMLang.ml
@@ -46,7 +46,17 @@ module RegMap = A.RegMap)
     = struct
 
       type arch_reg = Tmpl.arch_reg
+
+      module RegSet = Tmpl.RegSet
+      module RegMap = Tmpl.RegMap
+
       type t = Tmpl.t
+
+      type glob_t = {
+          global : (string * CType.t) list ;
+          aligned : (string * CType.t) list ;
+          volatile : string list ;
+        }
 
       open Printf
 
@@ -61,9 +71,6 @@ module RegMap = A.RegMap)
       and compile_val_inline = match O.mode with
       | Mode.Std -> checkVal Tmpl.dump_v
       | Mode.PreSi -> checkVal A.V.pp_v
-
-      module RegSet = Tmpl.RegSet
-      module RegMap = Tmpl.RegMap
 
       let dump_clobbers chan t =
         fprintf chan ":%s\n"
@@ -387,9 +394,9 @@ module RegMap = A.RegMap)
           (String.concat " " pp)
 
 
-      let dump chan indent env globEnv _volatileEnv proc t =
+      let dump chan indent env glob proc t =
 
-        if debug then debug_globEnv globEnv ;
+        if debug then debug_globEnv glob.global ;
 
         let compile_out_reg = match O.mode with
         | Mode.Std -> Tmpl.compile_out_reg
@@ -424,8 +431,8 @@ module RegMap = A.RegMap)
 
       let compile_cpy_fun proc a = sprintf "*%s" (Tmpl.addr_cpy_name a proc)
 
-      let dump_fun chan env globEnv _volatileEnv proc t =
-        if debug then debug_globEnv globEnv ;
+      let dump_fun chan env glob proc t =
+        if debug then debug_globEnv glob.global ;
         let labels = Tmpl.get_labels t in
         let labels =
           List.map
@@ -436,7 +443,7 @@ module RegMap = A.RegMap)
           List.map
             (fun x ->
               let ty =
-                try List.assoc x globEnv
+                try List.assoc x glob.global
                 with Not_found -> Compile.base in
               let ty = SkelUtil.dump_global_type x ty in
               match O.memory with
@@ -450,7 +457,7 @@ module RegMap = A.RegMap)
             List.map
               (fun x ->
                 let ty =
-                  try List.assoc x globEnv
+                  try List.assoc x glob.global
                   with Not_found -> assert false in
                 sprintf "%s **%s"
                   (CType.dump ty)
@@ -500,10 +507,10 @@ module RegMap = A.RegMap)
       let compile_out_reg_call proc reg =
         sprintf "&%s" (Tmpl.compile_out_reg proc reg)
 
-      let dump_call f_id _tr_idx chan indent _env alignedEnv _volatileEnv proc t =
+      let dump_call f_id _tr_idx chan indent _env glob proc t =
         let labels = List.map compile_label_call (Tmpl.get_labels t) in
         let addrs_proc = Tmpl.get_addrs t in
-        let addrs = List.map (compile_addr_call alignedEnv) addrs_proc in
+        let addrs = List.map (compile_addr_call glob.aligned) addrs_proc in
         let addrs_cpy =
           if O.memory = Memory.Indirect && O.cautious then
             List.map (compile_cpy_addr_call proc) addrs_proc

litmus/CLang.ml
@@ -44,6 +44,12 @@ module Make(C:Config)(E:Extra) = struct
 
   type t = CTarget.t
 
+  type glob_t = {
+      global : (string * CType.t) list ;
+      aligned : (string * CType.t) list ;
+      volatile : string list ;
+    }
+
   let dump_start chan indent proc =
     if not E.simple then begin
       if C.asmcommentaslabel then
@@ -82,7 +88,7 @@ module Make(C:Config)(E:Extra) = struct
     and ty = out_type env x in
     sprintf "%s*" (CType.dump ty),outname
 
-  let dump_fun chan env globEnv _envVolatile proc t =
+  let dump_fun chan env glob proc t =
 (*
   let pp_env =
   String.concat "; "
@@ -93,7 +99,7 @@ module Make(C:Config)(E:Extra) = struct
  *)
     let out fmt = fprintf chan fmt in
     let input_defs =
-      List.map (dump_global_def globEnv) t.CTarget.inputs
+      List.map (dump_global_def glob.global) t.CTarget.inputs
     and output_defs =
       List.map (dump_output_def env proc) t.CTarget.finals in
     let defs = input_defs@output_defs in
@@ -118,10 +124,17 @@ module Make(C:Config)(E:Extra) = struct
       t.CTarget.finals ;
     out "}\n\n"
 
+  let get_amper x env =
+    try
+      begin match List.assoc x env  with
+      | CType.Base "mtx_t" -> ""
+      | _ -> "&"
+      end
+    with Not_found -> "&"
 
-  let dump_call f_id tr_idx chan indent _env globEnv _envVolatile proc t =
+  let dump_call f_id tr_idx chan indent _env glob proc t =
     let is_array_of a =
-      try  match List.assoc a globEnv with
+      try  match List.assoc a glob.global with
       | CType.Array (t,_) -> Some t
       | _ -> None
       with Not_found -> None in
@@ -132,9 +145,7 @@ module Make(C:Config)(E:Extra) = struct
           let cast = match is_array_of x with
           | Some t -> sprintf "(%s *)" t
           | None -> "" in
-          let amper = match List.assoc x globEnv with
-          | CType.Base "mtx_t" -> ""
-          | _ -> "&" in
+          let amper = get_amper x glob.global in
           match C.memory with
          | Memory.Direct ->
              sprintf "%s%s_a->%s[%s]" cast amper (CTarget.fmt_reg x) idx
@@ -148,17 +159,14 @@ module Make(C:Config)(E:Extra) = struct
           let args = String.concat "," (global_args@out_args) in
           LangUtils.dump_code_call chan indent f_id args
 
-
-  let dump chan indent env globEnv _envVolatile proc t =
+  let dump chan indent env glob proc t =
     let out x = fprintf chan x in
     out "%sdo {\n" indent;
     begin
       let indent = "  " ^ indent in
       let dump_input x =
-        let ty,x = dump_global_def globEnv x in
-        let amper = match List.assoc x globEnv with
-        | CType.Base "mtx_t" -> ""
-        | _ -> "&" in
+        let ty,x = dump_global_def glob.global x in
+        let amper = get_amper x glob.global in
         match C.memory with
         | Memory.Direct -> begin match C.mode with
           | Mode.Std ->

litmus/KSkel.ml
@@ -510,14 +510,20 @@ let dump_threads _tname env test =
   O.o "/***************/" ;
   O.o "" ;
   let global_env = U.select_global env in
-  let global_env =
-    List.map
-      (fun (x,ty) -> x,CType.strip_volatile ty)
-      global_env in
+  let glob =
+    let global =
+      List.map (fun (x,ty) -> x,CType.strip_volatile ty) global_env
+    and aligned =
+      if List.exists (fun (a,_) -> U.is_aligned a env) test.T.globals then
+        Warn.fatal "align feature not available in klitmus" ;
+      [] in
+    let open Lang in
+    { global; aligned; volatile=[]; } in
   List.iter
     (fun (proc,(out,(_outregs,envVolatile))) ->
       let myenv = U.select_proc proc env in
-      Lang.dump_fun O.out myenv global_env envVolatile proc out ;
+      let glob = { glob with Lang.volatile=envVolatile; } in
+      Lang.dump_fun O.out myenv glob proc out ;
       O.f "static int thread%i(void *_p) {" proc ;
       O.oi "ctx_t *_a = (ctx_t *)_p;" ;
       O.o "" ;
@@ -537,7 +543,7 @@ let dump_threads _tname env test =
       | Some _|None -> idx in
       Lang.dump_call (LangUtils.code_fun proc)
         tr_idx O.out (Indent.as_string indent3)
-        myenv global_env envVolatile proc out ;
+        myenv glob proc out ;
       O.oii "}" ;
       O.oi "}" ;
       O.oi "atomic_inc(&done);" ;

litmus/LISALang.ml
@@ -21,14 +21,19 @@ module Make(V:Constant.S) = struct
   module A = LISAArch_litmus.Make(V)
 
   type arch_reg = A.reg
+  module RegSet = A.Out.RegSet
+  module RegMap = A.Out.RegMap
   type t = A.Out.t
+
+  type glob_t = {
+      global : (string * CType.t) list ;
+      aligned : (string * CType.t) list ;
+      volatile : string list ;
+    }
 
   module Tmpl = A.Out
 
   let checkVal f v = f v
-
-  module RegSet = A.Out.RegSet
-  module RegMap = A.Out.RegMap
 
   let debug = false
 
@@ -98,13 +103,13 @@ module Make(V:Constant.S) = struct
 
   and compile_out_reg_fun p r = sprintf "*%s" (Tmpl.dump_out_reg p r)
 
-  let dump_fun chan env globEnv _volatileEnv proc t =
+  let dump_fun chan env glob proc t =
     let addrs_proc = A.Out.get_addrs t in
     let addrs =
       List.map
         (fun x ->
           let ty =
-            try List.assoc x globEnv
+            try List.assoc x glob.global
             with Not_found -> Compile.base in
           let ty = SkelUtil.dump_global_type x ty in
           sprintf "%s *%s" ty x)
@@ -137,7 +142,7 @@ module Make(V:Constant.S) = struct
   let compile_out_reg_call proc reg =
     sprintf "&_a->%s" (Tmpl.compile_out_reg proc reg)
 
-  let dump_call f_id _tr_idx chan indent _env _globEnv _volatileEnv proc t =
+  let dump_call f_id _tr_idx chan indent _env _glob proc t =
     let addrs_proc = Tmpl.get_addrs t in
     let addrs = List.map compile_addr_call addrs_proc
     and outs = List.map (compile_out_reg_call proc) t.Tmpl.final in
@@ -145,6 +150,6 @@ module Make(V:Constant.S) = struct
     LangUtils.dump_code_call chan indent f_id args
 
 
-  let dump _chan _indent _env _globEnv _volatileEnv _proc _t = ()
+  let dump _chan _indent _env _glob _proc _t = ()
 
 end

litmus/language.ml
@@ -19,12 +19,18 @@ module type S = sig
   module RegMap : MyMap.S with type key = arch_reg
   type t
 
+  type glob_t = {
+      global : (string * CType.t) list ;  (* Types of global locations *)
+      aligned : (string * CType.t) list ; (* Hidden, implementation, type to force alignment *) 
+      volatile : string list ;
+    }
+
 (* Function dump *)
+
   val dump_fun :
     out_channel ->
     CType.t RegMap.t ->
-    (string * CType.t) list ->
-    string list ->
+    glob_t ->
     int ->
     t ->
     unit
@@ -35,8 +41,7 @@ module type S = sig
     out_channel ->
     string ->
     CType.t RegMap.t ->
-    (string * CType.t) list ->
-    string list ->
+    glob_t ->
     int ->
     t ->
     unit
@@ -46,8 +51,7 @@ module type S = sig
     out_channel ->
     string ->
     CType.t RegMap.t ->
-    (string * CType.t) list ->
-    string list ->
+    glob_t ->
     int ->
     t ->
     unit

litmus/preSi.ml
@@ -715,7 +715,7 @@ module Make
 
 
       let dump_run_thread
-          env test _some_ptr stats global_env
+          env test _some_ptr stats glob
           (_vars,inits) (proc,(out,(_outregs,envVolatile)))  =
         let my_regs = U.select_proc proc env in
         let addrs = A.Out.get_addrs out in (* accessed in code *)
@@ -774,7 +774,7 @@ module Make
         (* Dump code *)
         Lang.dump
           O.out (Indent.as_string Indent.indent2)
-          my_regs global_env envVolatile proc out ;
+          my_regs { glob with Lang.volatile=envVolatile; } proc out ;
         O.oii "barrier_wait(_b);" ;
 (* Collect shared locations final values, if appropriate *)
         let globs = U.get_displayed_globals test in
@@ -874,9 +874,20 @@ module Make
         end ;
         O.oi "barrier_wait(_b);" ;
         O.oi "switch (_role) {" ;
-        let global_env = U.select_global env in
+        let glob  =
+          let global = U.select_global env
+          and aligned =
+            if 
+              List.exists
+                (fun (a,_) -> U.is_aligned a env)
+                test.T.globals
+            then
+              Warn.fatal "align feature not implemented in presi mode";
+            [] in
+          let open Lang in
+          { global; aligned; volatile=[]; } in
         List.iter2
-          (dump_run_thread env test some_ptr stats global_env)
+          (dump_run_thread env test some_ptr stats glob)
           (part_vars test)
           test.T.code ;
         O.oi "}" ;

litmus/skel.ml
@@ -1721,16 +1721,17 @@ module Make
             | _ ->  ())
             test.T.src.MiscParser.init
         end ;
-        let aligned_env =
-          List.filter
-            (fun (a,_) -> U.is_aligned a env)
-            test.T.globals in
+        let aligned = List.filter (fun (a,_) -> U.is_aligned a env) test.T.globals
+        and global = U.select_global env in
+        let glob =
+          let open Lang in
+          { global; aligned; volatile=[]; } in
         List.iter
           (fun (proc,(out,(outregs,envVolatile))) ->
-            let myenv = U.select_proc proc env
-            and global_env = U.select_global env in
+            let myenv = U.select_proc proc env in
+            let glob = { glob with Lang.volatile=envVolatile; } in
             if do_ascall then begin
-              Lang.dump_fun O.out myenv global_env envVolatile proc out
+              Lang.dump_fun O.out myenv glob proc out
             end ;
             let  do_collect =  do_collect_local && (do_safer || proc=0) in
             O.f "static void *P%i(void *_vb) {" proc ;
@@ -1937,7 +1938,7 @@ module Make
                 if do_self then LangUtils.code_fun_cpy proc else
                 LangUtils.code_fun proc in
               Lang.dump_call f_id (fun _ s -> s) else Lang.dump)
-              O.out (Indent.as_string iloop) myenv aligned_env envVolatile proc out ;
+              O.out (Indent.as_string iloop) myenv glob proc out ;
             if do_verbose_barrier && have_timebase  then begin
               if do_timebase then begin
                 O.fx iloop "_a->tb_delta[%i][_i] = _delta;" proc ;