2b1428e270c81c5cb8a7fb13c0a42db77955fde8
Luc Maranget <Luc.Maranget@inria.fr>
2020-07-01 19:09:29 +0200

    [www] Add aarch64-MTE (minimal) book.

---
+++ catalogue/aarch64-MTE/shelf.py
@@ -0,0 +1,14 @@
+record = "AArch64"
+
+cats = [
+    "cats/aarch64-MTE.cat",
+    ]
+
+cfgs = [
+    "cfgs/web-MTE.cfg",
+]
+
+illustrative_tests = [    
+    "tests/coRR-MemTag1.litmus",
+    "tests/coRR-MemTag2.litmus",
+]
