814a6fc1610ec1a24f2cbd178e171966375626ac
Jade Alglave <jade.alglave@arm.com>
2020-07-07 13:09:20 +0100

    [www] Adding more AArch64 MTE litmus tests

--- catalogue/aarch64-MTE/shelf.py
+++ catalogue/aarch64-MTE/shelf.py
@@ -11,4 +11,11 @@ cfgs = [
 illustrative_tests = [    
     "tests/coRR-MemTag1.litmus",
     "tests/coRR-MemTag2.litmus",
+    "tests/MP+dmb.stPT+addr.litmus",
+    "tests/MP+dmb.stTT+addr.litmus",
+    "tests/R+dmb.stTT+rel-acq.litmus",
+    "tests/S+dmb.stTT+addr.litmus",
+    "tests/S+dmb.stTT+ctrl.litmus",
+    "tests/S+dmb.stTT+data.litmus",
+    "tests/S+dmb.stTT+rel.litmus",
 ]
