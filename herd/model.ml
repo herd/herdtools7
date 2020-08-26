b5a625c23b0d295f1a86f46178477b101c8f335d
Luc Maranget <Luc.Maranget@inria.fr>
2020-08-26 21:44:54 +0200

    [herd] Add the atomicity requirement to model x86tso-mixed.cat. Also make this model the default one for arch X86_64.

--- herd/model.ml
+++ herd/model.ml
@@ -109,7 +109,7 @@ match a with
       else "aarch64.cat")
 | `C -> File "c11_partialSC.cat"
 | `RISCV -> File "riscv.cat"
-| `X86_64 -> File "tso.cat"
+| `X86_64 -> File "x86tso-mixed.cat"
 | _ ->
     Warn.user_error 
     "There is no default model for architecture %s.\nSpecify a model explicitly using the -model flag." (Archs.pp a)
