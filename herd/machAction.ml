141dee1409427fca4b3052fd5144f843fb86cd4e
Jade Alglave <jade.alglave@arm.com>
2020-09-17 14:38:54 +0100

    [cat] Rename inv-loc to inv-field

--- herd/machAction.ml
+++ herd/machAction.ml
@@ -460,7 +460,7 @@ end = struct
           | None -> false
           end
       | _ -> false in
-      ["inv-loc",ok_act]
+      ["inv-field",ok_act]
     else []
 
   let is_isync act = match act with
