herd/herdtools7

commit 642cb5967db3ff455d31a9797587d483b4c7a4e3

[all]: LDR(Immediate) post-indexed write + LDR barrel shifters (#69)
This commit upstreams support for LDR (immediate) with post-indexed
write to the address. This commit also upstreams inline barrel shifters
for the LDR instruction.

This commit also upstreams unittests for these examples

Following https://github.com/herd/herdtools7/pull/31

lib/AArch64Parser.mly
@@ -146,18 +146,29 @@ k:
 | NUM  { MetaConst.Int $1 }
 | META { MetaConst.Meta $1 }
 
+k0:
+| { None }
+| COMMA k { Some $2}
+
 kr:
 | k { A.K $1 }
 | xreg { A.RV (A.V64,$1) }
 | wreg  { A.RV (A.V32,$1) }
 
+kr_shift:
+| k                { A.K $1, A.S_NOEXT }
+| xreg             { (A.RV (A.V64,$1)),  A.S_NOEXT }
+| wreg             { (A.RV (A.V32,$1)),  A.S_NOEXT }
+| wreg COMMA shift { (A.RV (A.V32, $1)), $3 }
+| xreg COMMA shift { (A.RV (A.V64, $1)), $3 }
+
 kr0:
+| { A.K (MetaConst.zero), A.S_NOEXT }
+| COMMA kr_shift { $2 }
+
+kr0_no_shift:
 | { A.K (MetaConst.zero) }
 | COMMA k { A.K $2 }
-| COMMA xreg {  A.RV (A.V64,$2) }
-| COMMA wreg {  A.RV (A.V32,$2) }
-| COMMA wreg COMMA SXTW {  A.RV (A.V32,$2) }
-/* For indexed accesses SXTW is considered always present */
 
 kwr:
 | k { A.K $1 }
@@ -220,19 +231,25 @@ instr:
 | TBZ reg COMMA NUM COMMA label_addr
   { let v,r = $2 in A.I_TBZ (v,r,MetaConst.Int $4,$6) }
 /* Memory */
-| LDR reg COMMA LBRK xreg kr0 RBRK
-  { let v,r = $2 in A.I_LDR (v,r,$5,$6) }
-| ldp_instr wreg COMMA wreg COMMA LBRK xreg kr0 RBRK
+/* must differentiate between regular and post-indexed load */
+| LDR reg COMMA LBRK xreg kr0 RBRK k0
+  { let v,r    = $2 in
+    let kr, os = $6 in
+    if Option.is_some $8 && kr = A.K MetaConst.zero then
+      A.I_LDR_P (v,r,$5,Option.get $8)
+    else
+      A.I_LDR (v,r,$5,kr,os) }
+| ldp_instr wreg COMMA wreg COMMA LBRK xreg kr0_no_shift RBRK
   { $1 A.V32 $2 $4 $7 $8 }
-| ldp_instr xreg COMMA xreg COMMA LBRK xreg kr0 RBRK
+| ldp_instr xreg COMMA xreg COMMA LBRK xreg kr0_no_shift RBRK
   { $1 A.V64 $2 $4 $7 $8 }
-| stp_instr wreg COMMA wreg COMMA LBRK xreg kr0 RBRK
+| stp_instr wreg COMMA wreg COMMA LBRK xreg kr0_no_shift RBRK
   { $1 A.V32 $2 $4 $7 $8 }
-| stp_instr xreg COMMA xreg COMMA LBRK xreg kr0 RBRK
+| stp_instr xreg COMMA xreg COMMA LBRK xreg kr0_no_shift RBRK
   { $1 A.V64 $2 $4 $7 $8 }
-| LDRB wreg COMMA LBRK xreg kr0 RBRK
+| LDRB wreg COMMA LBRK xreg kr0_no_shift RBRK
   { A.I_LDRBH (A.B,$2,$5,$6) }
-| LDRH wreg COMMA LBRK xreg kr0 RBRK
+| LDRH wreg COMMA LBRK xreg kr0_no_shift RBRK
   { A.I_LDRBH (A.H,$2,$5,$6) }
 | LDAR reg COMMA LBRK xreg RBRK
   { let v,r = $2 in A.I_LDAR (v,A.AA,r,$5) }
@@ -258,11 +275,11 @@ instr:
   { A.I_LDARBH (A.B,A.AQ,$2,$5) }
 | LDAPRH wreg COMMA LBRK xreg RBRK
   { A.I_LDARBH (A.H,A.AQ,$2,$5) }
-| STR reg COMMA LBRK xreg kr0 RBRK
+| STR reg COMMA LBRK xreg kr0_no_shift RBRK
   { let v,r = $2 in A.I_STR (v,r,$5,$6) }
-| STRB wreg COMMA LBRK xreg kr0 RBRK
+| STRB wreg COMMA LBRK xreg kr0_no_shift RBRK
   { A.I_STRBH (A.B,$2,$5,$6) }
-| STRH wreg COMMA LBRK xreg kr0 RBRK
+| STRH wreg COMMA LBRK xreg kr0_no_shift RBRK
   { A.I_STRBH (A.H,$2,$5,$6) }
 | STLR reg COMMA LBRK xreg RBRK
   { let v,r = $2 in A.I_STLR (v,r,$5) }
@@ -349,11 +366,11 @@ instr:
 | SWPALH wreg COMMA wreg COMMA  LBRK xreg zeroopt RBRK
   { A.I_SWPBH (A.H,A.RMW_AL,$2,$4,$7) }
 /* Memory Tagging */
-| STG xreg COMMA LBRK xreg kr0 RBRK
+| STG xreg COMMA LBRK xreg kr0_no_shift RBRK
   { A.I_STG ($2,$5,$6) }
-| STZG xreg COMMA LBRK xreg kr0 RBRK
+| STZG xreg COMMA LBRK xreg kr0_no_shift RBRK
   { A.I_STZG ($2,$5,$6) }
-| LDG xreg COMMA LBRK xreg kr0 RBRK
+| LDG xreg COMMA LBRK xreg kr0_no_shift RBRK
   { A.I_LDG ($2,$5,$6) }
 
 /* Fetch and ADD */

gen/AArch64Compile_gen.ml
@@ -112,10 +112,10 @@ module Make(Cfg:Config) : XXXCompile_gen.S =
       match sz with
       | Byte -> I_LDRBH (B,r1,r2,K o)
       | Short -> I_LDRBH (H,r1,r2,K o)
-      | Word -> I_LDR (V32,r1,r2,K o)
-      | Quad -> I_LDR (V64,r1,r2,K o)
+      | Word -> I_LDR (V32,r1,r2,K o, S_NOEXT)
+      | Quad -> I_LDR (V64,r1,r2,K o, S_NOEXT)
 
-    let do_ldr v r1 r2 = I_LDR (v,r1,r2,K 0)
+    let do_ldr v r1 r2 = I_LDR (v,r1,r2,K 0, S_NOEXT)
     let ldr = do_ldr vloc
     let ldg r1 r2 = I_LDG (r1,r2,K 0)
     let ldar r1 r2 = I_LDAR (vloc,AA,r1,r2)
@@ -123,7 +123,7 @@ module Make(Cfg:Config) : XXXCompile_gen.S =
     let ldxr r1 r2 = I_LDAR (vloc,XX,r1,r2)
     let ldaxr r1 r2 = I_LDAR (vloc,AX,r1,r2)
     let sxtw r1 r2 = I_SXTW (r1,r2)
-    let do_ldr_idx v r1 r2 idx = I_LDR (v,r1,r2,RV (vloc,idx))
+    let do_ldr_idx v r1 r2 idx = I_LDR (v,r1,r2,RV (vloc,idx), S_NOEXT)
     let ldr_idx = do_ldr_idx vloc
 
 
@@ -132,8 +132,8 @@ module Make(Cfg:Config) : XXXCompile_gen.S =
       match sz with
       | Byte -> I_LDRBH (B,r1,r2,RV (v,idx))
       | Short -> I_LDRBH (H,r1,r2,RV (v,idx))
-      | Word -> I_LDR (V32,r1,r2,RV (v,idx))
-      | Quad -> I_LDR (V64,r1,r2,RV (v,idx))
+      | Word -> I_LDR (V32,r1,r2,RV (v,idx), S_NOEXT)
+      | Quad -> I_LDR (V64,r1,r2,RV (v,idx), S_NOEXT)
 
     let str_mixed sz o r1 r2 =
       let open MachSize in

herd/AArch64Arch_herd.ml
@@ -96,7 +96,8 @@ module Make (C:Arch_herd.Config) (V:Value.S) =
     module V = V
 
     let mem_access_size = function
-      | I_LDR (v,_,_,_) | I_LDP (_,v,_,_,_,_)
+      | I_LDR (v,_,_,_,_) | I_LDP (_,v,_,_,_,_)
+      | I_LDR_P(v,_,_,_)
       | I_STR (v,_,_,_) | I_STLR (v,_,_) | I_STXR (v,_,_,_,_)
       | I_STP (_,v,_,_,_,_)
       | I_CAS (v,_,_,_,_) | I_SWP (v,_,_,_,_)

herd/AArch64Sem.ml
@@ -229,12 +229,37 @@ module Make
 (* Memory instructions *)
 (***********************)
 
-      let get_ea rs kr ii = match kr with
-      | AArch64.K k ->
-          read_reg_ord rs ii >>= fun v -> M.add v (V.intToV k)
-      | AArch64.RV(_,r) ->
-          (read_reg_ord rs ii >>| read_reg_ord r ii) >>= fun (v1,v2) ->
-            M.add v1 v2
+      (* Apply a shift as monadic op *)
+      let shift s =
+        let open AArch64Base in
+        match s with
+          | S_NOEXT   -> M.unitT
+          | S_LSL(n)  -> fun x -> M.op (Op.ShiftLeft) x (V.intToV n)
+          | S_LSR(n)  -> fun x -> M.op (Op.ShiftRight) x (V.intToV n)
+          | S_ASR(n)  -> fun x -> M.op (Op.ASR) x (V.intToV n)
+          | S_SXTW -> fun x ->
+            let m = V.op1 (Op.LeftShift 31) V.one in
+            M.op Op.Xor x m
+            >>= fun v -> M.op Op.Sub v m
+          | S_UXTW->
+            Warn.fatal "UXTW barrel shift not supported yet"
+
+      let get_ea rs kr s ii =
+      let open AArch64Base in
+      match kr, s with
+      | K 0, S_NOEXT -> (* Immediate with no shift*)
+          read_reg_ord rs ii
+      | K k, s -> (* Immediate with offset, with shift *)
+          read_reg_ord rs ii
+          >>= fun v -> shift s (V.intToV k)
+          >>= M.add v
+      | RV(_,r), S_NOEXT -> (* register, no shift *)
+          (read_reg_ord rs ii >>| read_reg_ord r ii)
+          >>= fun (v1,v2) -> M.add v2 v1
+      | RV(_,r), s -> (* register, with shift *)
+          (read_reg_ord rs ii >>| read_reg_ord r ii)
+          >>= fun (v1,v2) -> shift s v2
+          >>= fun v2 -> M.add v1 v2
 
       let lift_memop mop ma ii =
         if memtag then
@@ -255,14 +280,26 @@ module Make
             do_write_mem sz an a v ii)
           ma ii
 
-      let ldr sz rd rs kr ii =
+      let ldr sz rd rs kr s ii =
         lift_memop
           (fun ma -> ma >>= fun a ->
            old_do_read_mem sz AArch64.N a ii >>= fun v ->
-           write_reg_sz_non_mixed sz rd v ii )
-          (get_ea rs kr ii) ii
+           write_reg rd v ii )
+          (get_ea rs kr s ii) ii
 
-      and str sz rs rd kr ii = do_str sz AArch64.N rs (get_ea rd kr ii) ii
+      (* Post-Indexed load immediate *)
+      and ldr_p sz rd rs k ii =
+        assert (k >= -256 && k <= 255);
+        let open AArch64Base in
+          (read_reg_ord rs ii)
+          >>= fun a1 ->
+              (read_mem sz rd a1 ii)
+              >>|
+              (M.add a1 (V.intToV k)
+                >>= fun v -> write_reg rs v ii)
+          >>! B.Next
+
+      and str sz rs rd kr ii = do_str sz AArch64.N rs (get_ea rd kr AArch64.S_NOEXT ii) ii
 
       and stlr sz rs rd ii = do_str sz AArch64.L rs (read_reg_ord rd ii) ii
 
@@ -304,21 +341,6 @@ module Make
         | Inc -> M.op Op.Add v V.one
         | Neg -> M.op Op.Sub V.zero v
         | Inv -> Warn.fatal "size dependent inverse not implemented"
-
-      (* Apply a shift as monadic op *)
-      let shift s =
-        let open AArch64Base in
-        match s with
-          | S_NOEXT   -> M.unitT
-          | S_LSL(n)  -> fun x -> M.op (Op.ShiftLeft) x (V.intToV n)
-          | S_LSR(n)  -> fun x -> M.op (Op.ShiftRight) x (V.intToV n)
-          | S_ASR(n)  -> fun x -> M.op (Op.ASR) x (V.intToV n)
-          | S_SXTW -> fun x ->
-            let m = V.op1 (Op.LeftShift 31) V.one in
-            M.op Op.Xor x m
-            >>= fun v -> M.op Op.Sub v m
-          | S_UXTW->
-            Warn.fatal "UXTW barrel shift not supported yet"
 
       let rmw_amo_read rmw sz = let open AArch64 in match rmw with
       | RMW_A|RMW_AL -> old_do_read_mem sz XA
@@ -438,12 +460,15 @@ module Make
                   >>= fun () -> B.bccT v l
 
                       (* Load and Store *)
-        | I_LDR(var,rd,rs,kr) ->
+        | I_LDR(var,rd,rs,kr,s) ->
             let sz = tr_variant var in
-            ldr sz rd rs kr ii
+            ldr sz rd rs kr s ii
         | I_LDRBH (bh, rd, rs, kr) ->
             let sz = bh_to_sz bh in
-            ldr sz rd rs kr ii
+            ldr sz rd rs kr S_NOEXT ii
+        | I_LDR_P(var,rd,rs,k) ->
+            let sz = tr_variant var in
+            ldr_p sz rd rs k ii
 
         | I_LDAR(var,t,rd,rs) ->
             let sz = tr_variant var in
@@ -468,7 +493,7 @@ module Make
             if not memtag then Warn.user_error "STZG without -variant memtag" ;
             begin
               (read_reg_data MachSize.Quad rt ii >>= tag_extract) >>|
-              get_ea rn kr ii
+              get_ea rn kr AArch64.S_NOEXT ii
             end >>= fun (v,a) ->
             (M.op1 Op.TagLoc a >>| loc_extract a) >>= fun (atag,loc) ->
             (do_write_tag atag v ii >>| do_write_mem MachSize.Quad AArch64.N loc V.zero ii) >>! B.Next
@@ -477,14 +502,14 @@ module Make
             if not memtag then Warn.user_error "STG without -variant memtag" ;
             begin
               (read_reg_data MachSize.Quad rt ii >>= tag_extract) >>|
-              get_ea rn kr ii
+              get_ea rn kr S_NOEXT ii
             end >>= fun (v,a) ->
             M.op1 Op.TagLoc a  >>= fun a ->
             do_write_tag a v ii >>! B.Next
 
         | I_LDG (rt,rn,kr) ->
             if not memtag then Warn.user_error "LDG without -variant memtag" ;
-            get_ea rn kr ii  >>=
+            get_ea rn kr S_NOEXT ii  >>=
             fun a -> M.op1 Op.TagLoc a >>=
             fun atag -> do_read_tag atag ii
             >>= fun tag ->

herd/unittests/AArch64/A04.litmus
@@ -1,0 +1,8 @@
+AArch64 A4
+(* Tests load immediate, no offset, from location *)
+{ 0:X1=x }
+
+P0;
+  LDR X0, [X1];
+
+exists (0:X0=0)

herd/unittests/AArch64/A08.litmus
@@ -1,0 +1,9 @@
+AArch64 A8
+(* Tests load immediate, post-indexed, symbolic location *)
+{ 0:x1=x;}
+
+P0;
+  LDR X0, [X1], #44;
+
+exists (0:X0=0 /\ 0:X1!=x) (* This should be x+44, todo: encode arbitrary expressions *)
+(* See test A9 for a variant that uses a concrete address*)

jingle/AArch64Arch_jingle.ml
@@ -25,6 +25,16 @@ include Arch.MakeArch(struct
     | K(MetaConst.Int i),K(j) when i=j -> Some subs
     | _ -> None
 
+  let match_shift s s' subs =  match s,s' with
+    | S_LSL (MetaConst.Meta m), S_LSL y
+    | S_LSR (MetaConst.Meta m), S_LSR y
+    | S_ASR (MetaConst.Meta m), S_ASR y -> add_subs [(Cst(m,y))] subs
+    | S_LSL (MetaConst.Int m), S_LSL y
+    | S_LSR (MetaConst.Int m), S_LSR y
+    | S_ASR (MetaConst.Int m), S_ASR y when m=y -> Some subs
+    | S_SXTW, S_SXTW | S_UXTW, S_UXTW | S_NOEXT, S_NOEXT -> Some subs
+    | _ -> None
+
   let match_instr subs pattern instr = match pattern,instr with
     | I_NOP,I_NOP -> Some subs
     | I_FENCE fp,I_FENCE fi when fp = fi
@@ -56,7 +66,15 @@ include Arch.MakeArch(struct
         add_subs
           [Reg(sr_name r1,r1'); Reg(sr_name r2,r2'); Reg(sr_name r3,r3')]
           subs
-    | I_LDR(_,r1,r2,kr),I_LDR(_,r1',r2',kr')
+    | I_LDR_P(_,r1,r2,k),I_LDR_P(_,r1',r2',k')
+      ->
+        match_kr subs (K k) (K k') >>>
+        add_subs [Reg(sr_name r1,r1'); Reg(sr_name r2,r2')]
+    | I_LDR(_,r1,r2,kr,s),I_LDR(_,r1',r2',kr',s')
+      ->
+        match_kr subs kr kr' >>>
+        match_shift s s' >>>
+        add_subs [Reg(sr_name r1,r1'); Reg(sr_name r2,r2')]
     | I_STR(_,r1,r2,kr),I_STR(_,r1',r2',kr')
       ->
         match_kr subs kr kr' >>>
@@ -161,11 +179,17 @@ include Arch.MakeArch(struct
         conv_reg r2 >> fun r2 ->
         conv_reg r3 >! fun r3 ->
         I_STXRBH(a,b,r1,r2,r3)
-    | I_LDR(a,r1,r2,kr) ->
+    | I_LDR(a,r1,r2,kr,s) ->
         conv_reg r1 >> fun r1 ->
         conv_reg r2 >> fun r2 ->
+        find_shift s >> fun s ->
         expl_kr kr >! fun kr ->
-        I_LDR(a,r1,r2,kr)
+        I_LDR(a,r1,r2,kr,s)
+    | I_LDR_P(a,r1,r2,k) ->
+        conv_reg r1 >> fun r1 ->
+        conv_reg r2 >> fun r2 ->
+        find_cst k >! fun k ->
+        I_LDR_P(a,r1,r2,k)
     | I_LDP(t,a,r1,r2,r3,kr) ->
         conv_reg r1 >> fun r1 ->
         conv_reg r2 >> fun r2 ->

lib/AArch64Base.ml
@@ -415,7 +415,10 @@ type 'k kinstruction =
   | I_BL of lbl | I_BLR of reg
   | I_RET of reg option
 (* Load and Store *)
-  | I_LDR of variant * reg * reg * 'k kr
+  | I_LDR of variant * reg * reg * 'k kr * 'k s
+(* Post-indexed load with immediate - like a writeback *)
+(* sufficiently different (and semantically interesting) to need a new inst *)
+  | I_LDR_P of variant * reg * reg * 'k
   | I_LDP of temporal * variant * reg * reg * reg * 'k kr
   | I_STP of temporal * variant * reg * reg * reg * 'k kr
   | I_LDAR of variant * ld_type * reg * reg
@@ -525,6 +528,15 @@ let do_pp_instruction m =
     pp_memo memo ^ " " ^ pp_vreg v rt ^
     ",[" ^ pp_xreg ra ^ pp_kr true false kr ^ "]" in
 
+  let pp_mem_shift memo v rt ra kr s =
+    pp_memo memo ^ " " ^ pp_vreg v rt ^
+    ",[" ^ pp_xreg ra ^ pp_kr true false kr ^
+    ","  ^ pp_barrel_shift "," s (m.pp_k) ^ "]" in
+
+  let pp_mem_post memo v rt ra k =
+    pp_memo memo ^ " " ^ pp_vreg v rt ^
+    ",[" ^ pp_xreg ra ^ "]" ^ m.pp_k k in
+
   let pp_memp memo v r1 r2 ra kr =
     pp_memo memo ^ " " ^
     pp_vreg v r1 ^ "," ^
@@ -582,8 +594,12 @@ let do_pp_instruction m =
       sprintf "RET %s" (pp_xreg r)
 
 (* Load and Store *)
-  | I_LDR (v,r1,r2,k) ->
+  | I_LDR (v,r1,r2,k,S_NOEXT) ->
       pp_mem "LDR" v r1 r2 k
+  | I_LDR (v,r1,r2,k,s) ->
+      pp_mem_shift "LDR" v r1 r2 k s
+  | I_LDR_P (v,r1,r2,k) ->
+      pp_mem_post "LDR" v r1 r2 k
   | I_LDP (t,v,r1,r2,r3,k) ->
       pp_memp (match t with TT -> "LDP" | NT -> "LDNP") v r1 r2 r3 k
   | I_STP (t,v,r1,r2,r3,k) ->
@@ -717,10 +733,10 @@ let fold_regs (f_regs,f_sregs) =
   | I_LDAR (_,_,r1,r2) | I_STLR (_,r1,r2) | I_STLRBH (_,r1,r2)
   | I_SXTW (r1,r2) | I_LDARBH (_,_,r1,r2)
   | I_STOP (_,_,_,r1,r2) | I_STOPBH (_,_,_,r1,r2)
-  | I_RBIT (_,r1,r2)
+  | I_RBIT (_,r1,r2) | I_LDR_P (_,r1,r2,_)
   | I_LDG (r1,r2,_) | I_STZG (r1,r2,_) | I_STG (r1,r2,_)
     -> fold_reg r1 (fold_reg r2 c)
-  | I_LDR (_,r1,r2,kr) | I_STR (_,r1,r2,kr)
+  | I_LDR (_,r1,r2,kr,_) | I_STR (_,r1,r2,kr)
   | I_OP3 (_,_,r1,r2,kr,_)
   | I_LDRBH (_,r1,r2,kr) | I_STRBH (_,r1,r2,kr)
     -> fold_reg r1 (fold_reg r2 (fold_kr kr c))
@@ -774,8 +790,10 @@ let map_regs f_reg f_symb =
   | I_RET (Some r) ->
       I_RET (Some (map_reg r))
 (* Load and Store *)
-  | I_LDR (v,r1,r2,kr) ->
-     I_LDR (v,map_reg r1,map_reg r2,map_kr kr)
+  | I_LDR (v,r1,r2,kr,os) ->
+     I_LDR (v,map_reg r1,map_reg r2,map_kr kr,os)
+  | I_LDR_P (v,r1,r2,k) ->
+     I_LDR_P (v,map_reg r1, map_reg r2, k)
   | I_LDP (t,v,r1,r2,r3,kr) ->
      I_LDP (t,v,map_reg r1,map_reg r2,map_reg r3,map_kr kr)
   | I_STP (t,v,r1,r2,r3,kr) ->
@@ -871,6 +889,7 @@ let get_next = function
   | I_BLR _|I_BR _|I_RET _ -> [Label.Any]
   | I_NOP
   | I_LDR _
+  | I_LDR_P _
   | I_LDP _
   | I_STP _
   | I_STR _
@@ -955,7 +974,8 @@ include Pseudo.Make
         | I_DC _
         | I_MRS _
             as keep -> keep
-        | I_LDR (v,r1,r2,kr) -> I_LDR (v,r1,r2,kr_tr kr)
+        | I_LDR (v,r1,r2,kr,s) -> I_LDR (v,r1,r2,kr_tr kr,ap_shift k_tr s)
+        | I_LDR_P (v,r1,r2,k) -> I_LDR_P (v,r1,r2,k_tr k)
         | I_LDP (t,v,r1,r2,r3,kr) -> I_LDP (t,v,r1,r2,r3,kr_tr kr)
         | I_STP (t,v,r1,r2,r3,kr) -> I_STP (t,v,r1,r2,r3,kr_tr kr)
         | I_STR (v,r1,r2,kr) -> I_STR (v,r1,r2,kr_tr kr)
@@ -983,6 +1003,8 @@ include Pseudo.Make
         | I_STOP _ | I_STOPBH _
         | I_STZG _
           -> 2
+        | I_LDR_P _ (* reads, stores, then post-index stores *)
+          -> 3
         | I_NOP
         | I_B _ | I_BR _
         | I_BL _ | I_BLR _

commit aeab8b48c5fe55e19d469ca7d6b0650d53edfbfe

[herd] Fix regression introduced by commit 642cb5967db3ff455d31a9797587d483b4c7a4e3.        Also add unit test that would have detected the problem.

lib/AArch64Parser.mly
@@ -169,7 +169,13 @@ kr0:
 kr0_no_shift:
 | { A.K (MetaConst.zero) }
 | COMMA k { A.K $2 }
+| COMMA xreg { A.RV (A.V64,$2) }
+| COMMA wreg { A.RV (A.V32,$2) }
+| COMMA wreg COMMA SXTW { A.RV (A.V32,$2) }
 
+/* Beware: for w-indexed accesses SXTW is considered always present.
+   Far from ideal, one simple to get correct assembly output for
+   the litmus tool. */
 kwr:
 | k { A.K $1 }
 | wreg { A.RV (A.V32,$1) }

herd/unittests/AArch64/L000.litmus
@@ -1,0 +1,14 @@
+AArch64 L000
+
+{
+0:X2=x;
+}
+
+(* Regression on STR indexed syntax *)
+
+ P0                  ;
+ MOV W0,#1           ;
+ EOR W1,W0,W0        ;
+ STR W0,[X2,W1,SXTW] ;
+forall (x=1)
+ 