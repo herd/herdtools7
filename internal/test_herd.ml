8c430ff5536faa4052042b78a3702b603d58e4fa
Ethel Morgan <ethel.morgan@arm.com>
2020-10-28 17:28:10 +0000

    [herd] Refactor test_herd using internal_lib modules

--- internal/test_herd.ml
+++ internal/test_herd.ml
@@ -18,33 +18,8 @@
 
 (* Contributed by Ethel Morgan <Ethel.Morgan@arm.com> *)
 
-(* General util *)
-
-let rec read_all chan =
-  let line = try Some (input_line chan) with End_of_file -> None in
-  match line with
-  | None -> []
-  | Some line -> line :: (read_all chan)
-
-let read_file path =
-  if Sys.file_exists path
-  then begin
-    let chan = open_in path in
-    let lines = read_all chan in
-    close_in chan ;
-    Some lines
-  end
-  else None
-
-let write_file path lines =
-  let chan = open_out path in
-  List.iter (fun l -> Printf.fprintf chan "%s\n" l) lines ;
-  close_out chan
-
-(* Tool-specific util *)
-
-let herd_command herd libdir litmus =
-  Printf.sprintf "%s -set-libdir %s %s" herd libdir litmus
+let herd_args libdir litmus =
+  ["-set-libdir"; libdir; litmus]
 
 let time_re = Str.regexp "^Time "
 let without_time lines =
@@ -52,35 +27,40 @@ let without_time lines =
   List.filter not_time lines
 
 let run_herd herd libdir litmus =
-  let command = herd_command herd libdir litmus in
-  let stdout = Unix.open_process_in command in
-  let lines = read_all stdout in
-  match Unix.close_process_in stdout with
-  | Unix.WEXITED 0 -> Some (without_time lines)
-  | _ -> None
+  let args = herd_args libdir litmus in
+  let lines = Command.run_with_stdout herd args Channel.read_lines in
+  without_time lines
 
-(* TODO: Return a meaningful diff. *)
-let logs_differ a b = not (a = b)
+let log_compare a b = String.compare (String.concat "\n" a) (String.concat "\n" b)
 
 let expected_of_litmus litmus = litmus ^ ".expected"
+let is_litmus path = Filename.extension path = ".litmus"
 
 let run_test herd libdir litmus =
-  match run_herd herd libdir litmus with
+  let output = try
+    Some (run_herd herd libdir litmus)
+  with _ -> None in
+  match output with
   | None -> Printf.printf "Failed %s : Herd returned non-zero\n" litmus ; false
   | Some output ->
+
   let expected = expected_of_litmus litmus in
-  match read_file expected with
+  let expected_output = try
+    Some (Filesystem.read_file expected Channel.read_lines)
+  with _ -> None in
+  match expected_output with
   | None -> Printf.printf "Failed %s : Missing file %s\n" litmus expected ; false
   | Some expected_output ->
-  if logs_differ output expected_output then begin
+
+  if log_compare output expected_output <> 0 then begin
     Printf.printf "Failed %s : Logs do not match\n" litmus ;
     false
-  end
-  else true
+  end else
+    true
 
 let litmuses_of_dir dir =
   let all_files = Array.to_list (Sys.readdir dir) in
-  let only_litmus = List.filter (fun p -> (Filename.extension p) = ".litmus") all_files in
+  let only_litmus = List.filter is_litmus all_files in
   let full_paths = List.map (Filename.concat dir) only_litmus in
   List.sort String.compare full_paths
 
@@ -88,8 +68,9 @@ let litmuses_of_dir dir =
 
 let show_tests herd libdir litmus_dir =
   let litmuses = litmuses_of_dir litmus_dir in
-  let commands = List.map (herd_command herd libdir) litmuses in
-  List.iter (Printf.printf "%s\n") commands
+  let command_of_litmus l = Command.command herd (herd_args libdir l) in
+  let commands = List.map command_of_litmus litmuses in
+  Channel.write_lines stdout commands
 
 let run_tests herd libdir litmus_dir =
   let litmuses = litmuses_of_dir litmus_dir in
@@ -106,12 +87,10 @@ let promote_tests herd libdir litmus_dir =
   let litmuses = litmuses_of_dir litmus_dir in
   let outputs = List.map (run_herd herd libdir) litmuses in
   let expecteds = List.map expected_of_litmus litmuses in
-  let write_file_or_raise (path, lines) =
-    match lines with
-    | None -> failwith "Herd encountered error"
-    | Some lines -> write_file path lines
+  let write_file (path, lines) =
+    Filesystem.write_file path (fun o -> Channel.write_lines o lines)
   in
-  List.combine expecteds outputs |> List.iter write_file_or_raise
+  List.combine expecteds outputs |> List.iter write_file
 
 let usage = String.concat "\n" [
   Printf.sprintf "Usage: %s [opts] (show|test|promote)" Sys.argv.(0) ;
