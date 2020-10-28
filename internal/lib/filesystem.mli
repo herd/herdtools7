5397110b561f4e4d4b6000d4313ae2d14d486890
Ethel Morgan <ethel.morgan@arm.com>
2020-10-28 17:25:50 +0000

    [internal] Create Filesystem module in internal_lib library
    This commit:
    - Creates a module Filesystem, to provide Filesystem-related utilities,
      such as cleanly reading from and writing to files.
    - Creates unit tests for that module.

---
+++ internal/lib/filesystem.mli
@@ -0,0 +1,27 @@
+(****************************************************************************)
+(*                           the diy toolsuite                              *)
+(*                                                                          *)
+(* Jade Alglave, University College London, UK.                             *)
+(* Luc Maranget, INRIA Paris-Rocquencourt, France.                          *)
+(*                                                                          *)
+(* Copyright 2010-present Institut National de Recherche en Informatique et *)
+(* en Automatique and the authors. All rights reserved.                     *)
+(*                                                                          *)
+(* This software is governed by the CeCILL-B license under French law and   *)
+(* abiding by the rules of distribution of free software. You can use,      *)
+(* modify and/ or redistribute the software under the terms of the CeCILL-B *)
+(* license as circulated by CEA, CNRS and INRIA at the following URL        *)
+(* "http://www.cecill.info". We also give a copy in LICENSE.txt.            *)
+(****************************************************************************)
+
+(** Filesystem and file utilities. *)
+
+(** [read_file path f] opens a file for reading at [path], and calls [f] on the
+ *  open channel. The file is closed after [f] returns. If an exception is
+ *  raised, the file is closed before re-raising the exception. *)
+val read_file : string -> (in_channel -> 'a) -> 'a
+
+(** [write_file path f] opens a file for writing at [path], and calls [f] on
+ *  the open channel. The file is closed after [f] returns. If an exception is
+ *  raised, the file is closed before re-raising the exception. *)
+val write_file : string -> (out_channel -> 'a) -> 'a
