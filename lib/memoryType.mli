5f32f93eea95dc87526ba2aff9a0f03a1598dc4e
Luc Maranget <Luc.Maranget@inria.fr>
2021-04-09 13:07:00 +0200

    [lib] Specifying memory types.
    More precisely, one module for IA-32 memory types,
    and one do-nothing module.

---
+++ lib/memoryType.mli
@@ -0,0 +1,28 @@
+(****************************************************************************)
+(*                           the diy toolsuite                              *)
+(*                                                                          *)
+(* Jade Alglave, University College London, UK.                             *)
+(* Luc Maranget, INRIA Paris-Rocquencourt, France.                          *)
+(*                                                                          *)
+(* Copyright 2021-present Institut National de Recherche en Informatique et *)
+(* en Automatique and the authors. All rights reserved.                     *)
+(*                                                                          *)
+(* This software is governed by the CeCILL-B license under French law and   *)
+(* abiding by the rules of distribution of free software. You can use,      *)
+(* modify and/ or redistribute the software under the terms of the CeCILL-B *)
+(* license as circulated by CEA, CNRS and INRIA at the following URL        *)
+(* "http://www.cecill.info". We also give a copy in LICENSE.txt.            *)
+(************************************** UC**************************************)
+
+(* Memory types of global variables *)
+module type S = sig
+  type t
+  val pp : t -> string (* Pretty print *)
+  val emit : t -> string (* Emit in code *)
+
+  val parse : MiscParser.info -> t Misc.Simple.bds
+end
+
+module X86_64 : S
+
+module No : S
