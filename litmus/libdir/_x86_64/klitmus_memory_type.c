dea7d0c4d36d90668ab1b496bd7569e55cf53765
Luc Maranget <Luc.Maranget@inria.fr>
2021-04-10 09:25:57 +0200

    [litmus] klitmus, x86_64. New technique for setting page memory types.
    For some reason the convenient `set_memory_wt` function is no longer
    exported by recent kernel. Fortunately, the `set_array_pages_wt`
    function is still exported.
    
    Using `set_pages_array_{xx}` functions implies allocating an
    array of `struct page` pointers for storing pointers to contiguous pages.
    Thus, set_memory_{xx} functions are still used, when they exist, _i.e._ for
    all other memory types.

--- litmus/libdir/_x86_64/klitmus_memory_type.c
+++ litmus/libdir/_x86_64/klitmus_memory_type.c
@@ -2,17 +2,17 @@
 /*  IA32 memory types */
 /**********************/
 
-/* Those x86 specific prototypes are not available in linux-headers package */
+#include <linux/mm.h>
 
+/* Those x86 specific prototypes were not found in linux-headers package */
 int set_memory_uc(unsigned long addr,int numpages);
 int set_memory_wc(unsigned long addr,int numpages);
-int set_memory_wt(unsigned long addr,int numpages);
-int set_memory_wp(unsigned long addr,int numpages);
 int set_memory_wb(unsigned long addr,int numpages);
+int set_pages_array_wt(struct page **pages,int numpages);
 
 typedef enum { PAT_UC, PAT_WC, PAT_WT, PAT_WP, PAT_WB, } pat_t ;
 
-static size_t npages(size_t sz) {
+static size_t opages(size_t sz) {
   size_t r=0 ; size_t max=PAGE_SIZE;
   while (sz > max) {
     max *= 2; r++;
@@ -23,31 +23,40 @@ static size_t npages(size_t sz) {
 static int klitmus_set_memory(pat_t pat, unsigned long addr, int numpages) {
   switch (pat) {
   case PAT_UC:
-    return set_memory_uc(addr, numpages);
+    return set_memory_uc(addr,numpages);
   case PAT_WC:
-    return set_memory_wc(addr, numpages);
+    return set_memory_wc(addr,numpages);
   case PAT_WT:
-    return set_memory_wt(addr, numpages);
-  case PAT_WP:
-    return set_memory_wp(addr, numpages);
+    {
+      struct page **pages = kmalloc_array(sizeof(*pages),numpages,GFP_KERNEL);
+      if (!pages) return -1;
+      for (int k=0 ; k < numpages ; k++) {
+        pages[k] = virt_to_page(addr);
+        addr += PAGE_SIZE;
+      }
+      int ret = set_pages_array_wt(pages,numpages);
+      kfree(pages);
+      return ret ;
+    }
   case PAT_WB:
-    return set_memory_wb(addr, numpages);
+    return set_memory_wb(addr,numpages);
+  case PAT_WP:
   default:
     return -1; /* Should not happen */
   }
 }
 
 static void klitmus_free_pat(void *p,size_t sz) {
-  int np = npages(sz);
-  (void)set_memory_wb((unsigned long)p,1 << np);
-  free_pages((unsigned long)p,np);
+  int op = opages(sz);
+  (void)set_memory_wb((unsigned long)p,1 << op);
+  free_pages((unsigned long)p,op);
 }
 
 static void *klitmus_alloc_pat(pat_t pat, size_t sz) {
-  int np = npages(sz);
-  void *r = (void *)__get_free_pages(GFP_KERNEL,np);
+  int op = opages(sz);
+  void *r = (void *)__get_free_pages(GFP_KERNEL,op);
   if (!r) return r;
-  if (klitmus_set_memory(pat,(unsigned long)r,1 << np)) {
+  if (klitmus_set_memory(pat,(unsigned long)r,1 << op)) {
     klitmus_free_pat(r,sz);
     return NULL;
   }
