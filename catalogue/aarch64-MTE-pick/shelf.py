4654ece7b62a0cdc05ee5b7fff562845e8069002
Luc Maranget <Luc.Maranget@inria.fr>
2021-08-04 15:30:55 +0200

    [catalogue] Creare book for combined MTE pick depencies.
    Notice: two tests only in this book.

---
+++ catalogue/aarch64-MTE-pick/shelf.py
@@ -0,0 +1,14 @@
+record = "AArch64-MTE-Pick"
+
+cats = [
+    "cats/aarch64-pick.cat",
+    ]
+
+cfgs = [
+    "cfgs/web-MTE.cfg",
+]
+
+illustrative_tests = [
+    "tests/TTOB.litmus",
+    "tests/TTOB2.litmus",
+]
