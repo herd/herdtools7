469697165e5e4a915fb2be7a8071c412a3e27460
Luc Maranget <Luc.Maranget@inria.fr>
2021-10-15 16:29:34 +0200

    [litmus] AArch64, avoid meaningless warning
    Using 'W' registers to access locations of
    byte and halfword size is legitimate.
    
    Notice that proper usage of the 'B' and 'H' forms
    of the memory access instructions is not checked.

--- litmus/AArch64Arch_litmus.ml
+++ litmus/AArch64Arch_litmus.ml
@@ -61,6 +61,8 @@ module Make(O:Arch_litmus.Config)(V:Constant.S) = struct
           | Vreg _ | SIMDreg _ -> "+w"
           | _ -> "=r"
         let comment = comment
+
+(* t1 is declared (or inferred) type, t2 is type from instruction *)
         let error t1 t2 =
           let open CType in
 (*          Printf.eprintf "Error %s and %s\n" (debug t1) (debug t2) ; *)
@@ -68,11 +70,14 @@ module Make(O:Arch_litmus.Config)(V:Constant.S) = struct
           | (Base "int",Pointer _)
           | (Pointer _,Base "int")  ->
               true
+
           | _ -> false
         let warn t1 t2 =
           let open CType in
           match t1,t2 with
-          | Base ("int"|"int32_t"|"uint32_t"),
+          | Base ("int"|"int32_t"|"uint32_t"
+                  |"int16_t"|"uint16_t"
+                  |"int8_t"|"uint8_t"),
             Base ("int"|"int32_t"|"uint32_t") -> false
           | (Base "int",_)|(_,Base "int") -> true
           | _ -> false
