e66530f9c120daf4bf48dad5448168199acd0b07
Nikos Nikoleris <nikos.nikoleris@arm.com>
2022-07-06 17:15:47 +0100

    [herd] Simplify loader for instructions setting the link register
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

--- herd/loader.ml
+++ herd/loader.ml
@@ -39,50 +39,49 @@ struct
   type start_points = A.start_points
   type return_labels = A.return_labels
 
-  let rec load_code proc addr mem rets link_num = function
-    | [] -> mem,[],rets,link_num
+  let rec load_code proc addr mem rets = function
+    | [] -> mem,[],rets
     | ins::code ->
-      load_ins proc addr mem rets link_num code ins
+      load_ins proc addr mem rets code ins
 
-  and load_ins proc addr mem rets link_num code = fun x ->
+  and load_ins proc addr mem rets code = fun x ->
     match x with
     | A.Nop ->
-      load_code proc addr mem rets link_num code
+      load_code proc addr mem rets code
     | A.Instruction ins ->
       if Misc.is_some (A.is_link ins) then
-        let new_mem,start,new_rets,new_num =
-          load_code proc (addr+4) mem rets (link_num+1) code in
-        let lbl = Printf.sprintf "##%d" link_num in
+        let new_mem,start,new_rets =
+          load_code proc (addr+4) mem rets code in
+        let lbl = Printf.sprintf "##%d" addr in
         let newer_mem =
-            if Label.Map.mem lbl new_mem then
-              Warn.user_error
-                "Label %s cannot be created, since it is reserved internally" lbl ;
-            Label.Map.add lbl (proc,start) new_mem in
+          if Label.Map.mem lbl new_mem then
+            Warn.user_error
+              "Label %s cannot be created, since it is reserved internally" lbl ;
+          Label.Map.add lbl (proc,start) new_mem in
         let newer_rets = IntMap.add addr lbl new_rets in
-        newer_mem,(addr,ins)::start,newer_rets,new_num
+        newer_mem,(addr,ins)::start,newer_rets
       else
-        let mem,start,new_rets,new_num =
-          load_code proc (addr+4) mem rets link_num code in
-        mem,(addr,ins)::start,new_rets,new_num
+        let mem,start,new_rets =
+          load_code proc (addr+4) mem rets code in
+        mem,(addr,ins)::start,new_rets
     | A.Label (lbl,ins) ->
-        let mem,start,new_rets,new_num =
-          load_ins proc addr mem rets link_num code ins in
+        let mem,start,new_rets =
+          load_ins proc addr mem rets code ins in
         if Label.Map.mem lbl mem then
           Warn.user_error
             "Label %s occurs more that once" lbl ;
-        Label.Map.add lbl (proc,start) mem,start,new_rets,new_num
+        Label.Map.add lbl (proc,start) mem,start,new_rets
     | A.Symbolic _
     | A.Macro (_,_) -> assert false
 
   let load prog =
-    let rec load_iter num = function
-    | [] -> Label.Map.empty,[],IntMap.empty,num
+    let rec load_iter = function
+    | [] -> Label.Map.empty,[],IntMap.empty
     | ((proc,_,_),code)::prog ->
       let addr = 1000 * (proc+1) in
-      let mem,starts,rets,new_num = load_iter num prog in
-      let fin_mem,start,fin_rets,fin_num = load_code proc addr mem rets new_num code in
-      fin_mem,(proc,start)::starts,fin_rets,fin_num in
-    let mem, starts, rets, _ = load_iter 0 prog in
-    mem, starts, rets
+      let mem,starts,rets = load_iter prog in
+      let fin_mem,start,fin_rets = load_code proc addr mem rets code in
+      fin_mem,(proc,start)::starts,fin_rets in
+    load_iter prog
 
 end
