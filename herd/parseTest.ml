dd895439d52263796dc48a9b6c177a1795e95f1a
Luc Maranget <Luc.Maranget@inria.fr>
2022-07-29 17:56:59 +0200

    [herd] Variant declaration in test override other declarations.
    Variant are first defined by command line and configuration file.
    Those global declarations are overridden by any test specific
    declaration `Variant=...` from test source.
    
    Some part of herd code (specifically the `Mem` module) was unaware
    of such overriding.

--- herd/parseTest.ml
+++ herd/parseTest.ml
@@ -45,66 +45,63 @@ module NoCheck = struct
   let check parsed= parsed
 end
 
-module Top (TopConf:Config) = struct
+module Make
+    (Conf:Config)
+    (S:Sem.Semantics)
+    (P:sig
+      type pseudo
+      val parse : in_channel -> Splitter.result ->  pseudo MiscParser.t
+    end with type pseudo = S.A.pseudo)
+    (Check:
+       sig
+         val check : S.A.pseudo MiscParser.t -> S.A.pseudo MiscParser.t
+       end)
+    (M:XXXMem.S with module S = S) =
+  struct
+    module T = Test_herd.Make(S.A)
 
-  module Make
-      (S:Sem.Semantics)
-      (P:sig
-        type pseudo
-        val parse : in_channel -> Splitter.result ->  pseudo MiscParser.t
-      end with type pseudo = S.A.pseudo)
-      (Check:
-         sig
-           val check : S.A.pseudo MiscParser.t -> S.A.pseudo MiscParser.t
-         end)
-      (M:XXXMem.S with module S = S) =
-    struct
-      module T = Test_herd.Make(S.A)
-
-      let run dirty start_time filename chan env splitted =
-        try
-
-          let parsed = P.parse chan splitted in
-
-          (* Additional checks *)
-          let parsed = Check.check parsed in
-
-
+    let run dirty start_time filename chan env splitted =
+      try
+         let parsed = P.parse chan splitted in
+         (* Additional checks *)
+        let parsed = Check.check parsed in
           let name = splitted.Splitter.name in
-          let hash = MiscParser.get_hash parsed in
-          let env = match hash with
-          | None -> env
-          | Some hash ->
-              TestHash.check_env env name.Name.name filename hash in
+        let hash = MiscParser.get_hash parsed in
+        let env = match hash with
+        | None -> env
+        | Some hash ->
+            TestHash.check_env env name.Name.name filename hash in
           let test = T.build name parsed in
 (* Compute basic machine size *)
-          let sz =
-            if S.A.is_mixed then begin match TopConf.byte with
-            | MachSize.Tag.Size sz -> sz
-            | MachSize.Tag.Auto ->
-                let szs = test.Test_herd.access_size in
-                match szs with
-                | [] -> MachSize.Byte
-                | [sz] -> MachSize.pred sz
-                | sz::_ -> sz
-            end else begin
-              (* Cannot that easily check the test not to mix sizes,
-                 as there are several locations in test that may be of
-                 different sizes *)
-              MachSize.Byte
-            end in
+        let sz =
+          if S.A.is_mixed then begin match Conf.byte with
+          | MachSize.Tag.Size sz -> sz
+          | MachSize.Tag.Auto ->
+              let szs = test.Test_herd.access_size in
+              match szs with
+              | [] -> MachSize.Byte
+              | [sz] -> MachSize.pred sz
+              | sz::_ -> sz
+          end else begin
+            (* Cannot that easily check the test not to mix sizes,
+               as there are several locations in test that may be of
+               different sizes *)
+            MachSize.Byte
+          end in
 (* And run test *)
-          let module T =
-            Top_herd.Make
-              (struct
-                include TopConf
-                let byte = sz
-                let dirty = dirty
-              end)(M) in
-          T.run start_time test ;
-          env
-        with TestHash.Seen -> env
-    end
+        let module T =
+          Top_herd.Make
+            (struct
+              include Conf
+              let byte = sz
+              let dirty = dirty
+            end)(M) in
+        T.run start_time test ;
+        env
+      with TestHash.Seen -> env
+  end
+
+module Top (TopConf:Config) = struct
 
   module SP =
     Splitter.Make
@@ -212,7 +209,7 @@ module Top (TopConf:Config) = struct
           let module PPCS = PPCSem.Make(Conf)(Int64Value) in
           let module PPCM = PPCMem.Make(ModelConfig)(PPCS) in
           let module P = GenParser.Make (Conf) (PPC) (PPCLexParse) in
-          let module X = Make (PPCS) (P) (NoCheck) (PPCM) in
+          let module X = Make (Conf) (PPCS) (P) (NoCheck) (PPCM) in
           X.run dirty start_time name chan env splitted
       | `ARM ->
           let module ARM = ARMArch_herd.Make(ArchConfig)(Int32Value) in
@@ -226,7 +223,7 @@ module Top (TopConf:Config) = struct
           let module ARMS = ARMSem.Make(Conf)(Int32Value) in
           let module ARMM = ARMMem.Make(ModelConfig)(ARMS) in
           let module P = GenParser.Make (Conf) (ARM) (ARMLexParse) in
-          let module X = Make (ARMS) (P) (NoCheck) (ARMM) in
+          let module X = Make (Conf) (ARMS) (P) (NoCheck) (ARMM) in
           X.run dirty start_time name chan env splitted
       | `AArch64 ->
          let module
@@ -246,7 +243,7 @@ module Top (TopConf:Config) = struct
                let parser = (*MiscParser.mach2generic*) AArch64Parser.main
              end
 
-             module AArch64SemConf = struct
+            module AArch64SemConf = struct
                module C = Conf
                let dirty = ModelConfig.dirty
                let procs_user = ProcsUser.get splitted.Splitter.info
@@ -272,7 +269,7 @@ module Top (TopConf:Config) = struct
 
              module P = GenParser.Make (Conf) (AArch64) (AArch64LexParse)
 
-             module X = Make (AArch64S) (P) (AArch64C) (AArch64M)
+             module X = Make (Conf) (AArch64S) (P) (AArch64C) (AArch64M)
 
              let run = X.run
          end in
@@ -301,7 +298,7 @@ module Top (TopConf:Config) = struct
           let module X86S = X86Sem.Make(Conf)(Int32Value) in
           let module X86M = X86Mem.Make(ModelConfig)(X86S) in
           let module P = GenParser.Make (Conf) (X86) (X86LexParse) in
-          let module X = Make (X86S) (P) (NoCheck) (X86M) in
+          let module X = Make(Conf)(X86S)(P)(NoCheck)(X86M) in
           X.run dirty start_time name chan env splitted
 
       | `X86_64 ->
@@ -316,7 +313,7 @@ module Top (TopConf:Config) = struct
           let module X86_64S = X86_64Sem.Make(Conf)(Int64Value) in
           let module X86_64M = X86_64Mem.Make(ModelConfig)(X86_64S) in
           let module P = GenParser.Make(Conf)(X86_64)(X86_64LexParse) in
-          let module X = Make(X86_64S)(P)(NoCheck)(X86_64M) in
+          let module X = Make(Conf)(X86_64S)(P)(NoCheck)(X86_64M) in
           X.run dirty start_time name chan env splitted
 
 
@@ -332,7 +329,7 @@ module Top (TopConf:Config) = struct
           let module MIPSS = MIPSSem.Make(Conf)(Int64Value) in
           let module MIPSM = MIPSMem.Make(ModelConfig)(MIPSS) in
           let module P = GenParser.Make (Conf) (MIPS) (MIPSLexParse) in
-          let module X = Make (MIPSS) (P) (NoCheck) (MIPSM) in
+          let module X = Make (Conf) (MIPSS) (P) (NoCheck) (MIPSM) in
           X.run dirty start_time name chan env splitted
 
       | `RISCV ->
@@ -347,7 +344,7 @@ module Top (TopConf:Config) = struct
           let module RISCVS = RISCVSem.Make(Conf)(Int64Value) in
           let module RISCVM = RISCVMem.Make(ModelConfig)(RISCVS) in
           let module P = GenParser.Make (Conf) (RISCV) (RISCVLexParse) in
-          let module X = Make (RISCVS) (P) (NoCheck) (RISCVM) in
+          let module X = Make (Conf) (RISCVS) (P) (NoCheck) (RISCVM) in
           X.run dirty start_time name chan env splitted
       | `C ->
           let module C = CArch_herd.Make(ArchConfig)(Int64Value) in
@@ -369,7 +366,7 @@ module Top (TopConf:Config) = struct
           let module CS = CSem.Make(Conf)(Int64Value) in
           let module CM = CMem.Make(ModelConfig)(CS) in
           let module P = CGenParser_lib.Make (Conf) (C) (CLexParse) in
-          let module X = Make (CS) (P) (NoCheck) (CM) in
+          let module X = Make (Conf) (CS) (P) (NoCheck) (CM) in
           X.run dirty start_time name chan env splitted
       | `CPP as arch -> Warn.fatal "no support for arch '%s'" (Archs.pp arch)
       | `LISA ->
@@ -398,7 +395,7 @@ module Top (TopConf:Config) = struct
                 let tr_compat = Bell.tr_compat
               end) in
           let module P = GenParser.Make (Conf) (Bell) (BellLexParse) in
-          let module X = Make (BellS) (P) (BellC) (BellM) in
+          let module X = Make (Conf) (BellS) (P) (BellC) (BellM) in
           X.run dirty start_time name chan env splitted
     end else env
 
