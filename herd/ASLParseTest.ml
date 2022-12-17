2de90e3ffd4d649ad8e5b1ea2bd92ce34036439e
Hadrien Renaud <hadrien.renaud2@arm.com>
2022-12-17 00:25:25 +0000

    [herd-asl] Implement new values for ASL in herd
    The new arrays are not yet used by the ASL interpreter. This will
    recquire progress on the interpreter first.

--- herd/ASLParseTest.ml
+++ herd/ASLParseTest.ml
@@ -14,19 +14,22 @@
 (* "http://www.cecill.info". We also give a copy in LICENSE.txt.            *)
 (****************************************************************************)
 
-module Make(Conf:RunTest.Config)(ModelConfig:MemCat.Config) = struct
-  module ArchConfig = SemExtra.ConfigToArchConfig(Conf)
-  module ASLValue = Int64Value.Make(ASLBase.Instr)
-  module ASLS = ASLSem.Make(Conf)(ASLValue)
+module Make (Conf : RunTest.Config) (ModelConfig : MemCat.Config) = struct
+  module ArchConfig = SemExtra.ConfigToArchConfig (Conf)
+  module ASLS = ASLSem.Make (Conf)
   module ASLA = ASLS.ASL64AH
+
   module ASLLexParse = struct
     type instruction = ASLA.parsedPseudo
     type token = Asllib.Parser.token
+
     let lexer = Asllib.Lexer.token
     let parser = ASLBase.asl_generic_parser
   end
-  module ASLM = MemCat.Make(ModelConfig)(ASLS)
+
+  module ASLM = MemCat.Make (ModelConfig) (ASLS)
   module P = GenParser.Make (Conf) (ASLA) (ASLLexParse)
   module X = RunTest.Make (ASLS) (P) (ASLM) (Conf)
+
   let run = X.run
 end
