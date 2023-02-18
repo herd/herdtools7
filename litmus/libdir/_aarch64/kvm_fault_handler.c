f547fd670ddbb941e412ea6daf79bf453c233d9c
Nikos Nikoleris <nikos.nikoleris@arm.com>
2023-02-18 00:56:01 +0000

    [litmus] Identify and record Undefined Instruction faults
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

--- litmus/libdir/_aarch64/kvm_fault_handler.c
+++ litmus/libdir/_aarch64/kvm_fault_handler.c
@@ -16,7 +16,8 @@ static void fault_handler(struct pt_regs *regs,unsigned int esr) {
 }
 
 static void install_fault_handler(int cpu) {
-  install_exception_handler(EL1H_SYNC, ESR_EL1_EC_DABT_EL1,fault_handler);
+  install_exception_handler(EL1H_SYNC, ESR_EL1_EC_DABT_EL1, fault_handler);
+  install_exception_handler(EL1H_SYNC, ESR_EL1_EC_UNKNOWN, fault_handler);
 #ifdef USER_MODE
   struct thread_info *ti = thread_info_sp(user_stack[cpu]);
   ti->exception_handlers[EL0_SYNC_64][ESR_EL1_EC_DABT_EL0] = fault_handler;
--- litmus/libdir/_aarch64/kvm_fault_type.c
+++ litmus/libdir/_aarch64/kvm_fault_type.c
@@ -1,34 +1,44 @@
-
 enum fault_type_t {
-  FaultMMUAddressSize = 0,
+  NoFault,
+  FaultUndefinedInstruction,
+  FaultMMUAddressSize,
   FaultMMUTranslation,
   FaultMMUAccessFlag,
   FaultMMUPermission,
   FaultTagCheck,
   FaultUnsupported,
-  NoFault,
   FaultTypes,
 };
 
 static const char *fault_type_names[] = {
+  "NoFault",
+  "UndefinedInstruction",
   "MMU:AddressSize",
   "MMU:Translation",
   "MMU:AccessFlag",
   "MMU:Permission",
   "TagCheck",
   "Unsupported",
-  "NoFault"
 };
 
 static enum fault_type_t get_fault_type(unsigned int esr)
 {
-  unsigned int dfsc = esr & 0x3fU;
-  int fault_type = dfsc >> 2;
+  unsigned int ec;
+  unsigned int dfsc;
+  int fault_type;
+
   if (esr == -1)
     return NoFault;
 
-  if (fault_type >= FaultUnsupported)
-    return FaultUnsupported;
-  else
-    return fault_type;
+  ec = esr >> ESR_EL1_EC_SHIFT;
+  if (ec == ESR_EL1_EC_UNKNOWN) {
+    return FaultUndefinedInstruction;
+  } else {
+    dfsc = esr & 0x3fU;
+    fault_type = (dfsc >> 2) + FaultMMUAddressSize;
+    if (fault_type >= FaultUnsupported)
+      return FaultUnsupported;
+    else
+      return fault_type;
+  }
 }
