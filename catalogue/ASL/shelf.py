fb0d32c60987d01323e7dd9e75497971e79ff1a4
Luc Maranget <Luc.Maranget@inria.fr>
2023-05-25 14:36:47 +0200

    [www] Add files for the ASL book in catalogue
    Not activated yet.

---
+++ catalogue/ASL/shelf.py
@@ -0,0 +1,21 @@
+record = "ASL"
+
+cats = [
+    "cats/asl-pseudo-arch.cat",
+    ]
+
+cfgs = [
+    "cfgs/pseudo-conf.cfg",
+]
+
+illustrative_tests = [
+    "tests/MP-01.litmus",
+    "tests/LB-02.litmus",
+    "tests/LB-03.litmus",
+    "tests/LB-04.litmus",
+    "tests/LB-05.litmus",
+    "tests/LB-06.litmus",
+    "tests/LB-08.litmus",
+    "tests/LB-09.litmus",
+    "tests/LB-10.litmus",
+]
