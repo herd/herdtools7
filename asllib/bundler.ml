36f26a0886f3979ce73ea3f65fb5fb454cc2c424
Jade Alglave <jade.alglave@arm.com>
2023-09-27 21:21:57 +0100

    Adjusting header

--- asllib/bundler.ml
+++ asllib/bundler.ml
@@ -1,3 +1,28 @@
+(******************************************************************************)
+(*                                ASLRef                                      *)
+(*                                                                            *)
+(* Copyright (c) 2022-present, Arm Limited or its affiliates.                 *)
+(* All rights reserved.                                                       *)
+(*                                                                            *)
+(* SPDX-License-Identifier: Apache-2.0                                        *)
+(******************************************************************************)
+(* Authors:                                                                   *)
+(* Hadrien Renaud, University College London, UK.                             *)
+(******************************************************************************)
+(* Disclaimer:                                                                *)
+(* This material covers both ASLv0 (viz, the existing ASL pseudocode language *)
+(* which appears in the Arm Architecture Reference Manual) and ASLv1, a new,  *)
+(* experimental, and as yet unreleased version of ASL.                        *)
+(* This material is work in progress, more precisely at pre-Alpha quality as  *)
+(* per Arm’s quality standards.                                               *)
+(* In particular, this means that it would be premature to base any           *)
+(* production tool development on this material.                              *)
+(* However, any feedback, question, query and feature request would be most   *)
+(* welcome; those can be sent to Arm’s Architecture Formal Team Lead          *)
+(* Jade Alglave <jade.alglave@arm.com>, or by raising issues or PRs to the    *)
+(* herdtools7 github repository.                                              *)
+(******************************************************************************)
+
 open Asllib
 module IMap = ASTUtils.IMap
 module ISet = ASTUtils.ISet
