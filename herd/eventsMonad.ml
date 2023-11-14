42ab821fa9bad1c29ce91a81d6995a80b8755f4c
Nikos Nikoleris <nikos.nikoleris@arm.com>
2023-11-14 10:31:29 +0000

    [herd] Add branching events for AArch64 CAS instructions
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

--- herd/eventsMonad.ml
+++ herd/eventsMonad.ml
@@ -573,6 +573,7 @@ Monad type:
         (read_rn:'loc t) (read_rs:'v t)
         (write_rs:'v-> unit t)
         (read_mem: 'loc -> 'v t)
+        (branch: 'loc -> unit t)
         (rne: 'v -> 'v -> unit t)
         eiid =
       let eiid,read_rn = read_rn eiid in
@@ -587,12 +588,14 @@ Monad type:
             let ov,cl_rm,es_rm = Evt.as_singleton_nospecul read_mem in
             let eiid,write_rs = write_rs ov eiid in
             let (),cl_wrs,es_wrs = Evt.as_singleton_nospecul write_rs in
+            let eiid,branch = branch a eiid in
+            let (),cl_br,es_br =  Evt.as_singleton_nospecul branch in
             let eiid,nem = rne ov cv eiid in
             let (),cl_ne,eseq =  Evt.as_singleton_nospecul nem in
             assert (E.is_empty_event_structure eseq) ;
             let es =
-              E.aarch64_cas_no is_physical es_rn es_rs es_wrs es_rm in
-            let cls = cl_a@cl_cv@cl_rm@cl_wrs@cl_ne  in
+              E.aarch64_cas_no is_physical es_rn es_rs es_wrs es_rm es_br in
+            let cls = cl_a@cl_cv@cl_rm@cl_wrs@cl_ne@cl_br  in
             eiid,Evt.add ((),cls,es) acts)
           acts_rn (eiid,Evt.empty) in
       eiid,(acts, None)
@@ -603,6 +606,7 @@ Monad type:
         (read_rn:'loc t) (read_rs:'v t) (read_rt: 'v t)
         (write_rs:'v-> unit t)
         (read_mem: 'loc -> 'v t) (write_mem: 'loc -> 'v -> unit t)
+        (branch: 'loc -> unit t)
         (req: 'v -> 'v -> unit t)
         eiid =
       let eiid,read_rn = read_rn eiid in
@@ -621,12 +625,14 @@ Monad type:
             and (),cl_wm,es_wm= Evt.as_singleton_nospecul write_mem in
             let eiid,write_rs = write_rs ov eiid in
             let (),cl_wrs,es_wrs = Evt.as_singleton_nospecul write_rs in
+            let eiid,branch = branch a eiid in
+            let (),cl_br,es_br =  Evt.as_singleton_nospecul branch in
             let eiid,eqm = req ov cv eiid in
             let (),cl_eq,eseq =  Evt.as_singleton_nospecul eqm in
             assert (E.is_empty_event_structure eseq) ;
             let es =
-              E.aarch64_cas_ok is_physical prov_data es_rn es_rs es_rt es_wrs es_rm es_wm in
-            let cls = cl_a@cl_cv@cl_nv@cl_rm@cl_wm@cl_wrs@cl_eq  in
+              E.aarch64_cas_ok is_physical prov_data es_rn es_rs es_rt es_wrs es_rm es_wm es_br in
+            let cls = cl_a@cl_cv@cl_nv@cl_rm@cl_wm@cl_wrs@cl_br@cl_eq  in
             eiid,Evt.add ((),cls,es) acts)
           acts_rn (eiid,Evt.empty) in
       eiid,(acts, None)
@@ -678,10 +684,10 @@ Monad type:
 
     let aarch64_cas_ok (is_physical: bool) (read_rn: 'loc t) (read_rs: 'v t)
         (read_rt: 'v t) (write_rs: 'v -> unit t) (read_mem: 'loc -> 'v t)
-        (write_mem: 'loc -> 'v -> unit t) (req: 'v -> 'v -> unit t) =
+        (write_mem: 'loc -> 'v -> unit t) (branch: 'loc -> unit t) (req: 'v -> 'v -> unit t) =
       let do_ prov_data =
         do_aarch64_cas_ok is_physical prov_data read_rn read_rs read_rt
-          write_rs read_mem write_mem req
+          write_rs read_mem write_mem branch req
       in
       altT (do_ `DataFromRRs) (do_ `DataFromRx)
 
