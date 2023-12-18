herd/herdtools7

commit c39eb9fd888f47b1b1a1d44e57de09519a7404c8

[herd,asl] Avoid name clashes between stdlib.asl and other library definitions
Function defined by ASL standard library take precedence,
given file loading  order this means that definitions
that would clash with those of the ASL standard
library have to be removed at some point.
A (partial) static test is replaced by a dynamic one.

herd/ASLSem.ml
@@ -749,12 +749,16 @@ module Make (C : Config) = struct
           and custom_implems = build `ASLv1 "implementations.asl"
           and shared = build `ASLv0 "shared_pseudocode.asl" in
           let shared =
+            (*
+             * For Some reason, do not clash with
+             * stdlib definitions that will be loaded later.
+             *)
             (List.filter
                AST.(
                  fun d ->
                    match d.desc with
-                   | D_Func { name = "Zeros" | "Ones" | "Replicate" | "Abs" ; _ } ->
-                       false
+                   | D_Func { name;_} ->
+                      not (Asllib.Builder.is_stdlib_name name)
                    | _ -> true)
                shared [@warning "-40-42"])
           in

asllib/builder.ml
@@ -156,3 +156,27 @@ let stdlib =
 
 let with_stdlib ast =
   List.rev_append (Lazy.force stdlib |> ASTUtils.no_primitive) ast
+
+let extract_name k d =
+  let open AST in
+  match d.desc with
+  | D_Func {name;_}
+    -> name::k
+  | D_TypeDecl _ ->
+     prerr_string  "Type declaration in stdlib.asl" ;
+     exit 1
+  | D_GlobalStorage _ ->
+     prerr_string "Storage declaration in stdlib.asl" ;
+     exit 1
+
+
+let is_stdlib_name =
+  let module StringSet = Set.Make(String) in
+  let set =
+    lazy
+      begin
+        let extract_names ds =
+          List.fold_left extract_name [] ds |> StringSet.of_list in
+        Lazy.force stdlib |> extract_names
+      end in
+  fun name -> StringSet.mem name (Lazy.force set)

asllib/builder.mli
@@ -25,3 +25,4 @@ val from_file_multi_version :
 
 val stdlib : unit AST.t Lazy.t
 val with_stdlib : 'a AST.t -> 'a AST.t
+val is_stdlib_name : AST.identifier -> bool

commit ba5efdc02d19a02c32033a510ea4c1110e8b160a

[asl] Faster `IsZero` function in ASL standard library
Fixes a performance regression introduced by commit c39eb9f.

asllib/libdir/stdlib.asl
@@ -147,9 +147,9 @@ begin
   return NOT Zeros(N);
 end
 
-func IsZero(x :: bits(n)) => boolean
+func IsZero(x :: bits(N)) => boolean
 begin
-  return BitCount(x) == 0;
+  return x == Zeros(N);
 end
 
 func IsOnes{N}(x :: bits(N)) => boolean

herd/ASLSem.ml
@@ -750,15 +750,22 @@ module Make (C : Config) = struct
           and shared = build `ASLv0 "shared_pseudocode.asl" in
           let shared =
             (*
-             * For Some reason, do not clash with
-             * stdlib definitions that will be loaded later.
+             * Remove from shared pseudocode the functions declared in stdlib because:
+             * 1. it avoids name clashes at type-checking time;
+             * 2. when debugging, we know what function is called;
+             * 3. stdlib functions usually out-perform their shared-pseudocode
+             *    counterparts when executed in herd.
              *)
             (List.filter
                AST.(
                  fun d ->
                    match d.desc with
                    | D_Func { name;_} ->
-                      not (Asllib.Builder.is_stdlib_name name)
+                      let is_stdlib = Asllib.Builder.is_stdlib_name name in
+                      let () =
+                        if false && is_stdlib then
+                          Printf.eprintf "Subprogram %s removed from shared\n%!" name in
+                      not is_stdlib
                    | _ -> true)
                shared [@warning "-40-42"])
           in