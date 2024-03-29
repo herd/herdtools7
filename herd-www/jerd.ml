517228edf37acf203280ac8fda4738b56cdbb69b
maranget <Luc.Maranget@inria.fr>
2024-03-29 06:04:40 +0100

    [www] Fix herd-ww build

--- herd-www/jerd.ml
+++ herd-www/jerd.ml
@@ -187,7 +187,8 @@ let run_herd bell cat litmus cfg =
       | Some (Model.Generic _|Model.File _)|None -> OptAce.Iico
       | Some (Model.CAV12 _) -> OptAce.False
     let variant = !variant
-    let precision = !precision
+    let fault_handling = !fault_handling
+    let mte_precision = !mte_precision
     let dumpallfaults = !dumpallfaults
     let byte = !byte
     let endian = !endian
