7fc896d6b45f81c17ee5cd0bfda3ec2005d000f7
Luc Maranget <Luc.Maranget@inria.fr>
2024-09-04 16:55:33 +0200

    [herd,asl] PTE accesses in ASL+VMSA mode.
    Notice that page table are accessed in PTE2 mode. That is,
    PTE accesses undergo the vrtual to physical translation,
    as the mode that is naturally compatible with existing
    ASL code.

--- lib/ASLOp.ml
+++ lib/ASLOp.ml
@@ -87,6 +87,7 @@ let pp_op1 _hexa = function
   | ToAArch64 -> "ToAArch64"
   | FromAArch64 -> "FromAArch64"
 
+
 let ( let* ) = Option.bind
 let return c = Some c
 let return_concrete s = Constant.Concrete s |> return
@@ -249,6 +250,7 @@ let do_op1 op cst =
               (* Valid *)
                 let valid = pte.AArch64PteVal.valid in
                 Some (Constant.Concrete (ASLScalar.bv_of_bit valid))
+            | _ when  is_address_mask positions -> Some cst
             | _ -> None
           end
       | Constant.Symbolic x ->
