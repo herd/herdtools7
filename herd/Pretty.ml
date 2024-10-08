19f87adb185aa89faef53049e8480e8a26051fa8
Nikos Nikoleris <nikos.nikoleris@arm.com>
2024-10-08 17:27:54 +0100

    [herd] Show SysReg events in graphs by default
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

--- herd/Pretty.ml
+++ herd/Pretty.ml
@@ -1500,13 +1500,18 @@ module Make (S:SemExtra.S) : S with module S = S  = struct
     else
       fun e -> not (E.is_mem_store_init e)
 
+  let is_sysreg e =
+    match E.location_of e with
+    | Some (A.Location_reg (_,r)) ->
+        AArch64Base.parse_sysreg (A.pp_reg r) <> None
+    | Some (A.Location_global _) | None -> false
+
   let select_event = match PC.showevents with
   | AllEvents -> (fun _ -> true)
   | MemEvents ->  E.is_mem
-  | NonRegEvents -> (fun e -> not (E.is_reg_any e))
+  | NonRegEvents -> (fun e -> is_sysreg e || not (E.is_reg_any e))
   | MemFenceEvents -> let open Misc in E.is_mem ||| E.is_barrier ||| E.is_fault
   | NonBranchEvents -> (fun e -> not (E.is_commit e))
-
   let select_event = let open Misc in select_event &&& select_non_init
 
   let select_events = E.EventSet.filter select_event
