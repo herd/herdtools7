a366b4a163279e32b11e71d26e2079d5dbc094ce
Artem Khyzha <artem.khyzha@arm.com>
2024-10-29 13:19:05 +0000

    [catalogue,aarch64] updated the shelf.py file for MTE/VMSA/ifetch

--- catalogue/aarch64-ifetch/shelf.py
+++ catalogue/aarch64-ifetch/shelf.py
@@ -17,4 +17,12 @@ illustrative_tests = [
     "tests/DIC0-IDC1/IDC1.MP.RF+dmb.st+ctrlisb-ic-dsb-isb.litmus",
     "tests/DIC1-IDC1/DIC1.MP.RF+dmb.st+ctrlisb-ic-dsb-isb.litmus",
     "tests/DIC1-IDC1/DIC1.MP.RF+dmb.st+ctrlisb.litmus",
+    "tests/DIC0-IDC0/MP+dmb.st+addrisb-Imp_Instr_R.litmus",
+    "tests/DIC0-IDC0/MP.RF+dc.cvau-dmb.ish+ctrl-ic.ivau-dsb.ish-isb.litmus",
+    "tests/DIC0-IDC0/MP.RF+dc.cvau-dmb.ish+pickctrl-ic.ivau-isb.litmus",
+    "tests/DIC0-IDC0/MP.RF+dmb.st+pickaddr-cachesyncisb.litmus",
+    "tests/DIC0-IDC1/IDC1.MP+dmb.st+addrisb-Imp_Instr_R.litmus",
+    "tests/DIC0-IDC1/IDC1.MP.FF+dsb.ish-ic.ivau-dsb.ish+po.litmus",
+    "tests/DIC1-IDC1/DIC1.MP+dmb.st+addrisb-Imp_Instr_R.litmus",
+
 ]
