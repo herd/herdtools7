5fc62bfa5bc2a7861ec1f85f98610ebbdab09445
Artem Khyzha <artem.khyzha@arm.com>
2024-12-12 13:00:17 +0000

    [herd] remove unused ifetch code

--- herd/archExtra_herd.ml
+++ herd/archExtra_herd.ml
@@ -71,8 +71,6 @@ module type S = sig
   type instr = I.V.Cst.Instr.t
   type code = (int * instr) list
 
-  val convert_if_imm_branch : int -> int -> int Label.Map.t -> int Label.Map.t -> instr -> instr
-
   (* Program loaded in memory *)
   type program = int Label.Map.t
 
@@ -324,15 +322,6 @@ module Make(C:Config) (I:I) : S with module I = I
       (* Code memory is a mapping from globals locs, to instructions *)
       type instr = I.V.Cst.Instr.t
       type code = (int * instr) list
-
-      (* This function is a default behaviour for all architectures.
-         When variant -self is enabled, it fails trying to convert a branch
-         instruction to a label into a branch-with-offset representation. *)
-      let convert_if_imm_branch _ _ _ _ i =
-        if C.variant Variant.Ifetch then
-          Warn.fatal "Functionality %s not implemented for -variant self" "convert_if_imm_branch"
-        else
-          i
 
       (* Programm loaded in memory *)
       type program = int Label.Map.t
