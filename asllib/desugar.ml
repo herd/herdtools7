7595406b17dbaadadef20d8cafa564c658a27975
Roman-Manevich <Roman.Manevich@arm.com>
2025-01-17 10:51:08 +0000

    [asl] updated begin/end markers for desugaring rules

--- asllib/desugar.ml
+++ asllib/desugar.ml
@@ -132,6 +132,7 @@ let desugar_lhs_fields_tuple base field_opts =
       LE_Destructuring (List.map desugar_one field_opts)
 
 let desugar_case_stmt e0 cases otherwise =
+  (* Begin CaseToCond *)
   let case_to_cond e0 case tail =
     let { pattern; where; stmt } = case.desc in
     let e_pattern = E_Pattern (e0, pattern) |> add_pos_from pattern in
@@ -141,6 +142,7 @@ let desugar_case_stmt e0 cases otherwise =
       | Some e_where -> binop BAND e_pattern e_where
     in
     S_Cond (cond, stmt, tail) |> add_pos_from case
+    (* End *)
   in
   (* Begin CasesToCond *)
   let cases_to_cond e0 cases =
