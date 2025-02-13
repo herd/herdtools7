28f881e51cd6e95ad8053995bf244413f2e28ddc
Roman-Manevich <Roman.Manevich@arm.com>
2025-02-13 10:49:13 +0000

    [asl][reference] removed all multiply-defined hypertargets + added check in linter script

--- asllib/doc/doclint.py
+++ asllib/doc/doclint.py
@@ -4,8 +4,15 @@ import os, sys, fnmatch
 from extended_macros import apply_all_macros
 
 import argparse
-cli_parser = argparse.ArgumentParser(prog='ASL Reference Linter')
-cli_parser.add_argument('-t', '--transform', help='Rewrites *.tex files with extended macros', action='store_true')
+
+cli_parser = argparse.ArgumentParser(prog="ASL Reference Linter")
+cli_parser.add_argument(
+    "-t",
+    "--transform",
+    help="Rewrites *.tex files with extended macros",
+    action="store_true",
+)
+
 
 def extract_labels_from_line(line: str, left_delim: str, labels: set[str]):
     r"""
@@ -25,14 +32,16 @@ def extract_labels_from_line(line: str, left_delim: str, labels: set[str]):
         labels.add(label)
         label_pos = right_brace_pos + 1
 
+
 def check_hyperlinks_and_hypertargets():
     r"""
     Checks whether all labels defined in `\hyperlink` definitions match
-    labels defined in `\hypertarget` definitions, print the mismatches to the console.
+    labels defined in `\hypertarget` definitions, print the mismatches
+    to the console.
     """
-    latex_files = fnmatch.filter(os.listdir('.'), '*.tex')
-    hyperlink_labels : set[str] = set()
-    hypertarget_labels : set[str] = set()
+    latex_files = fnmatch.filter(os.listdir("."), "*.tex")
+    hyperlink_labels: set[str] = set()
+    hypertarget_labels: set[str] = set()
     for latex_source in latex_files:
         with open(latex_source) as file:
             for line in file.readlines():
@@ -43,7 +52,11 @@ def check_hyperlinks_and_hypertargets():
     if missing_hypertargets:
         num_missing_hypertargets = len(missing_hypertargets)
         num_errors += num_missing_hypertargets
-        print(f"ERROR: found {num_missing_hypertargets} hyperlinks without matching hypertargets: ", file=sys.stderr)
+        print(
+            f"ERROR: found {num_missing_hypertargets} hyperlinks without \
+              matching hypertargets: ",
+            file=sys.stderr,
+        )
         for label in missing_hypertargets:
             print(label, file=sys.stderr)
 
@@ -51,11 +64,16 @@ def check_hyperlinks_and_hypertargets():
     if not missing_hyperlinks == set():
         num_missing_hyperlinks = len(missing_hyperlinks)
         num_errors += num_missing_hyperlinks
-        print(f"ERROR: found {num_missing_hyperlinks} hypertargets without matching hyperlinks: ", file=sys.stderr)
+        print(
+            f"ERROR: found {num_missing_hyperlinks} hypertargets without\
+               matching hyperlinks: ",
+            file=sys.stderr,
+        )
         for label in missing_hyperlinks:
             print(label, file=sys.stderr)
 
     return num_errors
+
 
 def check_undefined_references_and_multiply_defined_labels():
     r"""
@@ -66,12 +84,27 @@ def check_undefined_references_and_multiply_defined_labels():
     with open("./ASLReference.log") as file:
         log_str = file.read()
         if "LaTeX Warning: There were undefined references." in log_str:
-            print(f"ERROR: There are undefined references (see ./ASLReference.log)", file=sys.stderr)
+            print(
+                f"ERROR: There are undefined references (see ./ASLReference.log)",
+                file=sys.stderr,
+            )
             num_errors += 1
         if "LaTeX Warning: There were multiply-defined labels." in log_str:
-            print(f"ERROR: There are multiply-defined labels (see ./ASLReference.log)", file=sys.stderr)
+            print(
+                f"ERROR: There are multiply-defined labels (see ./ASLReference.log)",
+                file=sys.stderr,
+            )
+            num_errors += 1
+        if "destination with the same identifier" in log_str:
+            print(
+                f"ERROR: There are multiply-defined \\hypertarget labels \
+                  (see 'destination with the same identifier' in \
+                  ./ASLReference.log)",
+                file=sys.stderr,
+            )
             num_errors += 1
     return num_errors
+
 
 def check_tododefines():
     r"""
@@ -80,24 +113,28 @@ def check_tododefines():
     """
     MAX_TODODEFINE_INSTANCES = 7
     num_todo_define = 0
-    latex_files = fnmatch.filter(os.listdir('.'), '*.tex')
+    latex_files = fnmatch.filter(os.listdir("."), "*.tex")
     for latex_source in latex_files:
         with open(latex_source) as file:
             file_str = file.read()
-            num_todo_define += file_str.count('\\tododefine')
-    num_todo_define -= 1 # Ignore the definition of the \tododefine macro itself.
+            num_todo_define += file_str.count("\\tododefine")
+    num_todo_define -= 1  # Ignore the definition of the \tododefine macro itself.
     if num_todo_define > MAX_TODODEFINE_INSTANCES:
         # Disallow adding new \tododefines
-        print(f"ERROR: There are {num_todo_define} occurrences of \\tododefine, expected at most {MAX_TODODEFINE_INSTANCES}")
+        print(
+            f"ERROR: There are {num_todo_define} occurrences of \\tododefine,\
+               expected at most {MAX_TODODEFINE_INSTANCES}"
+        )
         return num_todo_define
     else:
         print(f"WARNING: There are {num_todo_define} occurrences of \\tododefine")
         return 0
 
+
 def check_repeated_words():
     last_word = ""
     num_errors = 0
-    latex_files = fnmatch.filter(os.listdir('.'), '*.tex')
+    latex_files = fnmatch.filter(os.listdir("."), "*.tex")
     for latex_source in latex_files:
         with open(latex_source) as file:
             line_number = 0
@@ -108,17 +145,24 @@ def check_repeated_words():
                 if len(parts) < 2:
                     continue
                 for current_word in parts:
-                    if current_word.isalpha() and last_word.lower() == current_word.lower():
+                    if (
+                        current_word.isalpha()
+                        and last_word.lower() == current_word.lower()
+                    ):
                         num_errors += 1
-                        print(f"./{latex_source} line {line_number}: word repetition ({last_word} {current_word}) in '{line}'")
+                        print(
+                            f"./{latex_source} line {line_number}: \
+                              word repetition ({last_word} {current_word}) in '{line}'"
+                        )
                     last_word = current_word
     return num_errors
+
 
 def main():
     args = cli_parser.parse_args()
     if args.transform:
         apply_all_macros()
-    print('Linting files...')
+    print("Linting files...")
     num_errors = 0
     num_errors += check_hyperlinks_and_hypertargets()
     num_errors += check_undefined_references_and_multiply_defined_labels()
@@ -128,6 +172,7 @@ def main():
     if num_errors > 0:
         print(f"There were {num_errors} errors!", file=sys.stderr)
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
