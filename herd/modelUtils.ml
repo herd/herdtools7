52fb5d097af8e4aeb4f919f188f9b780df980e44
Luc Maranget <Luc.Maranget@inria.fr>
2025-02-17 16:06:09 +0100

    [herd] Explicit address ports, X86_64 semantics

--- herd/modelUtils.ml
+++ herd/modelUtils.ml
@@ -21,9 +21,13 @@ module Make(O:Model.Config) (S:SemExtra.S) = struct
   let memtag = O.variant Variant.MemTag
   let morello = O.variant Variant.Morello
   let do_deps = O.variant Variant.Deps
-  let iico_ctrl_as_dep = match S.A.arch with
+  let is_intel = match S.A.arch with
+    | `X86_64|`X86 -> true
+    | _ -> false
+  let is_aarch64 = match S.A.arch with
   | `AArch64 -> true
   | _ -> false
+  let iico_ctrl_as_dep = is_aarch64
   and kvm = O.variant Variant.VMSA
 
   let is_mem_kvm =
@@ -116,7 +120,18 @@ module Make(O:Model.Config) (S:SemExtra.S) = struct
              (E.is_store e2 && is_addr_port e1))) ||
             E.is_additional_mem e2)
            (* Patch: a better solution would be a direct iico from read address register to access *)
-          (if memtag || kvm || morello then E.EventRel.transitive_closure iico else iico) in
+          (if memtag || kvm || morello then
+              E.EventRel.transitive_closure iico
+           else if is_intel then
+             E.EventRel.union iico
+               (E.EventRel.sequence
+                  (E.EventRel.restrict_rel
+                     (fun _ e2 -> E.is_mem_load e2)
+                     iico)
+                  (E.EventRel.restrict_rel
+                     (fun e1 e2 -> E.is_mem_load e1 && E.is_mem_store e2)
+                     iico))
+           else iico) in
       S.union
         (E.EventRel.restrict_domain is_mem_load_total last_addr)
         (S.seq dd_pre last_addr) in
