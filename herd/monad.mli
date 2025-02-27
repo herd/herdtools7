d69f1b830c4c1ac7f5b7ab407598f0cbddf70fb8
Nikos Nikoleris <nikos.nikoleris@arm.com>
2025-02-27 16:31:32 +0000

    [herd] Avoid code replication for aarch64 cas semantics
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

--- herd/monad.mli
+++ herd/monad.mli
@@ -205,13 +205,6 @@ module type S =
                 unit t
 
     val aarch64_cas_no :
-      bool -> (* physical access *)
-        'loc t -> 'v t ->
-          ('v -> unit t) -> ('loc -> 'v t) ->
-           ('loc -> unit t) -> unit t ->
-            ('v -> 'v -> unit t) -> unit t
-
-    val aarch64_cas_no_with_writeback :
           bool -> (* physical access *)
             'loc t -> 'v t ->
               ('v -> unit t) -> ('loc -> 'v t) -> ('loc -> 'v -> unit t) ->
