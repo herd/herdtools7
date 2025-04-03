da7b671ee00e455a93025029c41b3bc8d5bacb98
Shale Xiong <xiongshale@gmail.com>
2025-04-03 11:04:46 +0100

    Improve code quality in diy.
    - Rewrite code using standard library such as `List.map` and `List.fold_left`.
    - Make some explanatory comments to single letter variable and key
      functions.
    - Remove some extra white space.

--- gen/alt.ml
+++ gen/alt.ml
@@ -305,6 +305,9 @@ module Make(C:Builder.S)
         | _ -> true
         end
 
+    (* List.is_empty only supports for ocaml 5.1 afterwards *)
+    let is_empty_list l = (l = [])
+
     let pp_ess ess =
       String.concat " "
         (List.fold_right
@@ -322,27 +325,23 @@ module Make(C:Builder.S)
     let sz (_,es) =
       if List.for_all (fun e -> is_id e.edge) es then 0 else 1
 
+    let c_minprocs_es c =
+      List.fold_left ( fun c e ->
+        match e.C.E.edge with
+        | Back _|Leave _ -> c
+        | _ -> match get_ie e with
+          | Int -> c
+          (* only inc when `Ext` *)
+          | Ext -> c + 1
+      ) c
 
-    let rec c_minprocs_es c = function
-      | [] -> c
-      | e::es ->
-          let c = match e.C.E.edge with
-          | Back _|Leave _ -> c
-          | _ ->
-              match get_ie e with
-              | Int -> c
-              | Ext -> c+1 in
-          c_minprocs_es c es
-
-    let rec c_minprocs_suff c = function
-      | [] -> c
-      | (_,es)::suff -> c_minprocs_suff (c_minprocs_es c es) suff
+    let c_minprocs_suff c =
+      List.fold_left ( fun c (_,es) -> c_minprocs_es c es) c
 
     let minprocs suff =
       let r = c_minprocs_suff 0 suff in
       if O.verbose > 3 then eprintf "MIN [%s] => %i\n" (pp_ess suff) r ;
       r
-
 
     let rec c_minint_es c = function
       | [] -> false,c
@@ -464,35 +463,29 @@ module Make(C:Builder.S)
       let safe = edges_ofs safe in
       let po_safe = extract_po safe in
 
-      let rec choose_relax rs k = match rs with
-      | [] -> k
-      | r0::rs -> (* Build simple cycles for relaxation r0 *)
+      (* iterates over all relax edges `rs` *)
+      let choose_relax rs k =
+      List.fold_left (fun k relex_edge ->
+        (* Build simple cycles for relaxation `relex_edge` *)
+        (* Partially apply function `call_rec` *)
+        let call_rec = call_rec prefix (f [fst relex_edge]) aset po_safe ~reject:reject in
 
-          let call_rec = call_rec prefix (f [fst r0]) aset po_safe ~reject:reject in
+(* Add safe edge to suffix *)
+        let rec add_safe over ss n suf k =
+          List.fold_left ( fun k s -> call_rec over n s suf (add_relaxs over) k ) k ss
+(* Addome relax edges `relex_edge` to suffix, or nothing *)
+        and add_relaxs over n suf k =
+          let k = call_rec true n relex_edge suf (add_relaxs true) k in
+          add_safe over safe n suf k in
 
-(* Add a safe edge to suffix *)
-          let rec add_safe over ss n suf k =
-            match ss with
-            | [] -> k
-            | s::ss ->
-                let k = call_rec over n s suf (add_relaxs over) k in
-                add_safe over ss n suf k
-
-(* Add some relax edges r0 to suffix, or nothing *)
-          and add_relaxs over n suf k =
-            let k = call_rec true n r0 suf (add_relaxs true) k in
-            add_safe over safe n suf k in
-
-          match prefix with
-          | [] -> (* Optimise: start with a relax edge r0 *)
-              let k = call_rec true n r0 [] (add_relaxs true) k in
-              choose_relax rs k
-          | _::_ ->
-              let k = add_relaxs false n [] k in
-              choose_relax rs k in
+        if is_empty_list prefix then
+          (* Optimise: start with a relax edge `relex_edge` *)
+            call_rec true n relex_edge [] (add_relaxs true) k
+        else
+            add_relaxs false n [] k
+      ) k rs in
 
 (* Alternative: mix relaxation from relax list *)
-
       let all_relax k =
         let relax_set = RelaxSet.of_list (List.map fst relax) in
         let extract_relaxs suff =
@@ -507,59 +500,43 @@ module Make(C:Builder.S)
               if nrs > O.max_relax || nrs < O.min_relax then k
               else f rs po_safe suff k)
             aset po_safe ~reject:reject in
-
 (* Add a one edge to suffix *)
-        let rec add_one over rs ss n suf k = match rs,ss with
-        | [],[] -> k
-        | [],s::ss ->
-            let k = call_rec over n s suf (add_one over relax safe) k in
-            add_one over rs ss n suf k
-        | r::rs,_ ->
-            let k = call_rec true n r suf (add_one true relax safe) k in
-            add_one over rs ss n suf k in
-
-
+        let rec add_one over rs ss n suf k =
+          let new_k = List.fold_left ( fun k r ->
+            call_rec true n r suf (add_one true relax safe) k
+          ) k rs in
+          List.fold_left ( fun k s ->
+            call_rec over n s suf (add_one over relax safe) k
+          ) new_k ss in
 (* Force first edge to be a relaxed one *)
-        let rec add_first rs k = match rs with
-        | [] -> k
-        | r::rs ->
-            let k = call_rec true n r [] (add_one true relax safe) k in
-            add_first rs k in
-
-        match prefix with
-        | [] ->
-            add_first relax k
-        | _::_ ->
-            add_one false relax safe n [] k in
+        let add_first rs k =
+          List.fold_left ( fun k r ->
+            call_rec true n r [] (add_one true relax safe) k
+          ) k rs in
+        (* Function `all_relax` entry point depends on prefix *)
+        if is_empty_list prefix then add_first relax k
+        else add_one false relax safe n [] k in
 
 (* New relax that does not enforce the first edge to be a relax *)
 
 (* As a safety check, generate cycles with no relaxation *)
-      let call_rec = call_rec prefix (f []) aset po_safe ~reject:reject in
-      let rec no_relax ss n suf k = match ss with
-      | [] -> k
-      | s::ss ->
-          let k = call_rec true n s suf (no_relax safe) k in
-          no_relax ss n suf k in
+      let rec no_relax ss n suf k =
+        List.fold_left (fun k s ->
+          call_rec prefix (f []) aset po_safe true n s suf (no_relax safe) k ~reject:reject
+        ) k ss in
 
-      fun k -> match relax with
-      | [] ->
-          no_relax safe n [] k
-      | _  ->
-         if O.mix && O.max_relax < 1 then k (* Let us stay logical *)
-          else if O.mix && O.max_relax > 1 then
-            all_relax k
-          else
-            choose_relax relax k
+      (* Function `zyva` starts after all the `let`-bindings *)
+      fun k ->
+        if is_empty_list relax then no_relax safe n [] k
+        else if O.mix && O.max_relax < 1 then k (* Let us stay logical *)
+        else if O.mix && O.max_relax > 1 then all_relax k
+        else choose_relax relax k
+      (* END of overall `zyva` *)
 
-    let rec all_int l =
-      match l with
-      | [] -> true
-      | a::s -> (is_int a)&&(all_int s)
+    let all_int l = List.for_all is_int l
 
-    let rec count_e ce = function
-      | [] -> ce
-      | e::es -> count_e (if is_int e then ce else ce+1) es
+    let count_e ce =
+      List.fold_left ( fun ce e -> if is_int e then ce else ce + 1 ) ce
 
 
     let count_ext es = count_e 0 es
@@ -569,10 +546,7 @@ module Make(C:Builder.S)
     | Diff -> true
 
     let count_p p =
-      let rec do_rec c = function
-        | [] -> c
-        | x::xs -> do_rec (if p x then c+1 else c) xs in
-      do_rec 0
+      List.fold_left ( fun acc x -> if p x then acc + 1 else acc ) 0
 
     let count_changes = count_p change_loc
 
@@ -609,9 +583,7 @@ module Make(C:Builder.S)
       rej
 
     let last_check_call rej aset f rs po_safe res k =
-      match res with
-      | [] -> k
-      | _ ->
+      if is_empty_list res then k else
           let lst = Misc.last res in
           if can_precede aset po_safe lst res then
             let es = List.map snd res in
@@ -685,12 +657,10 @@ module Make(C:Builder.S)
           | [] -> true
         end
 
-    let rec zyva_prefix prefixes aset relax safe reject n f k =
-      match prefixes with
-      | [] -> k
-      | pref::rem ->
-         zyva pref aset relax safe reject n f
-            (zyva_prefix rem aset relax safe reject n f k )
+    (* TODO: explain the parameter here.
+       Parameter `f` is a function check the result? *)
+    let zyva_prefix prefixes aset relax safe reject n f k =
+      List.fold_left ( fun k pref -> zyva pref aset relax safe reject n f k ) k prefixes
 
     let do_gen relax safe rej n =
       let sset = C.R.Set.of_list safe in
--- gen/top_gen.ml
+++ gen/top_gen.ml
@@ -196,11 +196,20 @@ let get_fence n =
           f::fs,ns
       | _ -> [],all
 
+  (* - `ro_prev` if there is a previous load
+     - `st` is the machine state
+     - `chk` if an initial is needed
+     - `p` procedure (number)
+     - `ro_prev` carried if the previous related load
+     - `init` the inital values
+     - `ns` input node list
+  *)
   let rec compile_proc pref chk loc_writes st p ro_prev init ns = match ns with
   | [] -> init,pref [],(C.EventMap.empty,[]),st
   | n::ns ->
       if O.verbose > 1 then eprintf "COMPILE PROC: <%s>\n" (C.str_node n);
       begin match  n.C.edge.E.edge with
+      (* There are following fences *)
       | E.Node _ ->
           let fs,ns =  collect_inserts ns in
           compile_proc
@@ -211,6 +220,7 @@ let get_fence n =
                    in cs@is)
                    fs is))
             chk loc_writes st p ro_prev init ns
+      (* A single fence *)
       | E.Insert f ->
           let ro_prev,init,cs,st, n1 = match ro_prev with
           | No  -> let init, cs, st = Comp.emit_fence st p init n f in
@@ -243,19 +253,19 @@ let get_fence n =
           mk_c (cf@is),
           (match n.C.evt.C.loc with
           | Data loc ->
-              let call_add =
-                StringSet.mem loc loc_writes  && not (U.do_poll n) in
-              if call_add then
-                F.add_final (A.get_friends st) p o n finals
-              else finals
+            let call_add =
+              StringSet.mem loc loc_writes  && not (U.do_poll n) in
+            if call_add then
+              F.add_final (A.get_friends st) p o n finals
+            else finals
           | Code _ ->
-              begin match o with
-              | None   -> finals (* Code write *)
-              | Some r -> (* fetch! *)
-                  let m,fenv =  finals in
-                  m,F.add_final_v p r (IntSet.singleton (Code.value_to_int (U.fetch_val n)))
-                    fenv
-              end),
+            begin match o with
+            | None   -> finals (* Code write *)
+            | Some r -> (* fetch! *)
+              let m,fenv =  finals in
+              m,F.add_final_v p r (IntSet.singleton @@ Code.value_to_int @@ U.fetch_val n)
+                fenv
+            end),
           st
       end
   (* END of compile_proc *)
@@ -405,6 +415,7 @@ let max_set = IntSet.max_elt
               i,c::cs,f@fs
         with NoObserver -> build_observers p i x vss
 
+  (* `env_wide` is a lookup table for the widths of locations and `atoms` is a set of all atom *)
   let check_writes env_wide atoms =
 
     let call_build_observers p i x vs =
@@ -433,65 +444,72 @@ let max_set = IntSet.max_elt
       if (not (StringSet.mem loc atoms) && O.optcond) then k
       else cons_one loc v k in
 
-    let rec check_rec p i =
-
+    (* - `p`, process number,
+       - `i`. initial value,
+       - `xvs`, type `U.cos`, the final values of write events for all locations *)
+    let check p i xvs =
       let open Config in
-      function
-      | [] -> i,[],[]
-      (* `vs` : (IntSet.elt array * IntSet.t) list list *)
-      | (x,vs)::xvs ->
-         let vs = List.map ( List.map ( fun (v, vset) -> (Array.map Code.value_to_int v, vset) ) ) vs in
-         let i,c,f = match O.cond with
-           | Observe ->
-              let vs = List.flatten vs in
-              begin match vs with
-              | [] -> i,[],[]
-              | _::_ ->
-                  let v,_ = Misc.last vs in
-                  i,[],cons_one x v []
-              end
+      let _p,i,cs,fs = List.fold_left (fun (p, i, cs, fs) (x, vs) ->
+        (* `vs` : (IntSet.elt array * IntSet.t) list list *)
+        let vs = List.map ( List.map
+            ( fun (v, vset) -> (Array.map Code.value_to_int v, vset) )
+          ) vs in
+        let i,c,f = match O.cond with
+          | Observe ->
+            let vs = List.flatten vs in
+            begin match vs with
+            | [] -> i,[],[]
+            | _::_ ->
+              let v,_ = Misc.last vs in
+              i,[],cons_one x v []
+            end
           | Unicond -> assert false
+          (* common branch *)
           | Cycle -> begin
-              match vs with
-              | [] -> i,[],[]
-              | [[(v,_)]] -> i,[],add_look_loc x v []
-              | [[_;(v,_)]] ->
-                  begin match O.do_observers with
-                  | Local -> i,[],add_look_loc x v []
-                  | Avoid|Accept|Three|Four|Infinity
-                    -> i,[],cons_one x v []
-                  | Enforce ->
-                      let i,c,f = call_build_observers p i x vs in
-                      i,c,add_look_loc x v f
-                  end
+            match vs with
+            | [] -> i,[],[]
+            (* the common case with one write event *)
+            | [[(v,_)]] -> i,[],add_look_loc x v []
+            | [[_;(v,_)]] ->
+              begin match O.do_observers with
+              | Local -> i,[],add_look_loc x v []
+              | Avoid|Accept|Three|Four|Infinity
+                -> i,[],cons_one x v []
+              | Enforce ->
+                let i,c,f = call_build_observers p i x vs in
+                i,c,add_look_loc x v f
+              end
+            | _ ->
+              let vs_flat = List.flatten vs in
+              let v,_ = Misc.last vs_flat in
+              begin match O.do_observers with
+              | Local -> i,[],add_look_loc x v []
+              | Three ->
+                begin match vs_flat with
+                | _x1::_x2::_x3::_x4::_ ->
+                  Warn.fatal "More than three writes"
+                | _ -> i,[],cons_one x v []
+                end
+              |Four ->
+                begin match vs_flat with
+                | _x1::_x2::_x3::_x4::_x5::_ ->
+                  Warn.fatal "More than four writes"
+                | _ -> i,[],cons_one x v []
+                end
+              | Infinity -> i,[],cons_one x v []
               | _ ->
-                  let vs_flat = List.flatten vs in
-                  let v,_ = Misc.last vs_flat in
-                  begin match O.do_observers with
-                  | Local -> i,[],add_look_loc x v []
-                  | Three ->
-                      begin match vs_flat with
-                      | _x1::_x2::_x3::_x4::_ ->
-                          Warn.fatal "More than three writes"
-                      | _ -> i,[],cons_one x v []
-                      end
-                  |Four ->
-                      begin match vs_flat with
-                      | _x1::_x2::_x3::_x4::_x5::_ ->
-                          Warn.fatal "More than four writes"
-                      | _ -> i,[],cons_one x v []
-                      end
-                  | Infinity ->
-                      i,[],cons_one x v []
-                  | _ ->
-                      let i,c,f = call_build_observers p i x vs in
-                      i,c,add_look_loc x v f
-                  end
-          end in
-          let i,cs,fs =
-            check_rec (p+List.length c) i xvs in
-          i,c@cs,f@fs in
-     check_rec
+                let i,c,f = call_build_observers p i x vs in
+                i,c,add_look_loc x v f
+              end
+        end in
+        (* Update the procedure number, carry over the new `i`,
+           and accumulating the new results of `c` and `f` *)
+        (p+List.length c), i, cs@c, fs@f
+      ) (p, i, [], []) xvs in
+      i,cs,fs in
+      (* END of check definition *)
+    check
+(* END of check_writes *)
 
   let compile_store st p init n =
     let ro,init,c,st = call_emit_access st p init n in
@@ -677,6 +695,7 @@ let max_set = IntSet.max_elt
     let no_local_ptes = StringSet.of_list (List.map fst last_ptes) in
     if O.verbose > 1 then U.pp_coherence cos0 ;
     let loc_writes = U.comp_loc_writes n in
+    (* `do_rec` compile individual instructions *)
     let rec do_rec p i = function
       | [] -> List.rev i,[],(C.EventMap.empty,[]),[],A.LocMap.empty
       | n::ns ->
@@ -713,6 +732,7 @@ let max_set = IntSet.max_elt
                    (A.pp_location loc)
                    (TypBase.pp t1) (TypBase.pp t2))
           env_p env in
+    (* end of `do_rec` *)
     let i,obsc,f =
       match O.cond with
       | Unicond -> [],[],[]
@@ -732,6 +752,11 @@ let max_set = IntSet.max_elt
           then
             let ess = List.map (List.map (fun n -> n.C.edge)) splitted in
             if ok ess then
+              (* - `i` is the initial value.
+                 - `cs` pseudo code list
+                 - `m` location to its last affect events
+                 - `fs` final state
+              *)
               let i,cs,(m,fs),ios,env =
                 do_rec (List.length obsc) i splitted in
               i,obsc@cs,(m,f@fs),ios,env
@@ -828,6 +853,7 @@ let max_set = IntSet.max_elt
         (* Add the all the pair in `last_ptes` into the post-condition `final_env` *)
         let f =
           List.fold_left (fun f (x,p) -> F.cons_pteval (A.Loc x) p f) f last_ptes in
+        (* `fc` converts faults `flts` and final values of registers `final_env` to final *)
         let fc =
           match O.cond with
           | Unicond ->
@@ -842,6 +868,7 @@ let max_set = IntSet.max_elt
         (i,c,fc flts,env),
         (U.compile_prefetch_ios (List.length obsc) ios,
          U.compile_coms splitted)
+  (* END of compile_cycle *)
 
 
 (********)
