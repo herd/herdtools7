5bf1f181bb2c57a789d6b31be642981f997fbc06
augustin64 <me.git@augustin64.fr>
2025-06-09 12:58:33 +0100

    [diymicro] Add all non-M cas iico

--- diymicro/iico.ml
+++ diymicro/iico.ml
@@ -8,8 +8,9 @@ module A = struct
   include AArch64_compile
 end
 
-let casRsRs ok st dep =
-  let src =
+let cas src dst ok =
+ fun st dep ->
+  let src_reg =
     match dep with
     | DepReg r -> r
     | _ -> Warn.fatal "Event has not forwarded any register"
@@ -22,35 +23,45 @@ let casRsRs ok st dep =
   let st = A.set_initial st loc (if ok then 0 else 1) in
   let st = A.add_condition st rcheck (if ok then 2 else 1) in
 
-  let ins =
-    A.pseudo [A.do_eor rs src src]
-    @ [A.mov rt 2]
-    @ A.pseudo [A.cas A.RMW_P rs rt rn; A.do_ldr A.vloc rcheck rn]
+  let pre_ins, st =
+    match src with
+    | `Rs -> A.pseudo [A.do_eor rs src_reg src_reg] @ [A.mov rt 2], st
+    | `Rt -> A.pseudo [A.do_eor rt src_reg src_reg; A.addi rt rt 2], st
+    | `Rn ->
+        let r_eor, st = A.next_reg st in
+        let ins =
+          A.pseudo
+            [A.do_eor r_eor src_reg src_reg; A.do_add64 A.vloc rn rn r_eor]
+          @ [A.mov rt 2]
+        in
+        ins, st
   in
 
-  ins, DepReg rs, st
+  let ins = A.pseudo [A.cas A.RMW_P rs rt rn; A.do_ldr A.vloc rcheck rn] in
 
-let casRsRt ok st dep =
-  let src =
-    match dep with
-    | DepReg r -> r
-    | _ -> Warn.fatal "Event has not forwarded any register"
-  in
-  let rs, st = A.next_reg st in
-  let rt, st = A.next_reg st in
-  let loc, rn, st = A.assigned_next_loc st in
-  let rcheck, st = A.next_reg st in
-
-  let st = A.set_initial st loc (if ok then 0 else 1) in
-  let st = A.add_condition st rcheck (if ok then 2 else 1) in
-
-  let ins =
-    A.pseudo [A.do_eor rs src src]
-    @ [A.mov rt 2]
-    @ A.pseudo [A.cas A.RMW_P rs rt rn; A.do_ldr A.vloc rcheck rn]
+  let post_ins, st, dst_dep =
+    match dst with (* dst Rn and Rt are not really interesting, but we still implement them *)
+    | `Rs -> [], st, DepReg rs
+    | `Rt -> [], st, DepReg rt
+    | `Rn ->
+        let rn_reg, st = A.next_reg st in
+        A.pseudo [A.do_ldr A.vloc rn_reg rn], st, DepReg rn_reg
   in
 
-  ins, DepReg rt, st
+  pre_ins @ ins @ post_ins, dst_dep, st
+
+let casRepr ok src dst =
+  "cas:"
+  ^ (if ok then "ok" else "no")
+  ^ " "
+  ^ (match src with `Rs -> "Rs" | `Rn -> "Rn" | `Rt -> "Rt")
+  ^ "->"
+  ^ match dst with `Rs -> "Rs" | `Rn -> "Rn" | `Rt -> "Rt"
+
+let cartesian3 l1 l2 l3 =
+  List.concat_map
+    (fun x -> List.concat_map (fun y -> List.map (fun z -> x, y, z) l3) l2)
+    l1
 
 let init () =
   add_iico (* Example edge *)
@@ -64,46 +75,16 @@ let init () =
       significant_dest = false;
     };
 
-  add_iico
-    {
-      repr = "cas:no Rs->Rs";
-      compile_edge = casRsRs false;
-      direction = Rr, Wr;
-      ie = Internal;
-      sd = Same;
-      significant_source = false;
-      significant_dest = false;
-    };
-
-  add_iico
-    {
-      repr = "cas:ok Rs->Rs";
-      compile_edge = casRsRs true;
-      direction = Rr, Wr;
-      ie = Internal;
-      sd = Same;
-      significant_source = false;
-      significant_dest = false;
-    };
-
-  add_iico
-    {
-      repr = "cas:no Rs->Rt";
-      compile_edge = casRsRt false;
-      direction = Rr, Wr;
-      ie = Internal;
-      sd = Same;
-      significant_source = false;
-      significant_dest = false;
-    };
-
-  add_iico
-    {
-      repr = "cas:ok Rs->Rt";
-      compile_edge = casRsRt true;
-      direction = Rr, Wr;
-      ie = Internal;
-      sd = Same;
-      significant_source = false;
-      significant_dest = false;
-    }
+  List.iter
+    (fun (ok, src, dst) ->
+      add_iico
+        {
+          repr = casRepr ok src dst;
+          compile_edge = cas src dst ok;
+          direction = Rr, Wr;
+          ie = Internal;
+          sd = Same;
+          significant_source = false;
+          significant_dest = false;
+        })
+    (cartesian3 [true; false] [`Rn; `Rs; `Rt] [`Rn; `Rs; `Rt])
