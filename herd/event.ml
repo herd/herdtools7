069ef8ac114e08f2cba5dd46e57b108766ab5e51
Nikos Nikoleris <nikos.nikoleris@arm.com>
2025-06-19 15:04:51 +0100

    [herd] Add SysReg events for AArch64
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

--- herd/event.ml
+++ herd/event.ml
@@ -116,6 +116,7 @@ val same_instance : event -> event -> bool
   val is_reg_store_any : event -> bool
   val is_reg_load_any : event -> bool
   val is_reg_any : event -> bool
+  val is_sysreg : event -> bool
 
 (* Store/Load to memory or register *)
   val is_store : event -> bool
@@ -724,6 +725,7 @@ module Make  (C:Config) (AI:Arch_herd.S) (Act:Action.S with module A = AI) :
     let is_reg_any e = Act.is_reg_any e.action
     let is_reg_store_any e = Act.is_reg_store_any e.action
     let is_reg_load_any e = Act.is_reg_load_any e.action
+    let is_sysreg e = Act.is_sysreg e.action
 
 (* Compatible events ie accesses of the same category *)
     let compatible_accesses e1 e2 =
