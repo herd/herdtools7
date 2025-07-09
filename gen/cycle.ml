ee8aaac64dbba1818de22143dccf47dd0bd2bc31
Shale Xiong <shale.xiong@arm.com,xiongshale@gmail.com>
2025-07-09 14:18:59 +0100

    [gen] Allow pseudo edges followed by internal communication edges.
    This means cycles such as "TLBI-sync.ISHsWR ISB Fri" will be allowed.

--- gen/cycle.ml
+++ gen/cycle.ml
@@ -750,8 +750,8 @@ let remove_store n0 =
       let p = find_non_pseudo_prev m.prev
       and n = find_non_pseudo m.next in
 (*      eprintf "[%a] in [%a]..[%a]\n" debug_node m debug_node p debug_node n ; *)
-      if not (E.is_ext p.edge || E.is_ext n.edge) then begin
-        Warn.fatal "Insert pseudo edge %s appears in-between  %s..%s (at least one neighbour must be an external edge)"
+      if not (E.is_com p.edge || E.is_com n.edge) then begin
+        Warn.fatal "Insert pseudo edge %s appears in-between  %s..%s (at least one neighbour must be a communication edge)"
           (E.pp_edge m.edge)  (E.pp_edge p.edge)  (E.pp_edge n.edge)
       end;
       match p.edge.E.edge with
