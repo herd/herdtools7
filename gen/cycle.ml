079fccd3062c5ff298f64b6b4f97fd15a81587c6
Shale Xiong <shale.xiong@arm.com,xiongshale@gmail.com>
2025-07-09 16:43:36 +0100

    [gen] Print more debug info for `node` type in `cycle.ml`.

--- gen/cycle.ml
+++ gen/cycle.ml
@@ -251,13 +251,16 @@ module Make (O:Config) (E:Edge.S) :
     String.concat ", " @@ List.map debug_val @@ Array.to_list v
 
   let debug_evt e =
-    sprintf "%s%s %s %s%s%s%s%s"
+    sprintf "#[%d] %s%s %s %s %s%s%s%s%s fault_check:%s value_check:%s"
+      e.idx
       (debug_dir e.dir)
       (debug_atom e.atom)
       (Code.pp_loc e.loc)
-      (match debug_vec e.cell with
-       | "" -> "" | s -> "cell=[" ^ s ^"] ")
+      ( if e.rmw then "rmw" else "" )
+      ( match debug_vec e.cell with | "" -> "" | s -> "cell=[" ^ s ^"] ")
       (debug_val e.v) (debug_tag e) (debug_morello e) (debug_vector e)
+      ( match e.check_fault with | Some (_,b) -> sprintf "%b" b | None -> "none" )
+      ( match e.check_value with | Some b -> sprintf "%b" b | None -> "none" )
 
   let debug_edge = E.pp_edge
 
@@ -1537,7 +1540,7 @@ let merge_changes n nss =
     if
       not O.allow_back &&
       List.exists proc_back nss
-    then Warn.fatal "Forbidden po vs. com" ;
+    then Warn.fatal "Forbidden po vs. com";
     if O.verbose > 1 then begin
       eprintf "SPLITTED:\n" ; debug_procs nss
     end ;
