79b9ce0fa168d73b04ee49e9e626215aeef6ce11
Luc Maranget <Luc.Maranget@inria.fr>
2025-08-01 17:03:11 +0200

    [build] Enable warning 9, except in asllib
    Warning 9 "missing-record-field-pattern"
    flags missing fields in a record pattern.
    
    I view it as usefull to find missing fields in complete
    record patterns when some field is added to a record.
    
    Notice that uncomplete record patterns are still possible
    by ending patterns with the wildcard `_`.
    
    As there are numerous such uncomplete patterns in `asllib`
    warning 9 remains disabled there.

--- lib/AArch64Op.ml
+++ lib/AArch64Op.ml
@@ -109,8 +109,8 @@ module
     (* Check that the PAC field of a virtual address is canonical *)
     let checkCanonical =
       let open Constant in function
-      | Symbolic (Virtual {pac}) ->
-          Some (boolToCst (PAC.is_canonical pac))
+      | Symbolic (Virtual a) ->
+          Some (boolToCst (PAC.is_canonical a.pac))
       | _ ->
           None
 
@@ -150,9 +150,9 @@ module
     let addOnePAC key pointer modifier =
       let open Constant in
       match pointer with
-      | Symbolic (Virtual {pac}) when not (PAC.is_canonical pac) ->
+      | Symbolic (Virtual a) when not (PAC.is_canonical a.pac) ->
           None
-      | Symbolic (Virtual ({pac; offset} as v)) ->
+      | Symbolic (Virtual ({pac; offset; _} as v)) ->
         let modifier = pp_cst true modifier in
         let pac = PAC.add key modifier offset pac in
         Some (Symbolic (Virtual {v with pac}))
@@ -166,7 +166,7 @@ module
     let addPAC key pointer modifier =
       let open Constant in
       match pointer with
-      | Symbolic (Virtual ({pac; offset} as v)) ->
+      | Symbolic (Virtual ({pac; offset; _} as v)) ->
         let modifier = pp_cst true modifier in
         let pac = PAC.add key modifier offset pac in
         Some (Symbolic (Virtual {v with pac}))
