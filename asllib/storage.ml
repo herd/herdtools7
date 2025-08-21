4458dfb50fb1e0e54562f2ee634a1be20cc71379
Hadrien Renaud <hadrien.renaud2@arm.com>
2025-08-21 14:05:52 +0100

    [asl] Simplify storage implementation.
    Storage indirection is only necessary in case of mutable values or
    references. As ASL does not feature any of those, we can simplify the
    environment to a simple map.

--- asllib/storage.ml
+++ asllib/storage.ml
@@ -24,95 +24,38 @@ open ASTUtils
 
 let _runtime_assertions = true
 
-type pointer = int
+type 'v t = 'v IMap.t
 
-module PMap = struct
-  module PM = Map.Make (Int)
-
-  let[@warning "-32"] of_list li =
-    List.fold_left (fun acc (key, value) -> PM.add key value acc) PM.empty li
-
-  include PM
-end
-
-module PSet = Set.Make (Int)
-
-type 'v t = { env : pointer IMap.t; mem : 'v PMap.t }
-
-let alloc =
-  let next = ref 0 in
-  fun () ->
-    let r = !next in
-    next := r + 1;
-    r
-
-let empty = { env = IMap.empty; mem = PMap.empty }
-let mem x t = IMap.mem x t.env
+let empty = IMap.empty
+let mem x t = IMap.mem x t
 
 let assign x v t =
-  let p = IMap.find x t.env in
-  { t with mem = PMap.add p v t.mem }
+  IMap.update x (function None -> raise Not_found | Some _ -> Some v) t
 
 let declare x v t =
-  let () =
-    if _runtime_assertions && mem x t then
-      let () =
-        Printf.eprintf "Storage element %s already declared in env.\n%!" x
-      in
-      assert false
-  in
-  let p = alloc () in
-  { env = IMap.add x p t.env; mem = PMap.add p v t.mem }
+  if _runtime_assertions then
+    IMap.update x
+      (function
+        | None -> Some v
+        | Some _ ->
+            let () =
+              Printf.eprintf "Storage element %s already declared in env.\n%!" x
+            in
+            assert false)
+      t
+  else IMap.add x v t
 
-let of_v_map map =
-  let mem_list = ref [] in
-  let env =
-    IMap.map
-      (fun v ->
-        let p = alloc () in
-        mem_list := (p, v) :: !mem_list;
-        p)
-      map
-  in
-  let mem = PMap.of_list !mem_list in
-  { env; mem }
-
-let add x v t = try assign x v t with Not_found -> declare x v t
-
-let find x t =
-  let p = IMap.find x t.env in
-  PMap.find p t.mem
-
-let find_opt x t = try Some (find x t) with Not_found -> None
-
-let remove x t =
-  try
-    let p = IMap.find x t.env in
-    { mem = PMap.remove p t.mem; env = IMap.remove x t.env }
-  with Not_found -> t
+let of_v_map map = map
+let add x v t = IMap.add x v t
+let find x t = IMap.find x t
+let find_opt x t = IMap.find_opt x t
+let remove x t = IMap.remove x t
 
 let patch_mem ~t_env ~t_mem to_avoid =
-  let env = t_env.env
-  and mem =
-    try
-      List.fold_left
-        (fun mem x ->
-          let p = IMap.find x t_mem.env in
-          PMap.remove p mem)
-        t_mem.mem to_avoid
-    with Not_found ->
-      let () =
-        Printf.eprintf "Bug in unsetting one of ";
-        List.iter (fun s -> Printf.eprintf "%s, " s) to_avoid;
-        Printf.eprintf "\n%!"
-      in
-      assert false
-  in
-  { env; mem }
+  List.fold_left (fun t x -> IMap.remove x t) t_env to_avoid
+  |> IMap.mapi (fun x _ -> IMap.find x t_mem)
 
-let to_seq t =
-  IMap.to_seq t.env
-  |> Seq.map (fun (name, pointer) -> (name, PMap.find pointer t.mem))
+let to_seq t = IMap.to_seq t
 
 let pp_print pp_elt =
   let open Format in
@@ -121,4 +64,4 @@ let pp_print pp_elt =
   fun f t ->
     fprintf f "@[<hv 2>{@ %a}@]" (PP.pp_print_seq ~pp_sep pp_one) (to_seq t)
 
-let map f t = { t with mem = PMap.map f t.mem }
+let map f t = IMap.map f t
