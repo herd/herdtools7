herd/herdtools7

commit c89a5b581f5dc21938e5ced62c8d8bbab9429b02

[all] AST restructuring for some AArch64  instructions.
Restructured instructions: ADD/SUB, logical operations, LDR and STR.

As regards operations the I_OP3 constructor implements
immediate and shift register versions of the operations:
they are now closer to ASL implementation and complete.
Additionally, the extended register variations of
ADD, SUB and of their "S" variants are implemented by
the new I_ADDSUBEXT constructor.

The LDR and STR instructions are completed including the
post-indexed and pre-indexed variations. As a consequence
the I_LDR_P and I_STR_P constructors are suppressed, since
their functionality now belongs to the general I_LDR and I_STR.

AArch64+ASL implementation also provided.

lib/AArch64Parser.mly
@@ -18,9 +18,9 @@
 open AArch64Base
 
 (* No constant third argument for those *)
-let check_op3 op kr =
-  match op,kr with
-  |(BIC|BICS),K _ -> raise Parsing.Parse_error
+let check_op3 op e =
+  match op,e with
+  |(BIC|BICS),OpExt.Imm _ -> raise Parsing.Parse_error
   | _ -> ()
 
 type 'k k0_bang = Nada | Bang | Konst of 'k
@@ -56,12 +56,15 @@ let mk_instrp instr v r1 r2 ra ko kb =
 %token <int> PROC
 
 %token SEMI COMMA PIPE COLON DOT BANG LCRL RCRL LBRK RBRK LPAR RPAR SCOPES LEVELS REGIONS
-%token TOK_SXTW
 
-%token SXTW SBFM UBFM
+/*  Extension */
+%token TOK_SXTB TOK_SXTH TOK_SXTW TOK_SXTX
+%token TOK_UXTB TOK_UXTH TOK_UXTW TOK_UXTX
+
+%token SBFM UBFM
 
 /* Inline Barrel Shift Operands */
-%token TOK_LSL TOK_LSR TOK_ASR TOK_MSL TOK_UXTW
+%token TOK_LSL TOK_LSR TOK_ASR TOK_MSL TOK_ROR
 
 /* Instructions */
 %token NOP HINT HLT
@@ -80,7 +83,7 @@ let mk_instrp instr v r1 r2 ra ko kb =
 %token <AArch64Base.op> OP
 %token <AArch64Base.sc> SC
 %token <AArch64Base.gc> GC
-%token TOK_ADD TOK_SUB TOK_SUBS
+%token TOK_ADD TOK_ADDS TOK_SUB TOK_SUBS
 %token CSEL CSINC CSINV CSNEG CSET CSETM CINC
 %token TOK_DMB TOK_DSB TOK_ISB
 %token TOK_SY TOK_ST TOK_LD
@@ -202,7 +205,7 @@ wreg:
 | SYMB_WREG { Symbolic_reg $1 }
 | ARCH_WREG { $1 }
 
-xwr:
+%inline wxreg:
 | xreg { V64,$1 }
 | wreg { V32,$1 }
 
@@ -283,23 +286,10 @@ kr_shift_address:
 | wreg COMMA shift { (RV (V32, $1)), $3 }
 | xreg COMMA shift { (RV (V64, $1)), $3 }
 
-kr_shift:
-| kr_shift_address { $1 }
-| wreg { (RV (V32,$1)),  S_NOEXT }
-
 (* For address argument only *)
 kr0:
 | { K (MetaConst.zero), S_NOEXT }
 | COMMA kr_shift_address { $2 }
-
-/*
-kr0_no_shift:
-| { K (MetaConst.zero) }
-| COMMA k { K $2 }
-| COMMA xreg { RV (V64,$2) }
-| COMMA wreg { RV (V32,$2) }
-| COMMA wreg COMMA TOK_SXTW { RV (V32,$2) }
-*/
 
 kx0_no_shift:
 | { K (MetaConst.zero) }
@@ -310,6 +300,51 @@ k0_no_shift:
 | { K (MetaConst.zero) }
 | COMMA k { K $2 }
 
+%inline op_ext_shift:
+| TOK_LSL k { OpExt.LSL $2 }
+| TOK_LSR k { OpExt.LSR $2}
+| TOK_ASR k { OpExt.ASR $2}
+| TOK_ROR k { OpExt.ROR $2}
+
+%inline op_ext_shift0:
+| { OpExt.LSL MetaConst.zero }
+| COMMA op_ext_shift { $2 }
+
+op_ext_imm:
+| k
+    { OpExt.Imm ($1,MetaConst.zero) }
+| k COMMA TOK_LSL k
+    { OpExt.Imm ($1,$4) }
+
+op_ext_w:
+| op_ext_imm { $1 }
+| wreg op_ext_shift0
+    { OpExt.Reg ($1,$2) }
+
+%inline op_ext_x:
+| op_ext_imm { $1 }
+| xreg op_ext_shift0
+    { OpExt.Reg ($1,$2) }
+
+op_ext_c:
+| op_ext_imm { $1 }
+| creg op_ext_shift0
+    { OpExt.Reg ($1,$2) }
+
+%inline add_sub_sext:
+| TOK_SXTB { Ext.SXTB }
+| TOK_SXTH { Ext.SXTH }
+| TOK_SXTW { Ext.SXTW }
+| TOK_SXTX { Ext.SXTX }
+| TOK_UXTB { Ext.UXTB }
+| TOK_UXTH { Ext.UXTH }
+| TOK_UXTW { Ext.UXTW }
+| TOK_UXTX { Ext.UXTX }
+
+
+%inline add_sub_ext:
+| add_sub_sext { $1,None }
+| add_sub_sext k { $1,Some $2 }
 /*
 kr0_no_shift_opt:
 | { None }
@@ -326,17 +361,45 @@ kwr:
 | k { K $1 }
 | wreg { RV (V32,$1) }
 
-kxr:
-| k { K $1 }
-| xreg { RV (V64,$1) }
+mem_sext:
+| TOK_UXTW { MemExt.UXTW }
+| TOK_LSL { MemExt.LSL }
+| TOK_SXTW { MemExt.SXTW }
+| TOK_SXTX { MemExt.SXTX }
+
+mem_ea:
+| LBRK cxreg RBRK
+    { $2,MemExt.Imm (MetaConst.zero,Idx)  }
+| LBRK cxreg COMMA k RBRK
+    { $2,MemExt.Imm ($4,Idx) }
+| LBRK cxreg RBRK COMMA k
+    { $2,MemExt.Imm ($5,PostIdx) }
+| LBRK cxreg COMMA k RBRK BANG
+    { $2,MemExt.Imm ($4,PreIdx) }
+| LBRK cxreg COMMA wxreg RBRK
+    {
+      let open MemExt in
+      let v,r = $4 in
+      $2,Reg (v,r,LSL,MetaConst.zero)
+    }
+| LBRK cxreg COMMA wxreg COMMA mem_sext RBRK
+    {
+      let open MemExt in
+      let v,r = $4 in
+      $2,Reg (v,r,$6,MetaConst.zero)
+    }
+| LBRK cxreg COMMA wxreg COMMA mem_sext k RBRK
+    {
+      let open MemExt in
+      let v,r = $4 in
+      $2,Reg (v,r,$6,$7)
+    }
 
 shift:
 | TOK_LSL NUM  { S_LSL(MetaConst.Int $2)  }
 | TOK_LSR NUM  { S_LSR(MetaConst.Int $2)  }
 | TOK_ASR NUM  { S_ASR(MetaConst.Int $2)  }
 | TOK_MSL NUM  { S_MSL(MetaConst.Int $2)  }
-| TOK_SXTW { S_SXTW }
-| TOK_UXTW { S_UXTW }
 
 zeroopt:
 | { () }
@@ -428,6 +491,23 @@ cond:
 | TOK_LS { LS }
 | TOK_AL { AL }
 
+%inline tok_instr_shift:
+| TOK_ASR { ASR }
+| TOK_LSL { LSL }
+| TOK_LSR { LSR}
+
+%inline tok_add_sub:
+| TOK_ADD  { ADD }
+| TOK_ADDS { ADDS }
+| TOK_SUB  { SUB }
+| TOK_SUBS { SUBS }
+
+%inline tok_add_sub_ext:
+| TOK_ADD  { Ext.ADD }
+| TOK_ADDS { Ext.ADDS }
+| TOK_SUB  { Ext.SUB }
+| TOK_SUBS { Ext.SUBS }
+
 label_addr:
 | NAME      { BranchTarget.Lbl $1 }
 | DOT NUM   { BranchTarget.Offset $2 }
@@ -469,15 +549,8 @@ instr:
 | TBZ reg COMMA NUM COMMA label_addr
   { let v,r = $2 in I_TBZ (v,r,MetaConst.Int $4,$6) }
 /* Memory */
-/* must differentiate between regular and post-indexed load */
-| LDR reg COMMA LBRK cxreg kr0 RBRK k0_opt
-  { let v,r    = $2 in
-    let kr, os = $6 in
-    match $8 with
-    | Some post when kr = K MetaConst.zero ->
-      I_LDR_P (v,r,$5,post)
-    | _ ->
-      I_LDR (v,r,$5,kr,os) }
+| LDR wxreg COMMA mem_ea
+  { let (v,r)   = $2 and (ra,ext) = $4 in I_LDR (v,r,ra,ext) }
 | LDUR reg COMMA LBRK cxreg k0_opt RBRK
   { let v,r = $2 in I_LDUR (v,r,$5,$6)}
 
@@ -510,10 +583,10 @@ instr:
   { I_STXP (V32,LY,$2,$4,$6,$9) }
 | STLXP wreg COMMA xreg COMMA xreg COMMA LBRK cxreg RBRK
   { I_STXP (V64,LY,$2,$4,$6,$9) }
-| LDRB wreg COMMA LBRK cxreg kr0 RBRK
-  { let (kr, s) = $6 in I_LDRBH (B,$2,$5,kr,s) }
-| LDRH wreg COMMA LBRK cxreg kr0 RBRK
-  { let (kr, s) = $6 in I_LDRBH (H,$2,$5,kr,s) }
+| LDRB wreg COMMA mem_ea
+  { let (ra,idx) = $4 in I_LDRBH (B,$2,ra,idx) }
+| LDRH wreg COMMA mem_ea
+  { let ra,idx = $4 in I_LDRBH (H,$2,ra,idx) }
 | LDRSB reg COMMA LBRK cxreg RBRK
   { let (v, s) = $2 in I_LDRS (v,B,s,$5) }
 | LDRSH reg COMMA LBRK cxreg RBRK
@@ -542,16 +615,12 @@ instr:
   { I_LDARBH (B,AQ,$2,$5) }
 | LDAPRH wreg COMMA LBRK cxreg RBRK
   { I_LDARBH (H,AQ,$2,$5) }
-| STR reg COMMA LBRK cxreg kr0 RBRK k0_opt
-  { let (v,r)   = $2 in
-    match $6, $8 with
-    (* post-indexed writes do not have shifters *)
-    | (_,S_NOEXT), Some s -> I_STR_P (v,r,$5,s)
-    | (kr,os),_ -> I_STR (v,r,$5,kr,os) }
-| STRB wreg COMMA LBRK cxreg kr0 RBRK
-  { let (kr,os) = $6 in I_STRBH (B,$2,$5,kr,os) }
-| STRH wreg COMMA LBRK cxreg kr0 RBRK
-  { let (kr, os) = $6 in I_STRBH (H,$2,$5,kr,os) }
+| STR wxreg COMMA mem_ea
+  { let (v,r)   = $2 and (ra,ext) = $4 in I_STR (v,r,ra,ext) }
+| STRB wreg COMMA mem_ea
+  { let (ra,idx) = $4 in I_STRBH (B,$2,ra,idx) }
+| STRH wreg COMMA mem_ea
+  { let (ra,idx) = $4 in I_STRBH (H,$2,ra,idx) }
 | STLR reg COMMA LBRK cxreg RBRK
   { let v,r = $2 in I_STLR (v,r,$5) }
 | STLRB wreg COMMA LBRK cxreg RBRK
@@ -647,7 +716,7 @@ instr:
     I_STUR_SIMD (v, r, $5, $6) }
 | MOV vreg INDEX COMMA vreg INDEX
   { I_MOV_VE ($2, $3, $5, $6) }
-| MOV vreg INDEX COMMA xwr
+| MOV vreg INDEX COMMA wxreg
   { let v,r = $5 in
     I_MOV_FG ($2, $3, v, r) }
 | MOV xreg COMMA vreg INDEX
@@ -1112,24 +1181,15 @@ instr:
 | TOK_SXTW xreg COMMA wreg
   { I_SXTW ($2,$4) }
 | MVN wreg COMMA wreg
-  { I_OP3 (V32,ORN,$2,ZR,RV (V32,$4), S_NOEXT) }
+  { I_OP3 (V32,ORN,$2,ZR,OpExt.Reg ($4,OpExt.LSL MetaConst.zero)) }
 | MVN xreg COMMA xreg
-  { I_OP3 (V64,ORN,$2,ZR,RV (V64,$4), S_NOEXT) }
+  { I_OP3 (V64,ORN,$2,ZR,OpExt.Reg ($4,OpExt.LSL MetaConst.zero)) }
 /* Special handling for ASR/LSL/LSR operation */
-| TOK_ASR xreg COMMA xreg COMMA kr
-  { I_OP3 (V64, ASR, $2, $4, $6, S_NOEXT) }
-| TOK_ASR wreg COMMA wreg COMMA kr
-  { I_OP3 (V32, ASR, $2, $4, $6, S_NOEXT) }
-| TOK_LSL xreg COMMA xreg COMMA kr
-  { I_OP3 (V64, LSL, $2, $4, $6, S_NOEXT) }
-| TOK_LSL wreg COMMA wreg COMMA kr
-  { I_OP3 (V32, LSL, $2, $4, $6, S_NOEXT) }
-| TOK_LSR xreg COMMA xreg COMMA kr
-  { I_OP3 (V64, LSR, $2, $4, $6, S_NOEXT) }
-| TOK_LSR wreg COMMA wreg COMMA kr
-  { I_OP3 (V32, LSR, $2, $4, $6, S_NOEXT) }
-| SXTW xreg COMMA wreg
-  { I_SXTW ($2,$4) }
+| tok_instr_shift xreg COMMA xreg COMMA op_ext_x
+  { I_OP3 (V64, $1, $2, $4, $6) }
+| tok_instr_shift wreg COMMA wreg COMMA op_ext_w
+  { I_OP3 (V32, $1, $2, $4, $6) }
+
 | SBFM xreg COMMA xreg COMMA k COMMA k
   { I_SBFM (V64,$2,$4,$6,$8) }
 | SBFM wreg COMMA wreg COMMA k COMMA k
@@ -1138,54 +1198,41 @@ instr:
   { I_UBFM (V64,$2,$4,$6,$8) }
 | UBFM wreg COMMA wreg COMMA k COMMA k
   { I_UBFM (V32,$2,$4,$6,$8) }
-| OP xreg COMMA xreg COMMA kxr
-  { check_op3 $1 $6 ; I_OP3 (V64,$1,$2,$4,$6, S_NOEXT) }
-| OP xreg COMMA xreg COMMA kr COMMA shift
-  { check_op3 $1 $6 ; I_OP3 (V64,$1,$2,$4,$6, $8) }
-| OP wreg COMMA wreg COMMA kwr
-  { check_op3 $1 $6 ; I_OP3 (V32,$1,$2,$4,$6, S_NOEXT) }
-| TOK_ADD xreg COMMA xreg COMMA kxr
-  { I_OP3 (V64,ADD,$2,$4,$6, S_NOEXT) }
-| TOK_ADD xreg COMMA xreg COMMA kr COMMA shift
-  { I_OP3 (V64,ADD,$2,$4,$6, $8) }
-| TOK_ADD wreg COMMA wreg COMMA kwr
-  { I_OP3 (V32,ADD,$2,$4,$6, S_NOEXT) }
-| TOK_ADD wreg COMMA wreg COMMA kwr COMMA shift
-  { I_OP3 (V32,ADD,$2,$4,$6, $8) }
-| TOK_ADD creg COMMA creg COMMA kxr
-  { I_OP3 (V128,ADD,$2,$4,$6, S_NOEXT) }
-| TOK_SUB xreg COMMA xreg COMMA kxr
-  { I_OP3 (V64,SUB,$2,$4,$6, S_NOEXT) }
-| TOK_SUB xreg COMMA xreg COMMA kr COMMA shift
-  { I_OP3 (V64,SUB,$2,$4,$6, $8) }
-| TOK_SUB wreg COMMA wreg COMMA kwr
-    { I_OP3 (V32,SUB,$2,$4,$6, S_NOEXT) }
-| TOK_SUB wreg COMMA wreg COMMA kwr COMMA shift
-    { I_OP3 (V32,SUB,$2,$4,$6, $8) }
-| TOK_SUB creg COMMA creg COMMA k
-  { I_OP3 (V128,SUB,$2,$4,K $6, S_NOEXT) }
-| TOK_SUBS xreg COMMA xreg COMMA kxr
-  { I_OP3 (V64,SUBS,$2,$4,$6, S_NOEXT) }
-| TOK_SUBS xreg COMMA xreg COMMA kr COMMA shift
-  { I_OP3 (V64,SUBS,$2,$4,$6, $8) }
-| TOK_SUBS wreg COMMA wreg COMMA kwr
-  { I_OP3 (V32,SUBS,$2,$4,$6, S_NOEXT) }
-| TOK_SUBS wreg COMMA wreg COMMA kwr COMMA shift
-  { I_OP3 (V32,SUBS,$2,$4,$6, $8) }
-| TOK_SUBS xreg COMMA creg COMMA creg
-  { I_OP3 (V128,SUBS,$2,$4,RV (V128,$6), S_NOEXT) }
-| OP wreg COMMA wreg COMMA kwr COMMA shift
-  { check_op3 $1 $6 ; I_OP3 (V32,$1,$2,$4,$6,$8) }
-| CMP wreg COMMA kr_shift
-  { let (reg,shift) = $4 in
-    I_OP3 (V32,SUBS,ZR,$2,reg,shift) }
-| CMP xreg COMMA kr_shift
-  { let (reg,shift) = $4 in
-    I_OP3 (V64,SUBS,ZR,$2,reg,shift) }
-| TST wreg COMMA k
-  { I_OP3 (V32,ANDS,ZR,$2,K $4, S_NOEXT) }
-| TST xreg COMMA k
-  { I_OP3 (V64,ANDS,ZR,$2,K $4, S_NOEXT) }
+(* Generic OP3 *)
+| OP xreg COMMA xreg COMMA op_ext_x
+  { check_op3 $1 $6 ; I_OP3 (V64,$1,$2,$4,$6) }
+| OP wreg COMMA wreg COMMA op_ext_w
+  { check_op3 $1 $6 ; I_OP3 (V32,$1,$2,$4,$6) }
+
+(* Addition, also consider extended register *)
+| tok_add_sub xreg COMMA xreg COMMA op_ext_x
+  { I_OP3 (V64, $1, $2, $4, $6) }
+| tok_add_sub wreg COMMA wreg COMMA op_ext_w
+  { I_OP3 (V32, $1, $2, $4, $6) }
+| tok_add_sub creg COMMA creg COMMA op_ext_c
+  { I_OP3 (V128, $1, $2, $4, $6) }
+| tok_add_sub_ext creg COMMA creg COMMA xreg COMMA add_sub_ext
+  { I_ADDSUBEXT (V128, $1, $2, $4, (V64, $6), $8) }
+| tok_add_sub_ext xreg COMMA xreg COMMA wxreg COMMA add_sub_ext
+  { I_ADDSUBEXT (V64,$1,$2,$4,$6,$8) }
+| tok_add_sub_ext wreg COMMA wreg COMMA wreg COMMA add_sub_ext
+  { I_ADDSUBEXT (V32, $1, $2, $4, (V32, $6), $8) }
+
+
+| CMP wreg COMMA op_ext_w
+  { I_OP3 (V32,SUBS,ZR,$2,$4) }
+| CMP xreg COMMA op_ext_x
+  { I_OP3 (V64,SUBS,ZR,$2,$4) }
+| CMP wreg COMMA wreg COMMA add_sub_ext
+  { I_ADDSUBEXT (V32,Ext.SUBS,ZR,$2,(V32,$4),$6) }
+| CMP xreg COMMA wxreg COMMA add_sub_ext
+  { I_ADDSUBEXT (V64,Ext.SUBS,ZR,$2,$4,$6) }
+
+| TST wreg COMMA op_ext_w
+  { I_OP3 (V32,ANDS,ZR,$2,$4) }
+| TST xreg COMMA op_ext_x
+  { I_OP3 (V64,ANDS,ZR,$2,$4) }
+
 | RBIT wreg COMMA wreg
   { I_RBIT (V32,$2,$4) }
 | RBIT xreg COMMA xreg

gen/AArch64Compile_gen.ml
@@ -138,54 +138,72 @@ module Make(Cfg:Config) : XXXCompile_gen.S =
 
     module U = GenUtils.Make(Cfg)(A)(Extra)
 
-    let idx_ext = function
-      | A64.V32 -> S_SXTW
-      | A64.V64  -> S_NOEXT
-      | A64.V128 -> assert false (* ? *)
-
+    let op3i v op r1 r2 i  =
+      let open OpExt in I_OP3 (v,op,r1,r2,Imm (i,0))
+    and op3r v op r1 r2 r3 =
+      let open OpExt in I_OP3 (v,op,r1,r2,Reg (r3,no_shift))
 
     let cbz r1 lbl = I_CBZ (vloc,r1,BranchTarget.Lbl lbl)
     let do_cbnz v r1 lbl = I_CBNZ (v,r1,BranchTarget.Lbl lbl)
     let cbnz = do_cbnz vloc
-    let do_cmpi v r i =  I_OP3 (v,SUBS,ZR,r,K i, S_NOEXT)
+
+    let do_cmpi v r i = op3i v SUBS ZR r i
     let cmpi r i = do_cmpi vloc r i
+
     let do_csel v r1 r2 r3 = I_CSEL (v,r1,r2,r3,EQ,Cpy)
     let do_cinc v r1 r2 r3 = I_CSEL (v,r1,r2,r3,EQ,Inc)
-    let cmp r1 r2 = I_OP3 (vloc,SUBS,ZR,r1,RV (vloc,r2), S_NOEXT)
+
+    let cmp r1 r2 = op3r vloc SUBS ZR r1 r2
+
     let b lbl = I_B (BranchTarget.Lbl lbl)
     let bne lbl = I_BC (NE,BranchTarget.Lbl lbl)
-    let eor sz r1 r2 r3 = I_OP3 (sz,EOR,r1,r2,RV (sz,r3), S_NOEXT)
+
+    let eor sz r1 r2 r3 = op3r sz EOR r1 r2 r3
+
     let eor_simd r1 r2 = I_EOR_SIMD (r1,r2,r2)
-    let andi sz r1 r2 k = I_OP3 (sz,AND,r1,r2,K k, S_NOEXT)
-    let incr r = I_OP3 (V32,ADD,r,r,K 1, S_NOEXT)
-    let lsri64 r1 r2 k = I_OP3 (V64,LSR,r1,r2,K k, S_NOEXT)
-    let do_addi v r1 r2 k = I_OP3 (v,ADD,r1,r2,K k, S_NOEXT)
+
+    let andi sz r1 r2 k = op3i sz AND r1 r2 k
+
+    let incr r = op3i V32 ADD r r 1
+
+    let lsri64 r1 r2 k = op3i V64 LSR r1 r2 k
+
+    let do_addi v r1 r2 k = op3i v ADD r1 r2 k
     let addi = do_addi vloc
     let addi_64 = do_addi V64
-    let add v r1 r2 r3 = I_OP3 (v,ADD,r1,r2,RV (v,r3), S_NOEXT)
+
+    let add v r1 r2 r3 = op3r v ADD r1 r2  r3
     let add_simd r1 r2 = I_ADD_SIMD (r1,r1,r2)
+
     let do_add64 v r1 r2 r3 =
-      let ext = idx_ext v in
-      I_OP3 (V64,ADD,r1,r2,RV (v,r3), ext)
-    let do_addcapa r1 r2 r3 = I_OP3 (V128,ADD,r1,r2,RV (V64,r3), S_NOEXT)
+      match v with
+      | V64 -> add v r1 r2 r3
+      | _ ->
+         let ext = Ext.v2sext v in
+         I_ADDSUBEXT (V64,Ext.ADD,r1,r2,(v,r3),(ext, None))
+
+    let do_addcapa r1 r2 r3 =
+      I_ADDSUBEXT (V128,Ext.ADD,r1,r2,(V64,r3),Ext.no_ext)
     let gctype r1 r2 = I_GC (GCTYPE,r1,r2)
     let gcvalue r1 r2 = I_GC (GCVALUE,r1,r2)
     let scvalue r1 r2 r3 = I_SC (SCVALUE,r1,r2,r3)
     let seal r1 r2 r3 = I_SEAL (r1,r2,r3)
     let cseal r1 r2 r3 = I_CSEAL (r1,r2,r3)
-    let subi sz r1 r2 k = I_OP3 (sz,SUB,r1,r2,K k, S_NOEXT)
+
+    let subi sz r1 r2 k = op3i sz SUB r1 r2 k
     let dec r1 r2 = subi vloc r1 r2 1
 
     let ldr_mixed r1 r2 sz o =
+      let idx = MemExt.Imm (o,Idx) in
       let open MachSize in
       match sz with
-      | Byte -> I_LDRBH (B,r1,r2,K o, S_NOEXT)
-      | Short -> I_LDRBH (H,r1,r2,K o, S_NOEXT)
-      | Word -> I_LDR (V32,r1,r2,K o, S_NOEXT)
-      | Quad -> I_LDR (V64,r1,r2,K o, S_NOEXT)
-      | S128 -> I_LDR (V128,r1,r2,K o, S_NOEXT)
+      | Byte -> I_LDRBH (B,r1,r2,idx)
+      | Short -> I_LDRBH (H,r1,r2,idx)
+      | Word -> I_LDR (V32,r1,r2,idx)
+      | Quad -> I_LDR (V64,r1,r2,idx)
+      | S128 -> I_LDR (V128,r1,r2,idx)
 
-    let do_ldr v r1 r2 = I_LDR (v,r1,r2,K 0, S_NOEXT)
+    let do_ldr v r1 r2 = I_LDR (v,r1,r2,MemExt.Imm(0,Idx))
     let ldg r1 r2 = I_LDG (r1,r2,0)
     let ldct r1 r2 = I_LDCT(r1,r2)
     let do_ldar vr r1 r2 = I_LDAR (vr,AA,r1,r2)
@@ -195,7 +213,11 @@ module Make(Cfg:Config) : XXXCompile_gen.S =
     let sxtw r1 r2 = I_SXTW (r1,r2)
 
     let do_ldr_idx v1 v2 r1 r2 idx =
-      I_LDR (v1,r1,r2,RV (v2,idx),idx_ext v2)
+      let open MemExt in
+      let sext = match v2 with
+        | V32 -> SXTW
+        | V64|V128 -> LSL in
+      I_LDR (v1,r1,r2,Reg (v2,idx,sext,0))
 
     let ldn n rs rt =
       let open SIMD in
@@ -215,31 +237,33 @@ module Make(Cfg:Config) : XXXCompile_gen.S =
 
 
     let ldr_mixed_idx v r1 r2 idx sz  =
+      let idx = MemExt.v2idx_reg v idx in
       let open MachSize in
       match sz with
-      | Byte -> I_LDRBH (B,r1,r2,RV (v,idx), idx_ext v)
-      | Short -> I_LDRBH (H,r1,r2,RV (v,idx),idx_ext v)
-      | Word -> I_LDR (V32,r1,r2,RV (v,idx), idx_ext v)
-      | Quad -> I_LDR (V64,r1,r2,RV (v,idx), idx_ext v)
-      | S128 -> I_LDR (V128,r1,r2,RV (v,idx),idx_ext v)
+      | Byte -> I_LDRBH (B,r1,r2,idx)
+      | Short -> I_LDRBH (H,r1,r2,idx)
+      | Word -> I_LDR (V32,r1,r2,idx)
+      | Quad -> I_LDR (V64,r1,r2,idx)
+      | S128 -> I_LDR (V128,r1,r2,idx)
 
     let str_mixed sz o r1 r2 =
+      let idx = MemExt.Imm (o,Idx) in
       let open MachSize in
       match sz with
-      | Byte -> I_STRBH (B,r1,r2,K o, S_NOEXT)
-      | Short -> I_STRBH (H,r1,r2,K o, S_NOEXT)
-      | Word -> I_STR (V32,r1,r2,K o, S_NOEXT)
-      | Quad -> I_STR (V64,r1,r2,K o, S_NOEXT)
-      | S128 -> I_STR (V128,r1,r2,K o, S_NOEXT)
+      | Byte -> I_STRBH (B,r1,r2,idx)
+      | Short -> I_STRBH (H,r1,r2,idx)
+      | Word -> I_STR (V32,r1,r2,idx)
+      | Quad -> I_STR (V64,r1,r2,idx)
+      | S128 -> I_STR (V128,r1,r2,idx)
 
-    let do_str v r1 r2 = I_STR (v,r1,r2,K 0, S_NOEXT)
+    let do_str v r1 r2 = I_STR (v,r1,r2,MemExt.Imm (0,Idx))
     let str = do_str vloc
     let stg r1 r2 = I_STG (r1,r2,0)
     let stct r1 r2 = I_STCT(r1,r2)
     let do_stlr v r1 r2 = I_STLR (v,r1,r2)
     let stlr = do_stlr vloc
 
-    let do_str_idx v r1 r2 idx = I_STR (vloc,r1,r2,RV (v,idx), idx_ext v)
+    let do_str_idx v r1 r2 idx = I_STR (vloc,r1,r2,MemExt.v2idx_reg v idx)
     let str_idx = do_str_idx vloc
     let stxr r1 r2 r3 = I_STXR (vloc,YY,r1,r2,r3)
     let stlxr r1 r2 r3 = I_STXR (vloc,LY,r1,r2,r3)
@@ -285,13 +309,14 @@ module Make(Cfg:Config) : XXXCompile_gen.S =
     let sumi_addr st rA o = sumi_addr_gen tempo1 st rA o
 
     let str_mixed_idx sz v r1 r2 idx  =
+      let idx = MemExt.v2idx_reg v idx in
       let open MachSize in
       match sz with
-      | Byte -> I_STRBH (B,r1,r2,RV (v,idx),idx_ext v)
-      | Short -> I_STRBH (H,r1,r2,RV (v,idx),idx_ext v)
-      | Word -> I_STR (V32,r1,r2,RV (v,idx),idx_ext v)
-      | Quad -> I_STR (V64,r1,r2,RV (v,idx),idx_ext v)
-      | S128 -> I_STR (V128,r1,r2,RV (v,idx),idx_ext v)
+      | Byte -> I_STRBH (B,r1,r2,idx)
+      | Short -> I_STRBH (H,r1,r2,idx)
+      | Word -> I_STR (V32,r1,r2,idx)
+      | Quad -> I_STR (V64,r1,r2,idx)
+      | S128 -> I_STR (V128,r1,r2,idx)
 
     let swp_mixed sz a rS rT rN =
       let open MachSize in
@@ -505,7 +530,7 @@ module Make(Cfg:Config) : XXXCompile_gen.S =
           pseudo
             (ld@
              [cmp rA;
-              bne out; I_OP3 (vloc,SUBS,rC,rC,K 1, S_NOEXT) ;
+              bne out; I_OP3 (vloc,SUBS,rC,rC,OpExt.Imm (1,0));
               cbnz rC lab ;
             ])@
           [Label (out,Nop)],st

herd/AArch64ASLSem.ml
@@ -122,14 +122,17 @@ module Make (TopConf : AArch64Sig.Config) (V : Value.AArch64ASL) :
       if i ==  i0 then ii
       else { ii with A.inst = i; }
 
-    let decode_shift =
-      let open AArch64Base in
+    let opext_decode_shift =
+      let open AArch64Base.OpExt in
       function
-      | S_NOEXT|S_LSL _ -> "ShiftType_LSL"
-      | S_LSR _         -> "ShiftType_LSR"
-      | S_ASR _         -> "ShiftType_ASR"
-    (* TODO:     | S_ROR _         -> "ShiftType_ROR" *)
-      | _ -> assert false
+      | LSL _ -> "ShiftType_LSL"
+      | LSR _ -> "ShiftType_LSR"
+      | ASR _ -> "ShiftType_ASR"
+      | ROR _ -> "ShiftType_ROR"
+
+    let opext_shift_amount =
+      let open AArch64Base.OpExt in
+      function |LSL k|LSR k|ASR k| ROR k -> k
 
     let decode_acquire =
       let open AArch64 in
@@ -279,11 +282,43 @@ module Make (TopConf : AArch64Sig.Config) (V : Value.AArch64ASL) :
                 "datasize" ^= liti datasize;
                 "inzero" ^= litb true;
                ]@[added]))
+      | I_ADDSUBEXT (v,Ext.(ADD|ADDS|SUB|SUBS as op),rd,rn,(_vm,rm),(e,ko))
+        ->
+         let datasize = variant_raw v in
+         let fname =
+           let open Ext in
+           match op with
+           | ADD -> "ADD_32_addsub_ext.opn"
+           | ADDS -> "ADDS_32_addsub_ext.opn"
+           | SUB -> "SUB_32_addsub_ext.opn"
+           | SUBS -> "SUBS_32_addsub_ext.opn" in
+         let base = "integer/arithmetic/add-sub/extendedreg/" in
+         let extend_type =
+           let open Ext in
+           match e with
+           | UXTB -> "ExtendType_UXTB"
+           | UXTH -> "ExtendType_UXTH"
+           | UXTW -> "ExtendType_UXTW"
+           | UXTX -> "ExtendType_UXTX"
+           | SXTB -> "ExtendType_SXTB"
+           | SXTH -> "ExtendType_SXTH"
+           | SXTW -> "ExtendType_SXTW"
+           | SXTX -> "ExtendType_SXTX" in
+         let shift =  match ko with None -> 0 | Some k -> k in
+         Some
+           (base ^ fname,
+            stmt
+            ["d" ^= reg rd;
+             "n" ^= reg rn;
+             "m" ^= reg rm;
+             "datasize" ^= liti datasize;
+             "extend_type" ^= var extend_type;
+             "shift" ^= liti shift;])
       | I_OP3
          (v,
           (ADD|ADDS|SUB|SUBS|AND|ANDS|BIC|BICS|EOR|EON|ORN|ORR as op),
-          rd, rn, RV (v', rm),
-          (S_NOEXT|S_LSL _|S_ASR _|S_LSR _ (* S_ROR _ *) as s)) when v = v' ->
+          rd, rn, OpExt.Reg(rm,s))
+        ->
          let base =
            match op with
            | ADD|ADDS|SUB|SUBS ->
@@ -313,16 +348,13 @@ module Make (TopConf : AArch64Sig.Config) (V : Value.AArch64ASL) :
                "n" ^= reg rn;
                "m" ^= reg rm;
                "datasize" ^= variant v;
-               "shift_type" ^= var (decode_shift s);
-               "shift_amount" ^= liti (shift_amount s); ])
+               "shift_type" ^= var (opext_decode_shift s);
+               "shift_amount" ^= liti (opext_shift_amount s); ])
     | I_OP3
-          (v, ((ADD|ADDS|SUB|SUBS) as op), rd, rn, K k,
-           (S_NOEXT|S_LSL (0|12) as ext)) ->
+        (v, ((ADD|ADDS|SUB|SUBS) as op), rd, rn, OpExt.Imm (k,s))
+      ->
          let datasize = variant_raw v in
-         let k =
-           match ext  with
-           | S_LSL s -> k lsl s
-           | _ -> k in
+         let k = k lsl s in
          let fname =
            match op with
            | ADD -> "ADD_32_addsub_imm.opn"
@@ -340,7 +372,7 @@ module Make (TopConf : AArch64Sig.Config) (V : Value.AArch64ASL) :
                  "datasize" ^= liti datasize;
            ] )
 
-      | I_OP3 (v, (AND|ANDS|EOR|ORR as op), rd, rn, K k, S_NOEXT) -> (
+      | I_OP3 (v, (AND|ANDS|EOR|ORR as op), rd, rn, OpExt.Imm (k,0)) -> (
           let datasize = variant_raw v in
           let fname =
             match op with
@@ -357,29 +389,21 @@ module Make (TopConf : AArch64Sig.Config) (V : Value.AArch64ASL) :
                   "n" ^= reg rn;
                   "imm" ^= litbv datasize k;
                   "datasize" ^= liti datasize; ]))
-      | I_STR (v, rt, rn, RV (v', rm), barrel_shift)
-      | I_LDR (v, rt, rn, RV (v', rm), barrel_shift) as i ->
+      | I_STR (v, rt, rn, MemExt.Reg (_vm,rm,e,s))
+      | I_LDR (v, rt, rn, MemExt.Reg (_vm,rm,e,s)) as i
+        ->
          let fname =
            match i with
            | I_STR _ -> "STR_32_ldst_regoff.opn"
            | I_LDR _ -> "LDR_32_ldst_regoff.opn"
            | _ -> assert false
           and extend_type =
-            match barrel_shift with
-            | S_NOEXT -> "ExtendType_UXTX"
-            | S_SXTW -> "ExtendType_SXTW"
-            | S_UXTW -> "ExtendType_UXTW"
-            | S_LSL _ -> "ExtendType_UXTX"
-            | _ ->
-                Warn.fatal "Unsupported barrel shift for LDR: %s."
-                  (AArch64Base.pp_barrel_shift "" barrel_shift string_of_int)
-          in
-          let shift =
-            match barrel_shift with
-            | S_LSL k -> k
-            | S_NOEXT -> 0
-            | _ -> 0
-          in
+            let open MemExt in
+            match e with
+            | UXTW -> "ExtendType_UXTW"
+            | SXTW -> "ExtendType_SXTW"
+            | SXTX -> "ExtendType_SXTX"
+            | LSL  -> "ExtendType_UXTX"  in
           Some
             ( "memory/single/general/register/" ^ fname,
               stmt
@@ -388,33 +412,36 @@ module Make (TopConf : AArch64Sig.Config) (V : Value.AArch64ASL) :
                   "n" ^= reg rn;
                   "m" ^= reg rm;
                   "extend_type" ^= var extend_type;
-                  "shift" ^= liti shift;
+                  "shift" ^= liti s;
                   "datasize" ^= variant v;
-                  "regsize" ^= variant v';
+                  "regsize" ^= variant v;
                 ] )
-      | I_STR (v, rt, rn, K k, S_NOEXT)
-      | I_LDR (v, rt, rn, K k, S_NOEXT) ->
+      | I_STR (v, rt, rn, MemExt.Imm (k,idx))
+      | I_LDR (v, rt, rn, MemExt.Imm (k,idx))
+        ->
           let memop,fname =
             match ii.A.inst with
             | I_STR _ -> "MemOp_STORE","STR_32_ldst_immpost.opn"
             | I_LDR _ -> "MemOp_LOAD","LDR_32_ldst_immpost.opn"
-            | _ -> assert false
-          in
-          let offset = k
-          in
+            | _ -> assert false in
+          let wback,postindex =
+            match idx with
+            | Idx -> false,false
+            | PreIdx -> true,false
+            | PostIdx -> true,true in
           Some
             ( "memory/single/general/immediate/signed/post-idx/"^fname,
               stmt
                 [
                   "t" ^= reg rt;
                   "n" ^= reg rn;
-                  "offset" ^= litbv 64 offset;
-                  "wback" ^= litb false;
-                  "postindex" ^= litb false;
+                  "offset" ^= litbv 64 k;
+                  "wback" ^= litb wback;
+                  "postindex" ^= litb postindex;
                   "signed" ^= litb false;
                   "nontemporal" ^= litb false;
                   "memop" ^= var memop;
-                  "tagchecked" ^= litb true;
+                  "tagchecked" ^= litb (wback || rn <> SP);
                   "datasize" ^= variant v;
                   "regsize" ^= variant v;
                   "rt_unknown" ^= litb false;

commit 465d100aa01f8b21ae32d826995f496edc0bffa1

[lib] Fix parser for Morello
It looks like commit c89a5b581f5d ("[all] AST restructuring for some
AArch64 instructions.") accidentally made Morello's C registers not
being recognised by LDR/STR instructions. For instance

```
AArch64 A
"DpAddrdWPCs RfeCsCs PodRWCsP Rfe"
Generator=diyone7 (version 7.58+1)
Prefetch=0:x=F,0:y=W,1:y=F,1:x=W
Com=Rf Rf
Orig=DpAddrdWPCs RfeCsCs PodRWCsP Rfe
{
__uint128 y; __uint128 x; __uint128 1:X1;

0:X0=0xffffc0000:x:1; 0:X3=0xffffc0000:y:1;
1:X3=0xffffc0000:y:1; 1:X0=0xffffc0000:x:1;
}
 P0               | P1           ;
 LDR X1,[C0]      | LDR C1,[C3]  ;
 SUB X4,X1,#1     | GCTYPE X1,C1 ;
 AND X4,X4,#4095  | MOV X2,#1    ;
 SCVALUE C4,C3,X4 | STR X2,[C0]  ;
 CSEAL C4,C3,C4   |              ;
 MOV X5,#0        |              ;
 MOV X6,#1        |              ;
 SCVALUE C5,C3,X5 |              ;
 SEAL C5,C5,C6    |              ;
 STR C5,[C4]      |              ;
exists (0:X1=1 /\ 1:X1=1) /\ ~Fault(P0,y)
```

result in

```
$ herd7 -variant morello A.litmus
Warning: File "A.litmus", line 14, characters 24-26: unexpected 'C1' (in prog) (User error)
```

Let's bring C registers back to parser rules for LDR/STR

Signed-off-by: Vladimir Murzin <vladimir.murzin@arm.com>

lib/AArch64Parser.mly
@@ -691,6 +691,8 @@ instr:
 /* Memory */
 | LDR wxreg COMMA mem_ea
   { let (v,r)   = $2 and (ra,ext) = $4 in I_LDR (v,r,ra,ext) }
+| LDR creg COMMA mem_ea
+  { let (ra,ext) = $4 in I_LDR (V128,$2,ra,ext) }
 | LDRSW xreg COMMA mem_ea
   { let r = $2 and (ra,ext) = $4 in I_LDRSW (r,ra,ext) }
 | LDRSB reg COMMA mem_ea
@@ -759,6 +761,8 @@ instr:
   { I_LDARBH (H,AQ,$2,$5) }
 | STR wxreg COMMA mem_ea
   { let (v,r)   = $2 and (ra,ext) = $4 in I_STR (v,r,ra,ext) }
+| STR creg COMMA mem_ea
+  { let (ra,ext) = $4 in I_STR (V128,$2,ra,ext) }
 | STRB wreg COMMA mem_ea
   { let (ra,idx) = $4 in I_STRBH (B,$2,ra,idx) }
 | STRH wreg COMMA mem_ea