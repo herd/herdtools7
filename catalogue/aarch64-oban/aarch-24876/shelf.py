3bc9902e9b6462dd52d9e12233a7ed49da43f262
Nikos Nikoleris <nikos.nikoleris@arm.com>
2025-11-26 17:38:40 +0000

    [AARCH-24876,herd] Strengthening of the TLBI-ob relation
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

---
+++ catalogue/aarch64-oban/aarch-24876/shelf.py
@@ -0,0 +1,14 @@
+record = "AARCH-24876"
+
+cats = []
+
+cfgs = [
+    "herd.cfg",
+]
+
+illustrative_tests = [
+    "tests/coRR+BBM.litmus",
+    "tests/coRR+alias-2BBM-alias.litmus",
+    "tests/coRR+posw0h2+BBM.litmus",
+    "tests/coRR+posw0w4+BBM.litmus",
+]
