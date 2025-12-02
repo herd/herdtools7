d4509dbaed116bf4ff3fc82b9c5d4ce10a9924f5
Hrutvik Kanabar <hrutvik.kanabar2@arm.com>
2025-12-02 10:21:14 +0000

    [asl][reference] Delete changelog

--- asllib/doc/extended_macros.py
+++ asllib/doc/extended_macros.py
@@ -28,8 +28,6 @@ def get_latex_sources(exclude) -> list[str]:
         excluded_files = [
             "ASLReference.tex",
             "ASLmacros.tex",
-            "ASLRefALP2.1ChangeLog.tex",
-            "ASLRefALP2ChangeLog.tex",
             "generated_macros.tex",
             "rendering_macros.tex",
             "variable_name_macros.tex",
