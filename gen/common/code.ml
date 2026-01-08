006d243af23bd061bd18ffde9543be96ab4797da
Shale Xiong <shale.xiong@arm.com,xiongshale@gmail.com>
2026-01-08 12:56:48 +0000

    [gen] Add `Obs`,`Coms.Int` and `Coms` wildcard for ext- int- and both-type- communication.

--- gen/common/code.ml
+++ gen/common/code.ml
@@ -85,6 +85,11 @@ let pp_ie = function
   | Ext -> "e"
   | UnspecCom -> "*"
 
+let pp_ie_full = function
+  | Int -> "Int"
+  | Ext -> "Ext"
+  | UnspecCom -> "*"
+
 let pp_extr = function
   | Dir d -> pp_dir d
   | Irr -> "*"
