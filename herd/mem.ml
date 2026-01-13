c14876c08d41d6b2189f40f7eeb227f891db088d
Hadrien Renaud <hadrien.renaud2@arm.com>
2026-01-13 10:25:30 +0000

    [herd] Rework solve regs to push final condition into equations

--- herd/mem.ml
+++ herd/mem.ml
@@ -182,6 +182,11 @@ module Make(C:Config) (S:Sem.Semantics) : S with module S = S	=
       | None -> Opts.unroll_default A.arch
       | Some u -> u
 
+    let speedcheck =
+      match C.speedcheck with
+      | Speed.False -> false
+      | Speed.True | Speed.Fast -> true
+
   let _profile = C.debug.Debug_herd.profile_asl
   let start_profile = if _profile then Sys.time else fun () -> 0.
   let end_profile = if _profile then end_profile else fun _ _ -> ()
@@ -868,53 +873,77 @@ let match_reg_events es =
 
     let debug_solver = C.debug.Debug_herd.solver > 0
 
-    let do_solve_regs test es csn =
+    let wanted_final_value test =
+      let map =
+        let rec loop acc =
+          let open ConstrGen in
+          function
+          | Atom (LV (Loc (A.Location_reg _ as loc), v)) -> A.LocMap.add loc v acc
+          | And li -> List.fold_left loop acc li
+          | Or [ x ] -> loop acc x
+          | _ -> acc
+        in
+        match test.Test_herd.cond with
+        | ConstrGen.ExistsState prop -> loop A.LocMap.empty prop
+        | _ -> A.LocMap.empty
+      in
+      fun loc -> A.LocMap.find_opt loc map
 
+    let do_solve_regs test es csn =
+      let wanted_final_values =
+        if speedcheck then wanted_final_value test else Fun.const None
+      in
       let rfm = match_reg_events es in
-      let csn =
-        S.RFMap.fold
-          (fun wt rf csn -> match wt with
-          | S.Final _ -> csn
-          | S.Load load ->
-              let v_loaded = get_read load in
-              let v_stored = get_rf_value test load rf in
-              try add_eq v_loaded v_stored csn
-              with Contradiction ->
-                let loc = Misc.as_some (E.location_of load) in
-                Printf.eprintf
-                  "Contradiction on reg %s: loaded %s vs. stored %s\n"
-                  (A.pp_location loc)
-                  (A.V.pp_v v_loaded)
-                  (A.V.pp_v v_stored) ;
-                let module PP = Pretty.Make(S) in
-                PP.show_es_rfm test es rfm ;
-                assert false)
-          rfm csn in
-      if  debug_solver then
-        prerr_endline "++ Solve  registers" ;
+      let add_equations wt rf csn =
+        match wt with
+        | S.Final loc -> (
+            if not speedcheck then csn
+            else
+              match (wanted_final_values loc, rf) with
+              | Some v_wanted, S.Store store ->
+                  let v_stored = get_written store in
+                  (* Here we can't use add_eq because it is possible that there
+                     is a contradiction, and in that case we want the solver to
+                     find it. *)
+                  VC.Assign (v_stored, VC.Atom v_wanted) :: csn
+              | _ -> csn)
+        | S.Load load -> (
+            let v_loaded = get_read load in
+            let v_stored = get_rf_value test load rf in
+            try add_eq v_loaded v_stored csn
+            with Contradiction ->
+              let loc = Misc.as_some (E.location_of load) in
+              Printf.eprintf
+                "Contradiction on reg %s: loaded %s vs. stored %s\n"
+                (A.pp_location loc) (A.V.pp_v v_loaded) (A.V.pp_v v_stored);
+              let module PP = Pretty.Make (S) in
+              PP.show_es_rfm test es rfm;
+              assert false)
+      in
+      let csn = S.RFMap.fold add_equations rfm csn in
+      if debug_solver then prerr_endline "++ Solve  registers";
       match VC.solve csn with
       | VC.NoSolns ->
-         if debug_solver then
-           pp_nosol "register" test es rfm ;
-         None
-      | VC.Maybe (sol,csn) ->
+          if debug_solver then pp_nosol "register" test es rfm;
+          None
+      | VC.Maybe (sol, csn) ->
           Some
-            (E.simplify_vars_in_event_structure sol es,
-             S.simplify_vars_in_rfmap sol rfm,
-             csn)
+            ( E.simplify_vars_in_event_structure sol es,
+              S.simplify_vars_in_rfmap sol rfm,
+              csn )
 
     let solve_regs test es csn =
       match do_solve_regs test es csn with
-      | Some (es,rfm,cns) as r ->
+      | Some (es, rfm, cns) as r ->
           if debug_solver && C.verbose > 0 then begin
-            let module PP = Pretty.Make(S) in
-            prerr_endline "Reg solved, direct" ;
+            let module PP = Pretty.Make (S) in
+            prerr_endline "Reg solved, direct";
             Printf.eprintf "++++ Remaing equations:\n%s\n++++++\n%!"
-              (VC.pp_cnstrnts cns) ;
+              (VC.pp_cnstrnts cns);
             PP.show_es_rfm test es rfm
-          end ;
+          end;
           r
-      | None ->  None
+      | None -> None
 
 (**************************************)
 (* Step 2. Generate rfmap for memory  *)
