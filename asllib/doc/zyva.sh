6c28fbfd1f92639e896dae230f938f1842a0f584
Luc Maranget <Luc.Maranget@inria.fr>
2026-01-14 14:04:41 +0100

    Faster zyva.sh file for developpement.
    We only run the first iteration of hevea,
    using the new option `-fix0`.

--- asllib/doc/zyva.sh
+++ asllib/doc/zyva.sh
@@ -1,5 +1,9 @@
-rm -rf HTML
-mkdir HTML
+mkdir -p HTML
 make  generated_spec
-hevea -fix -O -o HTML/ASL.html book.hva macros.hva ASLReference.tex
+hevea -fix0 -O -o HTML/ASL.html book.hva macros.hva ASLReference.tex
+bibhva HTML/ASL
+if false
+then
+    hevea -fix -O -o HTML/ASL.html book.hva macros.hva ASLReference.tex
+fi
 ( cd HTML && hacha ASL.html )
