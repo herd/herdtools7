19da623344f3b186e67809616b09482120d383ba
Shale Xiong <shale.xiong@arm.com,xiongshale@gmail.com>
2026-01-19 15:33:19 +0000

    [gen] Introduce a new relaxation/edge/atom parser in `diy` tool.
    - We introduce new choice syntax `|`. For example `PosWR|DpAddrdW` means
      either `PosWR` or `DpAddrdW`. This can be used in `diycross7` and
      `diy7`. In `diyone7`, although it will be parsed however an error
      occurs due to input is not a precise one cycle.
    - The new `parser.mly` and `lexer.mll` combination parses the input
      cycle into `Ast.t`.
    - Unify the paring process cross three `diy*` tools.

---
+++ gen/common/ast.ml
@@ -0,0 +1,35 @@
+type t =
+  | One of string
+  | Multi of t
+  | Seq of t list
+  | Choice of t list
+
+let rec pp = function
+| One s -> Printf.sprintf "%s" s
+| Seq ss ->
+    Printf.sprintf "(%s)" (String.concat "," (List.map pp ss))
+| Choice ss ->
+    Printf.sprintf "(%s)" (String.concat "|" (List.map pp ss))
+| Multi ss ->
+    Printf.sprintf "[%s]" (pp ss)
+
+let list_cross_product_map f lhs rhs =
+  List.map ( fun l ->
+    List.map ( fun r ->
+      f l r
+    ) rhs
+  ) lhs
+  |> List.flatten
+
+let rec flatten t =
+  let result = match t with
+  | One str -> [[str]]
+  | Multi multi -> flatten multi
+  | Seq seq -> List.fold_left
+    ( fun acc s ->
+      flatten s
+      |> list_cross_product_map ( @ ) acc
+    ) [[]] seq
+  | Choice choice -> List.map flatten choice |> List.flatten in
+  result
+
