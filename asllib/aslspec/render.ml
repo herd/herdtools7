3af7b6dc83e3c06923ac270dd2058a951bbade99
Roman-Manevich <Roman.Manevich@arm.com>
2026-01-24 15:33:42 +0000

    [aslspec] fix a latex-related rendering issue with custom operators

--- asllib/aslspec/render.ml
+++ asllib/aslspec/render.ml
@@ -447,7 +447,10 @@ module Make (S : SPEC_VALUE) = struct
           | _ ->
               pp_var fmt name)
       | Relation { name; is_operator; args } when is_operator ->
-          pp_operator name layout fmt args
+        (* operator often use custom macros, which may not mix well with arrays,
+           so it's better to put them inside braces. *)
+          fprintf fmt "{ %a }"
+          (pp_operator name layout) args
       | Relation { args } | Tuple { args } | Map { args } ->
           let pp_lhs fmt lhs =
             match lhs with
