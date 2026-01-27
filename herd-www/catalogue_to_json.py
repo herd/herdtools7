ec58cd03adc30801b73cc6779e65c70077fbe26e
Tiberiu Bucur <tiberiu.bucur@arm.com>
2026-01-27 19:59:21 +0000

    [herd-www] Support every Arm ARM release for all catalogues
    Note that this also removes the auto-generated catIncludes.ml
    in favour of a fetch API written through js_of_ocaml, which
    requests the dependencies from the server whilst running herd.
    A pseudo-filesystem is still mounted, but opening it now sends
    the aforementioned GET requests, instead of looking up the file
    contents in a StringMap.

--- herd-www/catalogue_to_json.py
+++ herd-www/catalogue_to_json.py
@@ -2,14 +2,31 @@
 
 import importlib
 import json
-import re
+import os
 import sys
+from glob import glob
+
+def discover_release_cats():
+    """
+    Discover extra aarch64.cat files under:
+        cats/*/aarch64.cat
+
+    Returns paths relative to the book directory, e.g.:
+        cats/ArmARM-L.b/aarch64.cat
+    """
+    matches = glob(os.path.join('cats', '*', 'aarch64.cat'))
+    # Normalize to forward slashes for JSON / browser use
+    return sorted(m.replace(os.sep, '/') for m in matches)
 
 def jsonify(fname):
     m = importlib.import_module(fname)
+
+    base_cats = list(m.cats)
+    extra_cats = discover_release_cats()
+
     j = json.dumps({
         'record' : m.record,
-        'cats' : m.cats,
+        'cats' : base_cats + extra_cats,
         'illustrative_tests' : m.illustrative_tests,
         'cfgs' : m.cfgs,
         'bells' : m.bells if 'bells' in dir(m) else None,
