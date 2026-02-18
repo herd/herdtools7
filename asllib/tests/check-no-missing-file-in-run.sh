cd4a5c1465cbe66552e0d47dae0dce132d766ede
Hadrien Renaud <hadrien.renaud2@arm.com>
2026-02-18 11:48:12 +0000

    [asl,tests] Implement in OCaml exhausivity check.

--- asllib/tests/check-no-missing-file-in-run.sh
+++ asllib/tests/check-no-missing-file-in-run.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+# This script execute [check_no_missing_file_in_run] for each [run.t] file in
+# [asllib/tests].
+#
+# The executable [check_no_missing_file_in_run] is recompiled with [dune].
+
+# bash configuration
 set -o errexit    # Used to exit upon error, avoiding cascading errors
 set -o nounset    # Exposes unset variables
 
-counter=0
+# Our executable
+checker="$1"/_build/default/asllib/tests/check_no_missing_file_in_run.exe
+
+# Recompilation if needed
+dune build --profile=release ${checker}
 
 for d in "$1"/asllib/tests/*.t/; do
-  for f in $(git ls-files HEAD "$d*.asl"); do
-    if ! grep -q "${f##*/}" "${d}run.t"; then
-      echo "ASL file committed and not run: $f";
-      counter=$counter+1;
-    fi;
-  done
+  ${checker} ${d} $(git ls-files HEAD ${d}) &
 done
 
-if [[ $counter -gt 0 ]]; then
-  exit 1;
-else
-  exit 0;
-fi;
+wait
+
