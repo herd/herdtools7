6ed85206d5d4d247542fa96545bf1deb938a5821
Luc Maranget <Luc.Maranget@inria.fr>
2026-02-24 16:08:22 +0100

    [herd] A few additional unit tests
    We add a few tests of the transitive closure of relations.

--- lib/tests/innerrel_test.ml
+++ lib/tests/innerrel_test.ml
@@ -26,3 +26,53 @@ let () =
     assert
       (R.all_topos_kont nodes g (fun _ n -> n+1) 0 = 1)
   with R.Cyclic -> assert false
+
+(**********************)
+(* Transitive closure *)
+(**********************)
+
+let to_list m = R.fold (fun p k -> p::k) m []
+
+let pp_i fmt m =
+  let open Format in
+  fprintf fmt "@[%a@]"
+    (pp_print_list ~pp_sep:pp_print_space
+      (fun fmt (i1, i2) -> fprintf fmt "@[%d<-%d@]" i1 i2))
+    (to_list m)
+
+let simple_tr =
+  let rec loop m =
+    let m2 = R.union m @@ R.sequence m m in
+    if R.subrel m2 m then m2
+    else loop m2 in
+  loop
+
+
+let check_manual k edges =
+  let i = R.of_list edges in
+  let ic = simple_tr i
+  and it = R.transitive_closure i in
+  let ok = R.equal it ic in
+  if not ok then
+    Format.eprintf "@[<hv>%a@ gave with trajan@ %a@ and with log@ %a@]@."
+      pp_i i pp_i it pp_i ic ;
+  k && ok
+
+let () =
+  assert
+    begin
+      List.fold_left check_manual true
+        [[(0, 0);];
+         [(0, 1);];
+         [(1, 1);];
+         [(0, 1); (1, 0)];
+         [(0, 1); (1, 1)];
+         [(0, 1); (1, 2); (2, 1)];
+         [(4, 5); (1, 4); (5, 1); (0, 4)];
+         [(1, 0); (1, 2); (2, 1)];
+         [(0, 1); (1, 0); (1, 2); (2, 1)];
+         [(0,1); (1,2); (2,3); (3,4); (4,5); (5,0);];
+         [(0,1); (0,2); (1,6); (1,3); (1,4); (2,5); (2,6);];
+         [(0,1); (0,2); (1,3); (1,4); (2,5); (2,6); (3,3); (1,1);];
+        ]
+    end
