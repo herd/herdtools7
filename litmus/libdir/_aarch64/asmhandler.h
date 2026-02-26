ba2ce7c7a9a8545013d5398faa05111392df1c57
Simon Park <simon.park@arm.com>
2026-02-26 14:57:59 +0000

    added initial header info

--- litmus/libdir/_aarch64/asmhandler.h
+++ litmus/libdir/_aarch64/asmhandler.h
@@ -1,3 +1,19 @@
+/****************************************************************************/
+/*                           the diy toolsuite                              */
+/*                                                                          */
+/* Jade Alglave, University College London, UK.                             */
+/* Luc Maranget, INRIA Paris, France.                                       */
+/* Rémy Citérin, ARM Ltd, Cambridge, UK                                     */
+/*                                                                          */
+/* Copyright 2025-present Institut National de Recherche en Informatique et */
+/* en Automatique and the authors. All rights reserved.                     */
+/*                                                                          */
+/* This software is governed by the CeCILL-B license under French law and   */
+/* abiding by the rules of distribution of free software. You can use,      */
+/* modify and/ or redistribute the software under the terms of the CeCILL-B */
+/* license as circulated by CEA, CNRS and INRIA at the following URL        */
+/* "http://www.cecill.info". We also give a copy in LICENSE.txt.            */
+/****************************************************************************/
 #ifndef ASMHANDLER_H
 #define ASMHANDLER_H 1
 void exceptions_init_test(void *p);
