ea1c8a5f7ee2669ca0705e35ed71dbda3a462a35
Shale Xiong <shale.xiong@arm.com>
2026-03-25 17:06:53 +0000

    [gen] Add predicate such as `@after(..)` syntax to relax parser
    Extend the relax AST with a predicate node and teach the lexer/parser to
    recognise predicate applications such as `@before(...)` and `@after(...)`.
    
    Add `map_predicate` support to the AST so predicate names can be
    converted before expansion. Parse `before` and `after` into edge predicates,
    then attach the parsed predicate to every edge produced by the decorated
    relax expression.

--- gen/common/edge.ml
+++ gen/common/edge.ml
@@ -64,6 +64,10 @@ module type S = sig
     | Hat
     | Rmw of RMW.rmw  (* Various sorts of read-modify-write *)
 
+  type edge_predicate =
+    | Before
+    | After
+
   val is_id : tedge -> bool
   val is_node : tedge -> bool
   val is_insert_store : tedge -> bool
@@ -72,7 +76,7 @@ module type S = sig
   val compute_rmw : RMW.rmw -> value -> value -> value
   val is_valid_rmw : RMW.rmw list -> bool
 
-  type edge = { edge: tedge;  a1:atom option; a2: atom option; }
+  type edge = { edge: tedge;  a1:atom option; a2: atom option; pred : edge_predicate option }
 
   val plain_edge : tedge -> edge
 
@@ -132,6 +136,12 @@ module type S = sig
 
 (* Resolve Irr directions and unspecified atom *)
   val resolve_edges : edge list -> edge list
+
+(* Add `predicate` to `edge` *)
+  val add_predicate : edge_predicate -> edge -> edge
+
+(* parse `predicate` *)
+  val parse_predicate : string -> edge_predicate
 
 (* Atomic variation over yet unspecified atoms *)
   val varatom : edge list -> (edge list -> 'a -> 'a) -> 'a -> 'a
@@ -197,7 +207,7 @@ and module RMW = A.RMW = struct
   let pre_parse_string s =
     let parsed_result = Lexing.from_string (String.trim s)
     |> LexUtil.parse Parser.main
-    |> Ast.expand in
+    |> Ast.expand (fun _ -> Warn.fatal "predicate should not exist.") in
     match parsed_result with
       | [x] -> x
       | _ ->
@@ -266,13 +276,17 @@ and module RMW = A.RMW = struct
     |Dp (dp, _, _) -> F.is_addr dp
     |_ -> false
 
-  type edge = { edge: tedge;  a1:atom option; a2: atom option; }
+  type edge_predicate =
+    | Before
+    | After
+
+  type edge = { edge: tedge;  a1:atom option; a2: atom option; pred : edge_predicate option }
 
   let can_merge e = not @@ is_insert_store e.edge
 
   open Printf
 
-  let plain_edge e = { a1=None; a2=None; edge=e; }
+  let plain_edge e = { a1=None; a2=None; edge=e; pred = None}
 
   let pp_plain = A.pp_plain
 
@@ -317,12 +331,26 @@ and module RMW = A.RMW = struct
       "{edge=%s, a1=%s, a2=%s}"
       (pp_tedge e.edge) (pp_atom_option e.a1) (pp_atom_option e.a2)
 
+  let pp_predicate = function
+    | Before -> "before"
+    | After -> "after"
+
   let pp_edge_compat compat e =
     let edge = match e.edge with
     | Id -> ""
     | _ -> pp_tedge_compat compat e.edge in
     let annotation = pp_annotations e.edge e.a1 e.a2 in
     edge ^ annotation
+
+  let do_pp_edge compat pp_atom_functor e =
+    let annotation = pp_atom_functor e.edge e.a1 e.a2 in
+    let edge = match e.edge with
+    | Id -> ""
+    | _ -> pp_edge_compat compat e.edge in
+    let edge_anno = edge ^ annotation in
+    match e.pred with
+    | None -> edge_anno
+    | Some pred -> sprintf "@%s(%s)" (pp_predicate pred) edge_anno
 
   let pp_edge e = pp_edge_compat false e
 
@@ -544,10 +572,10 @@ let fold_tedges f r =
     if do_self && instr_atom != None then
       iter_ie
         (fun ie ->
-           add_lxm_edge (sprintf "Iff%s" (pp_ie ie)) { a1=None; a2=instr_atom; edge=(Rf ie); } ;
-           add_lxm_edge (sprintf "Irf%s" (pp_ie ie)) { a1=None; a2=instr_atom; edge=(Rf ie); } ;
-           add_lxm_edge (sprintf "Fif%s" (pp_ie ie)) { a1=instr_atom; a2=None; edge=(Fr ie); } ;
-           add_lxm_edge (sprintf "Ifr%s" (pp_ie ie)) { a1=instr_atom; a2=None; edge=(Fr ie); });
+           add_lxm_edge (sprintf "Iff%s" (pp_ie ie)) { a1=None; a2=instr_atom; edge=(Rf ie); pred = None} ;
+           add_lxm_edge (sprintf "Irf%s" (pp_ie ie)) { a1=None; a2=instr_atom; edge=(Rf ie); pred = None} ;
+           add_lxm_edge (sprintf "Fif%s" (pp_ie ie)) { a1=instr_atom; a2=None; edge=(Fr ie); pred = None} ;
+           add_lxm_edge (sprintf "Ifr%s" (pp_ie ie)) { a1=instr_atom; a2=None; edge=(Fr ie); pred = None});
     ()
 
   let fold_pp_edges f =
@@ -831,7 +859,7 @@ let fold_tedges f r =
      - `remaining` is the unconsumed suffix of `es`. *)
   let rec merge_left (e:edge) (es:edge list) : (bool * edge option * edge list) =
     let is_default e = match e with
-      | { edge=Id; a1=None; a2=None; } -> true
+      | { edge=Id; a1=None; a2=None; pred=None } -> true
       | _ -> false in
     try
       let store_insert,next,rest = find_next_merge es in
@@ -938,6 +966,13 @@ let fold_tedges f r =
     let es = if do_mixed then List.map replace_plain es else es in
     validate_edges es ;
     es
+
+  let add_predicate pred e = {e with pred = Some pred}
+
+  let parse_predicate = function
+    | "before" -> Before
+    | "after" -> After
+    | s -> Warn.user_error "predicate %s is not supported." s
 
 (********************)
 (* Atomic variation *)
@@ -1072,7 +1107,7 @@ let fold_tedges f r =
             fold_atomo
               (fun ao k ->
                 if is_ifetch ao then k
-                else { edge=Id; a1=ao; a2=ao;}::k)
+                else { edge=Id; a1=ao; a2=ao;pred=None}::k)
               [] in
           List.iter
             (fun e -> eprintf " %s" (pp_edge e))
--- gen/tests/test_parser.ml
+++ gen/tests/test_parser.ml
@@ -20,29 +20,46 @@ let parser_tests =
     "[A|B|C]";
     "[A|B C]";
     "A,";
+    "A B C";
+    "[A,[B]]";
+    "A,[B,C]";
+    "[A,[B,C]]";
+    "@before(A)";
+    "A @after(B)";
+    "[A @before(B)]";
+    "@before(A) @before(B)";
+    "A @after(B) @after(C)";
+    "@before([A B])";
+    "@after([A B])";
   ]
 
 let unit_test parser_grammar label input =
   Printf.printf "%s: %s\n" label input ;
-  let ast =
-    Lexing.from_string input
-    |> LexUtil.parse parser_grammar in
-  Printf.printf "%s\n" (Ast.pp Fun.id ast) ;
-  Ast.expand ast
-  |> pp_list (pp_list Fun.id)
-  |> Printf.printf "%s\n"
+  try
+    let ast =
+      Lexing.from_string input
+      |> LexUtil.parse parser_grammar in
+    Printf.printf "%s\n" (Ast.pp Fun.id Fun.id ast) ;
+    Ast.expand ( fun pred prim -> Printf.sprintf "@%s(%s)" pred prim ) ast
+    |> pp_list (pp_list Fun.id)
+    |> Printf.printf "%s\n"
+  with Parser.Error ->
+    Printf.printf "Parser.Error\n"
+
+let parser_configs = [
+  "diyone7 parser", Parser.main;
+  "diycross7 parser", Parser.diycross7;
+  "diy7 parser", Parser.diy7;
+  "cumul parser (for -cumul in diy7)", Parser.cumul;
+]
 
 let () =
   List.iter
     (fun input ->
       List.iter
         (fun (label, parser_grammar) ->
-          unit_test parser_grammar label input
-          )
-        [
-          "default parser (for diyone7)", Parser.main;
-          "top level choice parser (for diycross7 and diy7)", Parser.main_top_level_choice;
-        ] ;
+          unit_test parser_grammar label input)
+        parser_configs ;
       Printf.printf "\n")
     parser_tests
 
@@ -60,26 +77,10 @@ let equivalent_syntax_tests = [
 let () =
   List.iter
     (fun input ->
-      unit_test Parser.main_top_level_choice "equivalent syntax test for (diycross7 and diy7)" input;
+      unit_test Parser.diycross7 "equivalent syntax test for diycross7" input;
+      unit_test Parser.diy7 "equivalent syntax test for diy7" input;
       Printf.printf "\n")
     equivalent_syntax_tests
-
-let cumul_tests = [
-  "A B";
-  "A,B";
-  "[A,B]";
-  "[A,[B]]";
-  "A B C";
-  "A,[B,C]";
-  "[A,[B,C]]";
-]
-
-let () =
-  List.iter
-    (fun input ->
-      unit_test Parser.cumul "cumul parser (for -cumul in diy7)" input;
-      Printf.printf "\n")
-    cumul_tests
 
 module TestAArch64 = AutoArch.Make(AArch64Arch_gen.Make(AArch64Arch_gen.Config))
 
