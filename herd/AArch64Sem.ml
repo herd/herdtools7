fc9ea66985fec61b5b4d8ae75e7dc2b23e0eb084
raghav <raghav.roy@arm.com>
2026-04-03 20:56:29 +0100

    Add LDAP/STLP instructions to herd

--- herd/AArch64Sem.ml
+++ herd/AArch64Sem.ml
@@ -1919,6 +1919,7 @@ Arguments:
               let open Annot in
               match tnt with
               | (`PaIQ) -> Q
+              | (`PaA) -> A
               | (`Pa) -> N
               | (`PaN) -> NTA in
             let open AArch64 in
@@ -2062,14 +2063,14 @@ Arguments:
           let open AArch64 in
           let open Annot in
           match tnt with
-          | (`PaIL) -> L
+          | (`PaIL) | (`PaL) -> L
           | (`Pa) -> N
           | (`PaN) -> NTA in
         match md with
         | AArch64.Idx ->
             let (>>|) =
               match tnt with
-              | AArch64.(`Pa|`PaN) -> (>>|)
+              | AArch64.(`Pa|`PaN|`PaL) -> (>>|)
               | AArch64.(`PaIL) -> M.seq_mem in
             let (>>>) = M.data_input_next in
             do_str rd
