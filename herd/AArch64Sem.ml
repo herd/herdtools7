0841e567a632a3707d82c1f8fa31e090471e07b6
raghav <raghav.roy@arm.com>
2026-04-07 22:43:09 +0100

    Add SEVL semantics
    Added SEVL semantics that does
    a simple write to a symbolic
    location that is thread-specific

--- herd/AArch64Sem.ml
+++ herd/AArch64Sem.ml
@@ -3656,7 +3656,13 @@ Arguments:
         | I_SEV ->
            !(M.mk_singleton_es (Act.NoAction) ii)
         | I_SEVL ->
-           !(M.mk_singleton_es (Act.NoAction) ii)
+          let thread_id = ii.A.proc in
+           let sym_val = Constant.mk_sym_virtual (Printf.sprintf "ev%d" thread_id) in
+           let loc = A.Location_global (V.Val sym_val) in
+           let nexp = AArch64Explicit.NExp AArch64Explicit.Other in
+           let v = V.intToV 1 in
+           !(M.mk_singleton_es (
+                Act.Access (Dir.W, loc, v, Annot.N, nexp, MachSize.Quad, Access.VIR)) ii)
         (* Branches *)
         | I_B l ->
            M.mk_singleton_es (Act.NoAction) ii
