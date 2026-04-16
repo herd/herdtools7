d956e5e6322b5acd016041c8e7c6bbde0a895db7
Diana Oloeru <diana.oloeru@arm.com>
2026-04-16 14:57:51 +0100

    [catalogue] Add ETS3 tests

---
+++ catalogue/aarch64-ETS3/shelf.py
@@ -0,0 +1,15 @@
+record = "AArch64 ETS3"
+
+cats = [
+    "cats/aarch64.cat",
+]
+
+cfgs = [
+    "cfgs/web.cfg",
+]
+
+illustrative_tests = [
+    "tests/MP+dmb.sttp+isb.litmus",
+    "tests/S+tlbi-sync.ishptedb1p+po.litmus",
+    "tests/S+tlbi-sync.ishpteel01p+po.litmus",
+]
