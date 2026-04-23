a195ce35826004d6cd11b8294743cb278ba15e0f
Luc Maranget <Luc.Maranget@inria.fr>
2026-04-23 13:26:33 +0200

    [herd,test] Include x66_64 catalogue tests in default testing

---
+++ catalogue/x86_64/shelf.py
@@ -0,0 +1,41 @@
+record = "X86-64"
+
+cats = [
+    "cats/x86tso-mixed.cat",
+    "cats/sc.cat",
+    ]
+
+cfgs = [
+    "cfgs/new-web.cfg"
+]
+
+illustrative_tests = [
+    "tests/2+2W.litmus",
+    "tests/LB.litmus",
+    "tests/MP.litmus",
+    "tests/MP+po+po-rfi-po.litmus",
+    "tests/R.litmus",
+    "tests/R+po+mfence.litmus",
+    "tests/R+po+po-rfi-po.litmus",
+    "tests/R+po+rfi-po.litmus",
+    "tests/RWC.litmus",
+    "tests/RWC+po+mfence.litmus",
+    "tests/RWC+po+rfi-po.litmus",
+    "tests/SB.litmus",
+    "tests/SB+mfence+po.litmus",
+    "tests/SB+mfence+po-rfi-po.litmus",
+    "tests/SB+mfence+rfi-po.litmus",
+    "tests/SB+mfences.litmus",
+    "tests/SB+po+po-rfi-po.litmus",
+    "tests/SB+po+rfi-po.litmus",
+    "tests/SB+rfi-po+po-rfi-po.litmus",
+    "tests/SB+rfi-pos.litmus",
+    "tests/S.litmus",
+    "tests/WRC.litmus",
+    "tests/WRR+2W.litmus",
+    "tests/WRW+2W.litmus",
+    "tests/WRW+WR.litmus",
+    "tests/WRW+WR+po+mfence.litmus",
+    "tests/WRW+WR+po+rfi-po.litmus",
+    "tests/WWC.litmus",
+]
