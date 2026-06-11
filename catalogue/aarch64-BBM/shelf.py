f9ff8b3341c70d0c39b20bede4d368cfa629d223
Nikos Nikoleris <nikos.nikoleris@arm.com>
2026-06-11 16:18:48 +0100

    [catalogue] Add a new catalogue for aarch64 illustrative of BBM
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

---
+++ catalogue/aarch64-BBM/shelf.py
@@ -0,0 +1,20 @@
+record = "AArch64 BBM"
+
+cats = [
+    "cats/aarch64.cat",
+]
+
+cfgs = [
+    "cfgs/web.cfg",
+]
+
+illustrative_tests = [
+    "tests/CoRR+PteGRE.litmus",
+    "tests/CoRR+PteNC.litmus",
+    "tests/CoRR+PteOA.DB0.litmus",
+    "tests/CoRR+PteOA.DB0.val.litmus",
+    "tests/CoRR+PteOA.DB1.val.litmus",
+    "tests/MP+tlbi-sync.ishspteaf0pteoa.af1+pos.litmus",
+    "tests/MP+tlbi-sync.ishsptev0pteoa.v1+pos.litmus",
+    "tests/MP+tlbi-sync.ishsRpteaf0pte-amo.casptepteoa.af1+pos.litmus",
+]
