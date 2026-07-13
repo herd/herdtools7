8a83b1c8fc6676fa029655c66542852506ef064c
Hadrien Renaud <hadrien.renaud2@arm.com>
2026-07-13 10:20:54 +0100

    [herd] When previewing on MacOS, remove temp pdfs
    The previous commits created a pdf file per execution shown, and the
    previous commit merged them into a single PDF file. This commit cleans
    up the files that are merged.
    
    Notice that after this commit, there is no difference when showing a
    single pdf file.

--- herd/show.ml
+++ herd/show.ml
@@ -62,7 +62,10 @@ module Make(O:PrettyConf.S) = struct
     let r = Sys.command (suppress_stderr cmd) in
     if O.debug then eprintf "Command: [%s] -> %i\n%!" cmd r
 
-  let run_cmds cmds = String.concat " && " cmds |> run_cmd
+  let run_cmds cmds =
+    (* In debug mode, break lines so that command is easier to read *)
+    let and_ = if O.debug then " \\\n  && " else " && " in
+    String.concat and_ cmds |> run_cmd
 
   let do_show_file ?(keep_tmp_pdf = O.debug) name_dot prog ext : unit =
     let name_ps = extfile name_dot ext in
@@ -77,11 +80,21 @@ module Make(O:PrettyConf.S) = struct
     {|/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/MacOS/join|}
 
   let do_show_file_preview name_dot =
+    let dirname, name_dot = Filename.(dirname name_dot, basename name_dot) in
     run_cmds
       [
+        (* cd in temp dir, just so that command is easier to read in debug mode *)
+        sprintf "cd %s" dirname;
+        (* Generate the pdfs *)
         sprintf "%s -Tpdf %s -O" generator name_dot;
-        sprintf "%s -o %s.all.pdf %s*.pdf" macos_join_pdf name_dot name_dot;
-        sprintf "open -a Preview %s.all.pdf" name_dot;
+        (* Merge the pdfs into one big pdf *)
+        sprintf "%s -o %s.all %s*.pdf" macos_join_pdf name_dot name_dot;
+        (* Remove the individual pdfs *)
+        sprintf "rm %s*.pdf" name_dot;
+        (* Move the big pdf into a .pdf file *)
+        sprintf "mv %s.all %s.pdf" name_dot name_dot;
+        (* Open the big pdf *)
+        sprintf "open -a Preview %s.pdf" name_dot;
       ]
 
   let show_file_with_view view name_dot : unit =
