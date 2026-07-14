486d972a66eeac83e00ea855518ac0dcdc6555b3
Shale Xiong <shale.xiong@arm.com>
2026-07-14 14:11:11 +0100

    [gen] Delegate `worth_final` to structured atoms.

--- gen/common/AArch64Arch_gen.ml
+++ gen/common/AArch64Arch_gen.ml
@@ -660,6 +660,10 @@ module StructuredAtom = struct
     | Some { access_type = PairAccess _; _ } -> Some 2
     | Some _|None -> None
 
+  let worth_final = function
+    | { access_order = OrderAtomic _; _ } -> true
+    | _ -> false
+
   let applies a d =
     let open WPTE in
     match a.access_type,a.access_order,d with
@@ -1099,13 +1103,8 @@ let is_tthm fields =
        (fun atom r -> f (StructuredAtom.to_legacy atom) r)
        r
 
-   let worth_final (a,_) = match a with
-     | Atomic _ -> true
-     | Acq _|AcqPc _|Rel _|Plain _|Tag|Instr
-     | CapaTag|CapaSeal
-     | Pte _|Neon _
-     | Pair _
-       -> false
+   let worth_final atom =
+     StructuredAtom.worth_final (StructuredAtom.of_legacy atom)
 
 
 
