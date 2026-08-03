82574a0e41e9fd3ec7d19106a2a517d5d06ffc75
Pau Ruiz Safont <pau.ruizsafont@arm.com>
2026-08-03 17:22:15 +0100

    [internal] replace new_temp_dir
    The new function does not have to fork into a new process and is faster
    as a result.
    
    Signed-off-by: Pau Ruiz Safont <pau.ruizsafont@arm.com>

--- internal/lib/filesystem.ml
+++ internal/lib/filesystem.ml
@@ -47,7 +47,14 @@ let rec remove_recursive path =
       Sys.remove path
   end
 
+let temp_name root =
+  let name = Random.bits64 () in
+  Printf.sprintf "%s/%Ld" root name
+
 let new_temp_dir () =
-  let path = ref "" in
-  Command.run ~stdout:(fun c -> path := input_line c) "mktemp" ["-d"] ;
-  !path
+  let rec try_name counter =
+    let name = temp_name (Filename.get_temp_dir_name ()) in
+    try Sys.mkdir name 0o700 ; name
+    with Sys_error _ as e ->
+      if counter >= 10 then raise e else try_name (counter + 1)
+  in try_name 0
