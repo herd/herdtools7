e588116b0d60525a76b4a0080d0d018d6a8452b7
Shale Xiong <shale.xiong@arm.com>
2026-08-20 11:28:04 +0100

    [gen] Replace AArch64 compilation assertions with diagnostics

--- gen/cycle.ml
+++ gen/cycle.ml
@@ -959,7 +959,7 @@ let check_cycle c =
           | Data _ ->
             let bank = n.evt.bank in
             begin match bank with
-            | Instr -> Warn.fatal "instruction annotation to data bank not possible?"
+            | Instr -> Warn.fatal "instruction annotation on a data location is not possible"
             | Ord ->
               let st = set_write_val_ord st n in
               let check_fault, st =
