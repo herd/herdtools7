43e3fe1401e8efce5c7b29858f83a31793674a7f
Shale Xiong <shale.xiong@arm.com>
2026-09-01 15:35:23 +0100

    [gen] Reject annotations incompatible with edge directions

--- gen/tests/test_parser.ml
+++ gen/tests/test_parser.ml
@@ -90,6 +90,10 @@ let remove_invalid_relaxes_inputs = [
   "[A,Amo.StAdd,L]";
   "[P,Amo.StAdd,L]";
   "[A,Amo.LdAdd,L]";
+  "[A,PosWW]";
+  "[A,PosRW]";
+  "[Q,PodWR]";
+  "[Q,PodRR]";
 ]
 
 let remove_invalid_relaxes_test input =
