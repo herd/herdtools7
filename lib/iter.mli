ab1667972e366ba25bfafe8dbf79aaab4f7ba800
Filippo Sestini <filippo.sestini@arm.com>
2026-09-02 15:26:05 +0100

    [lib] Add module Iter

---
+++ lib/iter.mli
@@ -0,0 +1,23 @@
+(****************************************************************************)
+(*                           the diy toolsuite                              *)
+(*                                                                          *)
+(* Jade Alglave, University College London, UK.                             *)
+(* Luc Maranget, INRIA Paris-Rocquencourt, France.                          *)
+(*                                                                          *)
+(* Copyright 2026-present Institut National de Recherche en Informatique et *)
+(* en Automatique and the authors. All rights reserved.                     *)
+(*                                                                          *)
+(* This software is governed by the CeCILL-B license under French law and   *)
+(* abiding by the rules of distribution of free software. You can use,      *)
+(* modify and/ or redistribute the software under the terms of the CeCILL-B *)
+(* license as circulated by CEA, CNRS and INRIA at the following URL        *)
+(* "http://www.cecill.info". We also give a copy in LICENSE.txt.            *)
+(****************************************************************************)
+
+(** Push-based iterators. *)
+
+type 'a t = ('a -> unit) -> unit
+
+val fold : ('acc -> 'a -> 'acc) -> 'acc -> 'a t -> 'acc
+(** [fold f init iter] folds [f] over every element produced by [iter], in
+    iteration order. *)
