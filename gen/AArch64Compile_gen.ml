80ecb84e9ff15a97aeab68cd5e2148e46b15325f
Shale Xiong <shale.xiong@arm.com>
2026-09-10 13:48:00 +0100

    [gen] Check pair-load results per register.

--- gen/AArch64Compile_gen.ml
+++ gen/AArch64Compile_gen.ml
@@ -1657,7 +1657,8 @@ module Make(Cfg:Config) : XXXCompile_gen.S =
 
     let emit_ldp_reg opt st init rA =
       let r1,r2,st = next_reg2 st in
-      r1,init,pseudo [do_ldp opt r1 r2 rA;add vloc r1 r1 r2;],st
+      let st = A.set_friends r1 [r2] st in
+      r1,init,pseudo [do_ldp opt r1 r2 rA;],st
 
     let emit_ldp opt st p init loc =
       let rA,init,st =  U.next_init st p init loc in
--- gen/cycle.ml
+++ gen/cycle.ml
@@ -1172,16 +1172,15 @@ let set_read_individual_v n cell check_value =
   let idx,atom = split_access_atom e.atom in
   let v = E.extract_value cell.(idx) atom in
 (* eprintf "SET READ: cell=0x%x, v=0x%x\n" cell v ; *)
-  let e = { e with v=v; check_value } in
+  let e = { e with v=v; cell=[|v|]; check_value } in
   n.evt <- e
 (* eprintf "AFTER %a\n" debug_node n *)
 
 let set_read_pair_v n cell check_value =
   let e = n.evt in
-  let v0 = E.extract_value cell.(0) e.atom |> Value.to_int
-  and v1 =  E.extract_value cell.(1) e.atom |> Value.to_int in
-  let v = v0 + v1 |> Value.from_int in
-  let e = { e with v=v; check_value } in
+  let v0 = E.extract_value cell.(0) e.atom
+  and v1 = E.extract_value cell.(1) e.atom in
+  let e = { e with v=v0; cell=[|v0;v1|]; check_value } in
   n.evt <- e
 
 (* Assume all the events are for the same location,
